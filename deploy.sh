5a4430215928011b192cbed8f154fd2b27b4a28a
Josef Reidinger <jreidinger@suse.cz>
2021-12-29 15:59:13 +0100

    handle failures

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
-sudo zypper --non-interactive install gcc gcc-c++ make openssl-devel ruby-devel npm git
+sudo zypper --non-interactive install gcc gcc-c++ make openssl-devel ruby-devel npm git || exit 1
 
-git clone https://github.com/yast/the-installer
+git clone https://github.com/yast/the-installer || exit 1
 cd the-installer
 sudo cp yastd/share/dbus-yastd.conf /etc/dbus-1/system.d/yastd.conf
 cd yastd; bundle install; cd -
