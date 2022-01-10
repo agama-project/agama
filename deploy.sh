a1e53fc48eca144e7b24d8e0fb92d9af669fcc85
Josef Reidinger <jreidinger@suse.cz>
2022-01-10 09:53:06 +0100

    add augeas-devel dependency

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-sudo zypper --non-interactive install gcc gcc-c++ make openssl-devel ruby-devel npm git || exit 1
+sudo zypper --non-interactive install gcc gcc-c++ make openssl-devel ruby-devel npm git augeas-devel || exit 1
 
 git clone https://github.com/yast/the-installer || exit 1
 cd the-installer
