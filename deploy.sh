343e2117ac42057323bac363580362f91642effd
Imobach González Sosa <igonzalezsosa@suse.com>
2022-01-10 12:31:17 +0000

    Minor tweaks to the deploy.sh script
    * Do not run the proxy as root (it is not needed).
    * Make sure to install Ruby 2.7 development files.
    * Install gems in vendor/bundle directories.

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 #! /bin/sh
-sudo zypper --non-interactive install gcc gcc-c++ make openssl-devel ruby-devel npm git augeas-devel || exit 1
+sudo zypper --non-interactive install gcc gcc-c++ make openssl-devel ruby2.7-devel npm git augeas-devel || exit 1
 
 git clone https://github.com/yast/the-installer || exit 1
 cd the-installer
 sudo cp yastd/share/dbus-yastd.conf /etc/dbus-1/system.d/yastd.conf
-cd yastd; bundle install; cd -
-cd yastd-proxy; bundle install; cd -
+cd yastd; bundle config --set local path vendor/bundle; bundle install; cd -
+cd yastd-proxy; bundle config --set local path vendor/bundle; bundle install; cd -
 cd web; npm install; cd -
 cd yastd; sudo bundle exec bin/yastd& cd -
-cd yastd-proxy; sudo bundle exec bin/yastd-proxy& cd -
+cd yastd-proxy; bundle exec bin/yastd-proxy& cd -
 cd web; npm start& cd -
 
 wait
