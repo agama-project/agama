bc0f35be954ba6b699e27ae357f1696a7d85bd08
Imobach González Sosa <igonzalezsosa@suse.com>
2022-02-23 12:59:12 +0000

    [web] Remove unneeded packages and files
    * Remove the web-vitals package.
    * Remove service workers configuration.

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -22,8 +22,6 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import * as serviceWorker from "./serviceWorker";
 import { InstallerClientProvider } from "./context/installer";
 import { AuthProvider } from "./context/auth";
 import InstallerClient from "./lib/InstallerClient";
@@ -43,13 +41,3 @@ ReactDOM.render(
   </StrictMode>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorker.unregister();
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
