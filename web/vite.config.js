f32165de812ce497009c0ce7d59292035d4904f3
Imobach González Sosa <igonzalezsosa@suse.com>
2022-02-23 14:33:09 +0000

    [web] Adjust the base public path

--- web/vite.config.js
+++ web/vite.config.js
@@ -4,6 +4,7 @@ import { viteCommonjs } from '@originjs/vite-plugin-commonjs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [react(), viteCommonjs()],
   optimizeDeps: {
     include: ['attr-accept']
