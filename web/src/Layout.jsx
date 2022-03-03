853406b5d9f579e67f080478ad40bf3acbceba82
David Díaz González <dgonzalez@suse.de>
2022-03-03 13:42:34 +0000

    [web] Add a custom layout component
    It is the starting point for setting up the proposed layout[1][2] and it
    is using none PatterFly component in an attempt to keep it tiny and simple.
    
    Of course, components like Masthead[3], Panel[4], or even [Page] can be
    replace some parts of it in the future, if really needed.
    
    About CSS, it tries to follow the same methodology than PatternFly,
    i.e., BEM[6].
    
    [1] https://trello.com/c/UBTn9amG (internal link)
    [2] https://yast.opensuse.org/blog/2022-02-23/start-2022#dinstaller
    [3] https://www.patternfly.org/v4/components/masthead
    [4] https://www.patternfly.org/v4/components/panel
    [5] https://www.patternfly.org/v4/components/page
    [6] http://getbem.com/

---
+++ web/src/Layout.jsx
@@ -0,0 +1,104 @@
+/*
+ * Copyright (c) [2022] SUSE LLC
+ *
+ * All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of version 2 of the GNU General Public License as published
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, contact SUSE LLC.
+ *
+ * To contact SUSE LLC about this file by physical or electronic mail, you may
+ * find current contact information at www.suse.com.
+ */
+
+import React from "react";
+
+import {
+  EOS_MENU as MenuIcon
+} from "eos-icons-react";
+
+/**
+ * D-Installer main layout component.
+ *
+ * It displays the content in a single vertical responsive column with sticky
+ * header and fixed footer.
+ *
+ * @example
+ *   <Layout
+ *     sectionTitle="Software"
+ *     SectionIcon={SoftwareSectionIcon}
+ *     FooterActions={SoftwareSectionActions}
+ *   >
+ *     <SoftwareSection />
+ *   </Layout>
+ *
+ * @param {object} props - component props
+ * @param {string} [props.sectionTitle] - the section title in the header
+ * @param {React.ReactNode} [props.SectionIcon] - the section icon in the header
+ * @param {React.ReactNode} [props.FooterMessages] - messages to be  shown in the footer
+ * @param {React.ReactNode} [props.FooterActions] - actions shown in the footer
+ * @param {React.ReactNode} [props.children] - the section content
+ *
+ */
+function Layout({ sectionTitle, SectionIcon, FooterMessages, FooterActions, children }) {
+  const responsiveWidthRules = "pf-u-w-75-on-md pf-u-w-66-on-lg pf-u-w-50-on-xl pf-u-w-33-on-2xl"
+  const className = `layout ${responsiveWidthRules}`
+
+  // FIXME: by now, it is here only for illustrating a possible app/section menu
+  const renderHeaderLeftAction = () => <MenuIcon className="layout__header__action-icon" />
+
+  const renderSectionTitle = () => {
+    return (
+      <h1>
+        <SectionIcon className="layout__header__section-title-icon" />
+        { sectionTitle }
+      </h1>
+    );
+  }
+
+  const renderFooter = () => {
+    if(!FooterActions) return null;
+
+    return (
+      <div className="layout__footer">
+        <div className="layout__footer-info-area">
+          <FooterMessages />
+        </div>
+
+        <div className="layout__footer-actions-area">
+          <FooterActions />
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className={className}>
+      <div className="layout__header">
+        <div className="layout__header__left-action">
+          { renderHeaderLeftAction () }
+        </div>
+
+        <div className="layout__header__section-title">
+          { renderSectionTitle() }
+        </div>
+      </div>
+
+      <main className="layout__content">
+        { children }
+      </main>
+
+      { renderFooter() }
+    </div>
+  );
+}
+
+export default Layout;
