0f3c469184d4b43b164e0ae92f2e69a8944d9512
Josef Reidinger <jreidinger@suse.cz>
2022-03-14 22:12:54 +0100

    add dbus frontend for users

---
+++ web/src/lib/client/users.js
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) [2022] SUSE LLC
+ *
+ * All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of version 2 of the GNU General Public License as published
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, contact SUSE LLC.
+ *
+ * To contact SUSE LLC about this file by physical or electronic mail, you may
+ * find current contact information at www.suse.com.
+ */
+
+import { applyMixin, withDBus } from "./mixins";
+
+const USERS_IFACE = "org.opensuse.DInstaller.Users1";
+
+export default class UsersClient {
+  constructor(dbusClient) {
+    this._client = dbusClient;
+  }
+
+  /**
+   * Return the first user structure
+   *
+   * @return {Promise.<Object>}
+   */
+  async getUser() {
+    const proxy = await this.proxy(USERS_IFACE);
+    const [fullName, userName, autologin] = proxy.FirstUser.map(u => u.v);
+    return { fullName, userName, autologin };
+  }
+
+  /**
+   * Return true if root password is set
+   *
+   * @return {Promise.<Boolean>}
+   */
+  async isRootPassword() {
+    const proxy = await this.proxy(USERS_IFACE);
+    return proxy.RootPasswordSet;
+  }
+
+  /**
+   * Set the languages to install
+   *
+   * @param {object} user - object with full name, user name, password and boolean for autologin
+   * @return {Promise.<String|undefined>}
+   */
+  async setUser(user) {
+    const proxy = await this.proxy(USERS_IFACE);
+    return proxy.SetFirstUser(user.fullName, user.userName, user.password, user.autologin);
+  }
+
+  /**
+   * Set the root password
+   *
+   * @param {String} password - plain text root password ( maybe allow client side encryption?)
+   * @return {Promise.<String|undefined>}
+   */
+  async setRootPassword(password) {
+    const proxy = await this.proxy(USERS_IFACE);
+    return proxy.SetRootPassword(password, false);
+  }
+
+  // TODO: ssh key for root
+}
+
+applyMixin(UsersClient, withDBus);
