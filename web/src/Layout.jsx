7a35d85bb9a856f47518d85f256d7155f8871b64
David Díaz González <dgonzalez@suse.de>
2022-03-16 10:53:45 +0000

    [web] Do not show the (fake) application menu
    It was there as an example, but let's not display it until we have a
    real content for it.

--- web/src/Layout.jsx
+++ web/src/Layout.jsx
@@ -21,8 +21,6 @@
 
 import React from "react";
 
-import { EOS_MENU as MenuIcon } from "eos-icons-react";
-
 /**
  * D-Installer main layout component.
  *
@@ -39,6 +37,7 @@ import { EOS_MENU as MenuIcon } from "eos-icons-react";
  *   </Layout>
  *
  * @param {object} props - component props
+ * @param {React.ReactNode} [props.MenuIcon] - the icon for the application menu
  * @param {string} [props.sectionTitle] - the section title in the header
  * @param {React.ReactNode} [props.SectionIcon] - the section icon in the header
  * @param {React.ReactNode} [props.FooterMessages] - messages to be  shown in the footer
@@ -47,6 +46,7 @@ import { EOS_MENU as MenuIcon } from "eos-icons-react";
  *
  */
 function Layout({
+  MenuIcon,
   sectionTitle,
   SectionIcon,
   FooterMessages,
