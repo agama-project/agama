4c860d4a91ee9944d060806b5339e0c7b8f75815
David Díaz González <dgonzalez@suse.de>
2022-03-29 12:31:04 +0100

    [web] Add "About" component
    Which is displayed in the footer, next to the logo.

---
+++ web/src/About.jsx
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) [2022] SUSE LLC
+ *
+ * All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of version 2 of the GNU General Public License as published
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, contact SUSE LLC.
+ *
+ * To contact SUSE LLC about this file by physical or electronic mail, you may
+ * find current contact information at www.suse.com.
+ */
+
+import React, { useState } from "react";
+import { Button, Modal, ModalVariant } from "@patternfly/react-core";
+
+export default function About() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const open = () => setIsOpen(true);
+  const close = () => setIsOpen(false);
+
+  return (
+    <>
+      <Button variant="link" onClick={open}>
+        About
+      </Button>
+
+      <Modal
+        isOpen={isOpen}
+        showClose={false}
+        variant={ModalVariant.small}
+        title="About D-Installer"
+        actions={[
+          <Button key="confirm" variant="primary" onClick={close}>
+            Close
+          </Button>
+        ]}
+      >
+        Lorem ipsum
+      </Modal>
+    </>
+  );
+}
--- web/src/Layout.jsx
+++ web/src/Layout.jsx
@@ -24,6 +24,8 @@ import React from "react";
 import "./layout.scss";
 import logo from "./assets/suse-horizontal-logo.svg";
 
+import About from "./About";
+
 /**
  * D-Installer main layout component.
  *
@@ -83,6 +85,7 @@ function Layout({ MenuIcon, sectionTitle, SectionIcon, FooterActions, children }
       <div className="layout__footer">
         <div className="layout__footer-info-area">
           <img src={logo} alt="Logo of SUSE" className="company-logo" />
+          <About />
         </div>
         <div
           className="layout__footer-actions-area"
