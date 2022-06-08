3386c6fc44d41f5525b064a1b75c9f489b075df4
Imobach González Sosa <igonzalezsosa@suse.com>
2022-06-08 16:01:37 +0100

    Simplify DBusError tests

--- web/src/DBusError.test.jsx
+++ web/src/DBusError.test.jsx
@@ -22,25 +22,27 @@
 import React from "react";
 
 import { screen } from "@testing-library/react";
-import { installerRender } from "./test-utils";
+import { installerRender, plainRender } from "./test-utils";
 
 import DBusError from "./DBusError";
 
 describe("DBusError", () => {
-  it("includes a generic D-Bus connection problem message", async () => {
-    installerRender(<DBusError />);
+  it("includes a generic D-Bus connection problem message", () => {
+    plainRender(<DBusError />);
 
-    await screen.findByText(/Could not connect to the D-Bus service/i);
+    expect(screen.getByText(/Could not connect to the D-Bus service/i))
+      .toBeInTheDocument()
   });
 
-  it("includes a button for reloading", async () => {
-    installerRender(<DBusError />);
+  it("includes a button for reloading", () => {
+    plainRender(<DBusError />);
 
-    await screen.findByRole("button", { name: /Reload/i });
+    expect(screen.getByText(/Could not connect to the D-Bus service/i))
+      .toBeInTheDocument()
   });
 
   it("calls location.reload when user clicks on 'Reload'", async () => {
-    const { user } = installerRender(<DBusError />);
+    const { user } = plainRender(<DBusError />);
 
     const reloadButton = await screen.findByRole("button", { name: /Reload/i });
 
