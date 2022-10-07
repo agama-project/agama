6f5e9f09d94aa2fa71d1fb8ba2e71177ba9938ff
Imobach González Sosa <igonzalezsosa@suse.com>
2022-10-07 17:03:20 +0100

    [web] Reimplement mixins using class expresions

--- web/src/client/mixins.js
+++ web/src/client/mixins.js
@@ -19,133 +19,106 @@
  * find current contact information at www.suse.com.
  */
 
-const withDBus = {
-  /**
-   * Registers a proxy for given iface
-   *
-   * @param {string} iface - D-Bus iface
-   * @param {string} path - D-Bus object path
-   * @return {Object} a cockpit DBusProxy
-   */
-  async proxy(iface, path) {
-    const proxy = this._client.proxy(iface, path, { watch: true });
-    await proxy.wait();
-
-    return proxy;
-  },
-
-  /**
-   * Register a callback to run when properties change for given D-Bus path
-   *
-   * @param {string} path - D-Bus path
-   * @param {string} iface - D-Bus interface name
-   * @param {function} handler - callback function
-   * @return {function} function to unsubscribe
-   */
-  onObjectChanged(path, iface, handler) {
-    const { remove } = this._client.subscribe(
-      {
-        path,
-        interface: "org.freedesktop.DBus.Properties",
-        member: "PropertiesChanged"
-      },
-      (_path, _iface, _signal, args) => {
-        const [source_iface, changes, invalid] = args;
-        if (iface === source_iface) {
-          handler(changes, invalid);
-        }
-      }
-    );
-    return remove;
-  },
-
-  /**
-   * Register a callback to run when some D-Bus signal is emitted
-   *
-   * @param {function} handler - callback function
-   * @return {function} function to unsubscribe
-   */
-  onSignal(match, handler) {
-    const { remove } = this._client.subscribe(match, handler);
-    return remove;
-  }
-};
-
+// @ts-check
 const STATUS_IFACE = "org.opensuse.DInstaller.ServiceStatus1";
 
-const withStatus = (object_path) => {
-  return {
-    /**
-     * Returns the service status
-     *
-     * @return {Promise.<number>} 0 for idle, 1 for busy
-     */
-    async getStatus() {
-      const proxy = await this.proxy(STATUS_IFACE, object_path);
-      return proxy.Current;
-    },
+/**
+ * @typedef {object} Progress
+ * @property {number} total - number of steps
+ * @property {number} current - current step
+ * @property {string} message - message of the current step
+ * @property {boolean} finished - whether the progress already finished
+ */
 
-    /**
-     * Register a callback to run when the "CurrentInstallationPhase" changes
-     *
-     * @param {function} handler - callback function
-     * @return {function} function to disable the callback
-     */
-    onStatusChange(handler) {
-      return this.onObjectChanged(object_path, STATUS_IFACE, (changes) => {
-        if ("Current" in changes) {
-          handler(changes.Current.v);
-        }
-      });
-    }
-  };
+/**
+ * @callback ProgressHandler
+ * @param {Progress} progress - progress status
+ * @return {void}
+ */
+
+/**
+ * @typedef {new(...args: any[]) => T} GConstructor
+ * @template {object} T
+ */
+
+/**
+ * @typedef {GConstructor<{ client: import("./dbus").DBusClient }>} WithDBusClient
+ */
+
+/**
+ * Extends the given class with methods to get andd track the progress over D-Bus
+ *
+ * @template {!WithDBusClient} T
+ * @param {string} object_path - object path
+ * @param {T} superclass - superclass to extend
+ */
+const WithStatus = (superclass, object_path) => class extends superclass {
+  /**
+   * Returns the service status
+   *
+   * @return {Promise<number>} 0 for idle, 1 for busy
+   */
+  async getStatus() {
+    const proxy = await this.client.proxy(STATUS_IFACE, object_path);
+    return proxy.Current;
+  }
+
+  /**
+   * Register a callback to run when the "CurrentInstallationPhase" changes
+   *
+   * @param {function} handler - callback function
+   * @return {function} function to disable the callback
+   */
+  onStatusChange(handler) {
+    return this.client.onObjectChanged(object_path, STATUS_IFACE, (changes) => {
+      if ("Current" in changes) {
+        handler(changes.Current.v);
+      }
+    });
+  }
 };
 
 const PROGRESS_IFACE = "org.opensuse.DInstaller.Progress1";
 
-const withProgress = (object_path) => {
-  return {
-    /**
-     * Returns the service progress
-     *
-     * @return {Promise.<object>} an object containing the total steps,
-     *   the current step and whether the service finished or not.
-     */
-    async getProgress() {
-      const proxy = await this.proxy(PROGRESS_IFACE, object_path);
-      return {
-        total:    proxy.TotalSteps,
-        current:  proxy.CurrentStep[0],
-        message:  proxy.CurrentStep[1],
-        finished: proxy.Finished
-      };
-    },
+/**
+ * Extends the given class with methods go get and track the progress over D-Bus
+ * @param {string} object_path - object_path
+ * @param {T} superclass - superclass to extend
+ * @template {!WithDBusClient} T
+ */
+const WithProgress = (superclass, object_path) => class extends superclass {
+  /**
+   * Returns the service progress
+   *
+   * @return {Promise<Progress>} an object containing the total steps,
+   *   the current step and whether the service finished or not.
+   */
+  async getProgress() {
+    const proxy = await this.client.proxy(PROGRESS_IFACE, object_path);
+    return {
+      total:    proxy.TotalSteps,
+      current:  proxy.CurrentStep[0],
+      message:  proxy.CurrentStep[1],
+      finished: proxy.Finished
+    };
+  }
 
-    /**
-     * Register a callback to run when the progress changes
-     *
-     * @param {function} handler - callback function
-     * @return {function} function to disable the callback
-     */
-    onProgressChange(handler) {
-      return this.onObjectChanged(object_path, PROGRESS_IFACE, (changes) => {
-        const { TotalSteps, CurrentStep, Finished } = changes;
-        if (TotalSteps === undefined && CurrentStep === undefined && Finished === undefined) {
-          return;
-        }
+  /**
+   * Register a callback to run when the progress changes
+   *
+   * @param {ProgressHandler} handler - callback function
+   * @return {import ("./dbus").RemoveFn} function to disable the callback
+   */
+  onProgressChange(handler) {
+    return this.client.onObjectChanged(object_path, PROGRESS_IFACE, (changes) => {
+      const { TotalSteps, CurrentStep, Finished } = changes;
+      if (TotalSteps === undefined && CurrentStep === undefined && Finished === undefined) {
+        return;
+      }
 
-        this.getProgress().then(handler);
-      });
-    }
-  };
+      this.getProgress().then(handler);
+    });
+  }
 };
 
-/**
- * Utility method for applying mixins to given object
- *
- * @param {Object} klass - target object
- * @param {...function} fn - function(s) to be copied to given object prototype
- */
-const applyMixin = (klass, ...fn) => Object.assign(klass.prototype, ...fn);
-
-export { applyMixin, withDBus, withStatus, withProgress };
+export { WithStatus, WithProgress };
