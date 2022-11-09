a32d72aab7b132ab06d51918cca9ac742d2c24aa
Martin Vidner <mvidner@suse.cz>
2022-11-09 11:24:54 +0100

    [service] Set up D-Bus service activation in setup.sh

--- setup.sh
+++ setup.sh
@@ -9,7 +9,17 @@ sudo zypper --non-interactive install gcc gcc-c++ make openssl-devel ruby-devel 
 sudo systemctl start cockpit
 
 # set up the d-installer service
-sudo cp service/share/dbus.conf /usr/share/dbus-1/system.d/org.opensuse.DInstaller.conf
+MYDIR=$(realpath $(dirname $0))
+sudo cp -v $MYDIR/service/share/dbus.conf /usr/share/dbus-1/system.d/org.opensuse.DInstaller.conf
+(
+  # D-Bus service activation
+  cd $MYDIR/service/share
+  DBUSDIR=/usr/share/dbus-1/system-services
+  for SVC in org.opensuse.DInstaller*.service; do
+    echo '->' $DBUSDIR/$SVC
+    sed -e "s@Exec=/usr/bin/@Exec=$MYDIR/service/bin/@" $SVC | sudo tee $DBUSDIR/$SVC > /dev/null
+  done
+)
 cd service; bundle config set --local path 'vendor/bundle'; bundle install; cd -
 
 # set up the web UI
