0357de74b5fc8bf3017804d8f9e9f956b9de535b
Imobach González Sosa <igonzalezsosa@suse.com>
2022-12-15 12:47:44 +0000

    [service] Implement the ImportGpgCallback

--- service/test/dinstaller/software/callbacks/signature_test.rb
+++ service/test/dinstaller/software/callbacks/signature_test.rb
@@ -86,10 +86,48 @@ describe DInstaller::Software::Callbacks::Signature do
     end
   end
 
-      expect(subject).to receive(:ask) do |question|
-        expect(question.text).to include("repomd.xml")
+  describe "import_gpg_key" do
+    let(:asked_question) do
+      instance_double(DInstaller::Question, text: "Better safe than sorry", answer: answer)
+    end
+
+    let(:answer) { :Trust }
+
+    let(:key) do
+      {
+        "id"          => "0123456789ABCDEF",
+        "fingerprint" => "2E2EA448C9DDD7A91BC28441AEE969E90F05DB9D",
+        "name"        => "YaST:Head:D-Installer"
+      }
+    end
+
+    before do
+      allow(subject).to receive(:ask).and_yield(asked_question)
+    end
+
+    context "when the user answers :Trust" do
+      let(:answer) { :Trust }
+
+      it "returns true" do
+        expect(subject.import_gpg_key(key, 1)).to eq(true)
       end
-      expect(subject.accept_unsigned_file("repomd.xml", 1))
+    end
+
+    context "when the user answers :Skip" do
+      let(:answer) { :Skip }
+
+      it "returns false" do
+        expect(subject.import_gpg_key(key, 1)).to eq(false)
+      end
+    end
+
+    it "includes a message" do
+      expect(subject).to receive(:ask) do |question|
+        expect(question.text).to include(key["id"])
+        expect(question.text).to include(key["name"])
+        expect(question.text).to include("2E2E A448 C9DD")
+      end
+      subject.import_gpg_key(key, 1)
     end
   end
 end
