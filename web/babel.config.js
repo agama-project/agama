0f165f437f9b1288fdb50515b6f1ac73c1f8f37f
David Díaz González <dgonzalez@suse.de>
2023-02-09 15:49:48 +0000

    [web] Use react-refresh/babel plugin only in development mode
    To avoid problems/complains in "test" mode.

--- web/babel.config.js
+++ web/babel.config.js
@@ -1,9 +1,16 @@
+const { NODE_ENV } = process.env;
+
+const presets = [
+  '@babel/preset-react',
+  ['@babel/preset-env', { targets: { node: 'current' } }]
+];
+const plugins = [];
+
+if (NODE_ENV === 'development') {
+  plugins.push('react-refresh/babel');
+}
+
 module.exports = {
-  presets: [
-    '@babel/preset-react',
-    ['@babel/preset-env', {targets: {node: 'current'}}]
-  ],
-  plugins: [
-    'react-refresh/babel',
-  ]
+  presets,
+  plugins
 };
