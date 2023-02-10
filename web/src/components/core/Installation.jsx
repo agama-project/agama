11cca006d63ebe535c27a7ae71a6322a4c7040e4
Imobach González Sosa <igonzalezsosa@suse.com>
2023-02-10 17:28:47 +0000

    [web] Avoid redrawing the App when the status changes

---
+++ web/src/components/core/Installation.jsx
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) [2023] SUSE LLC
+ *
+ * All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of version 2 of the GNU General Public License as published
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, contact SUSE LLC.
+ *
+ * To contact SUSE LLC about this file by physical or electronic mail, you may
+ * find current contact information at www.suse.com.
+ */
+
+import React, { useEffect, useState } from "react";
+import { useInstallerClient } from "~/context/installer";
+import { InstallationProgress, InstallationFinished } from "~/components/core";
+import { IDLE } from "~/client/status";
+
+function Installation() {
+  const client = useInstallerClient();
+  const [status, setStatus] = useState(undefined);
+
+  useEffect(() =>
+    client.manager.onStatusChange(setStatus), [client.manager]
+  );
+
+  return (status === IDLE) ? <InstallationFinished /> : <InstallationProgress />;
+}
+
+export default Installation;
