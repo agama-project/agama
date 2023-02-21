3e212c5107a80bfcc1dc22a697f8a4de2fc33a05
David Díaz González <dgonzalez@suse.de>
2023-02-21 01:24:02 +0000

    [web] Simplify SVG mock

--- web/__mocks__/svg.js
+++ web/__mocks__/svg.js
@@ -1,10 +1,8 @@
 import React from 'react';
 
-export default (props) => {
-  return (
-    // Simple SVG square based on a wikimedia example https://commons.wikimedia.org/wiki/SVG_examples
-    <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="48" height="48" {...props}>
-      <rect x="0" y="0" width="48" height="48" />
-    </svg>
-  );
-}
+export default ({...props}) => (
+  // Simple SVG square based on a wikimedia example https://commons.wikimedia.org/wiki/SVG_examples
+  <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="48" height="48" {...props}>
+    <rect x="0" y="0" width="48" height="48" />
+  </svg>
+);
