268e488976d18ea6bfd8286b9f9e59e770ec28a6
David Díaz González <dgonzalez@suse.de>
2023-03-06 12:25:22 +0000

    [web] Adapt ProposalTargeSection
    To render a more appropiate content when no candidate devices available.

--- web/src/components/storage/ProposalTargetSection.test.jsx
+++ web/src/components/storage/ProposalTargetSection.test.jsx
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) [2022] SUSE LLC
+ * Copyright (c) [2022-2023] SUSE LLC
  *
  * All Rights Reserved.
  *
@@ -36,70 +36,102 @@ const FakeProposalTargetForm = ({ id, onSubmit }) => {
 jest.mock("~/components/storage/ProposalSummary", () => mockComponent("Proposal summary"));
 jest.mock("~/components/storage/ProposalTargetForm", () => FakeProposalTargetForm);
 
-const proposal = {
-  availableDevices: [{ id: "/dev/sda", label: "/dev/sda, 500 GiB" }],
-  candidateDevices: ["/dev/sda"],
-  lvm: false
-};
+let proposal;
 
-it("renders the proposal summary", () => {
-  installerRender(<ProposalTargetSection proposal={proposal} />);
+describe("when there are candidate devices available", () => {
+  beforeEach(() => {
+    proposal = {
+      availableDevices: [{ id: "/dev/sda", label: "/dev/sda, 500 GiB" }],
+      result : {
+        candidateDevices: ["/dev/sda"],
+        lvm: false
+      }
+    };
+  });
 
-  screen.getByText("Proposal summary");
-});
+  it("renders the proposal summary", () => {
+    installerRender(<ProposalTargetSection proposal={proposal} />);
 
-it("renders an icon for configuring the candidate devices", () => {
-  installerRender(<ProposalTargetSection proposal={proposal} />);
+    screen.getByText("Proposal summary");
+  });
 
-  screen.getByRole("button", { name: "Section settings" });
-});
+  it("renders an icon for configuring the candidate devices", () => {
+    installerRender(<ProposalTargetSection proposal={proposal} />);
 
-it("does not show the popup by default", async () => {
-  installerRender(<ProposalTargetSection proposal={proposal} />);
+    screen.getByRole("button", { name: "Section settings" });
+  });
 
-  await waitFor(() => {
+  it("does not show the popup by default", async () => {
+    installerRender(<ProposalTargetSection proposal={proposal} />);
+
+    await waitFor(() => {
+      expect(screen.queryByRole("dialog")).not.toBeInTheDocument();
+    });
+  });
+
+  it("shows the popup with the form when the icon is clicked", async () => {
+    const { user } = installerRender(<ProposalTargetSection proposal={proposal} />);
+
+    const button = screen.getByRole("button", { name: "Section settings" });
+    await user.click(button);
+
+    await screen.findByRole("dialog");
+    screen.getByRole("form", { name: "Target form" });
+  });
+
+  it("closes the popup without submitting the form when cancel is clicked", async () => {
+    const calculateFn = jest.fn();
+
+    const { user } = installerRender(<ProposalTargetSection proposal={proposal} calculateProposal={calculateFn} />);
+
+    const button = screen.getByRole("button", { name: "Section settings" });
+    await user.click(button);
+
+    const popup = await screen.findByRole("dialog");
+    const cancel = within(popup).getByRole("button", { name: "Cancel" });
+    await user.click(cancel);
+
     expect(screen.queryByRole("dialog")).not.toBeInTheDocument();
+    expect(calculateFn).not.toHaveBeenCalled();
+  });
+
+  it("closes the popup and submits the form when accept is clicked", async () => {
+    const calculateFn = jest.fn();
+
+    const { user } = installerRender(<ProposalTargetSection proposal={proposal} calculateProposal={calculateFn} />);
+
+    const button = screen.getByRole("button", { name: "Section settings" });
+    await user.click(button);
+
+    const popup = await screen.findByRole("dialog");
+    const accept = within(popup).getByRole("button", { name: "Accept" });
+    await user.click(accept);
+
+    expect(screen.queryByRole("dialog")).not.toBeInTheDocument();
+    expect(calculateFn).toHaveBeenCalled();
   });
 });
 
-it("shows the popup with the form when the icon is clicked", async () => {
-  const { user } = installerRender(<ProposalTargetSection proposal={proposal} />);
+describe("when there are no candidate devices available", () => {
+  beforeEach(() => {
+    proposal = {
+      result: {
+        candidateDevices: []
+      },
+      availableDevices: []
+    };
+  });
 
-  const button = screen.getByRole("button", { name: "Section settings" });
-  await user.click(button);
+  it("renders an informative message", () => {
+    installerRender(<ProposalTargetSection proposal={proposal} />);
 
-  await screen.findByRole("dialog");
-  screen.getByRole("form", { name: "Target form" });
-});
+    screen.getByText("No available devices");
+  });
 
-it("closes the popup without submitting the form when cancel is clicked", async () => {
-  const calculateFn = jest.fn();
+  it("does not render an icon for configuring the candidate devices", () => {
+    installerRender(<ProposalTargetSection proposal={proposal} />);
 
-  const { user } = installerRender(<ProposalTargetSection proposal={proposal} calculateProposal={calculateFn} />);
-
-  const button = screen.getByRole("button", { name: "Section settings" });
-  await user.click(button);
-
-  const popup = await screen.findByRole("dialog");
-  const cancel = within(popup).getByRole("button", { name: "Cancel" });
-  await user.click(cancel);
-
-  expect(screen.queryByRole("dialog")).not.toBeInTheDocument();
-  expect(calculateFn).not.toHaveBeenCalled();
-});
-
-it("closes the popup and submits the form when accept is clicked", async () => {
-  const calculateFn = jest.fn();
-
-  const { user } = installerRender(<ProposalTargetSection proposal={proposal} calculateProposal={calculateFn} />);
-
-  const button = screen.getByRole("button", { name: "Section settings" });
-  await user.click(button);
-
-  const popup = await screen.findByRole("dialog");
-  const accept = within(popup).getByRole("button", { name: "Accept" });
-  await user.click(accept);
-
-  expect(screen.queryByRole("dialog")).not.toBeInTheDocument();
-  expect(calculateFn).toHaveBeenCalled();
+    const actionButton = screen.queryByRole("button", { name: "Section settings" });
+    expect(actionButton).toBeNull();
+  });
 });
