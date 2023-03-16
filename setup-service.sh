5b5f127472234d0c11a5919e1d138b77ef5112ff
Martin Vidner <mvidner@suse.cz>
2023-03-16 14:37:57 +0100

    setup.sh: split off setup-service.sh to be used by CLI and CI tests

---
+++ setup-service.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+
+# Using a git checkout in the current directory,
+# set up the service (backend) part of d-installer
+# so that it can be used by
+# - the web frontend (as set up by setup.sh)
+# - the CLI
+# or both
+
+# Exit on error; unset variables are an error
+set -eu
+
+MYDIR=$(realpath $(dirname $0))
+
+# Helper:
+# Ensure root privileges for the installation.
+# In a testing container, we are root but there is no sudo.
+if [ $(id --user) != 0 ]; then
+  SUDO=sudo
+  if [ $($SUDO id --user) != 0 ]; then
+    echo "We are not root and cannot sudo, cannot continue."
+    exit 1
+  fi
+else
+  SUDO=""
+fi
+
+# Helper:
+# Like "sed -e $1 < $2 > $3" but $3 is a system file owned by root
+sudosed() {
+  echo "$2 -> $3"
+  sed -e "$1" "$2" | $SUDO tee "$3" > /dev/null
+}
+
+# - D-Bus configuration
+$SUDO cp -v $MYDIR/service/share/dbus.conf /usr/share/dbus-1/d-installer.conf
+
+# - D-Bus activation configuration
+#   (this could be left out but then we would rely
+#    on the manual startup via bin/d-installer)
+(
+  cd $MYDIR/service/share
+  DBUSDIR=/usr/share/dbus-1/d-installer-services
+  mkdir -p $DBUSDIR
+  for SVC in org.opensuse.DInstaller*.service; do
+    sudosed "s@\(Exec\)=/usr/bin/@\1=$MYDIR/service/bin/@" $SVC $DBUSDIR/$SVC
+  done
+  sudosed "s@\(ExecStart\)=/usr/bin/@\1=$MYDIR/service/bin/@" \
+          systemd.service /usr/lib/systemd/system/d-installer.service
+  $SUDO systemctl daemon-reload
+)
+
+# - Install the service dependencies
+(
+  cd $MYDIR/service
+  bundle config set --local path 'vendor/bundle'
+  bundle install
+)
+
--- setup.sh
+++ setup.sh
@@ -3,6 +3,8 @@
 # This script sets up the development environment without installing any
 # package. This script is supposed to run within a repository clone.
 
+MYDIR=$(realpath $(dirname $0))
+
 # Helper:
 # Ensure root privileges for the installation.
 # In a testing container, we are root but there is no sudo.
@@ -21,42 +23,9 @@ fi
 $SUDO zypper --non-interactive install gcc gcc-c++ make openssl-devel ruby-devel \
   'npm>=18' git augeas-devel cockpit jemalloc-devel || exit 1
 
-# Helper:
-# Like "sed -e $1 < $2 > $3" but $3 is a system file owned by root
-sudosed() {
-  echo "$2 -> $3"
-  sed -e "$1" "$2" | $SUDO tee "$3" > /dev/null
-}
-
-MYDIR=$(realpath $(dirname $0))
-
 # Backend setup
 
-# - D-Bus configuration
-$SUDO cp -v $MYDIR/service/share/dbus.conf /usr/share/dbus-1/d-installer.conf
-
-# - D-Bus activation configuration
-#   (this could be left out but then we would rely
-#    on the manual startup via bin/d-installer)
-(
-  cd $MYDIR/service/share
-  DBUSDIR=/usr/share/dbus-1/d-installer-services
-  mkdir -p $DBUSDIR
-  for SVC in org.opensuse.DInstaller*.service; do
-    sudosed "s@\(Exec\)=/usr/bin/@\1=$MYDIR/service/bin/@" $SVC $DBUSDIR/$SVC
-  done
-  sudosed "s@\(ExecStart\)=/usr/bin/@\1=$MYDIR/service/bin/@" \
-          systemd.service /usr/lib/systemd/system/d-installer.service
-  $SUDO systemctl daemon-reload
-)
-
-# - Install the service dependencies
-(
-  cd $MYDIR/service
-  bundle config set --local path 'vendor/bundle'
-  bundle install
-)
-
+$MYDIR/setup-service.sh
 
 # Web Frontend
 
