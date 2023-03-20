20d51dc82db120ed7b801343d2884aeb229802b2
Imobach González Sosa <igonzalezsosa@suse.com>
2023-03-20 13:19:20 +0000

    Fix ProposalTargetSection test

--- web/src/components/storage/ProposalTargetSection.test.jsx
+++ web/src/components/storage/ProposalTargetSection.test.jsx
@@ -109,7 +109,7 @@ describe("when there are no candidate devices available", () => {
   it("renders an informative message", () => {
     installerRender(<ProposalTargetSection proposal={proposal} />);
 
-    screen.getByText("No available devices");
+    screen.getByText("No devices found");
   });
 
   it("does not allow configuring the candidate devices", () => {
