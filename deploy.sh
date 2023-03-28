a1390e4fb775cb5a237d9662b90e6ae856f199e7
Imobach González Sosa <igonzalezsosa@suse.com>
2023-03-28 17:58:24 +0100

    Adapt shell scripts to use Agama

--- deploy.sh
+++ deploy.sh
@@ -4,17 +4,17 @@
 
 sudo rpm --import https://build.opensuse.org/projects/YaST/public_key
 sudo zypper ar -f \
-	https://download.opensuse.org/repositories/YaST:/Head:/D-Installer/openSUSE_Tumbleweed/YaST:Head:Agama.repo
+	https://download.opensuse.org/repositories/YaST:/Head:/Agama/openSUSE_Tumbleweed/YaST:Head:Agama.repo
 RUBY_VERSION=ruby:$(rpm --eval '%{rb_ver}')
 sudo zypper --non-interactive in --no-recommends \
-	"rubygem($RUBY_VERSION:d-installer)" \
-	cockpit-d-installer \
+	"rubygem($RUBY_VERSION:agama)" \
+	cockpit-agama \
 	cockpit
 
 sudo systemctl start cockpit
-sudo systemctl start d-installer
+sudo systemctl start agama
 
 # set 'linux' as password
 echo "linux:Nk1RhI1GqlxdA" | sudo chpasswd -e linux
 
-xdg-open http://localhost:9090/cockpit/@localhost/d-installer/index.html
+xdg-open http://localhost:9090/cockpit/@localhost/agama/index.html
