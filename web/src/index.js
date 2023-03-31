434dc8c5c8616eac2d670e0eaa927e969c30fd04
David Díaz González <dgonzalez@suse.de>
2023-03-31 15:33:22 +0100

    [web] Fix CSS imports
    For ensuring our CSS overrides are imported at the very end.

--- web/src/index.js
+++ web/src/index.js
@@ -29,8 +29,11 @@ import { InstallerClientProvider } from "~/context/installer";
 import { SoftwareProvider } from "~/context/software";
 import { createClient } from "~/client";
 
+/**
+ * Import PF4 base styles before any JSX since components coming from PF4 may
+ * import styles dependent on variables and rules previously defined there.
+ */
 import "@patternfly/patternfly/patternfly-base.scss";
-import "~/assets/styles/index.scss";
 
 import App from "~/App";
 import Main from "~/Main";
@@ -41,6 +44,18 @@ import { ProposalPage as StoragePage, DASDPage, ISCSIPage } from "~/components/s
 import { UsersPage } from "~/components/users";
 import { L10nPage } from "~/components/l10n";
 import { NetworkPage } from "~/components/network";
+
+/**
+ * As JSX components might import CSS stylesheets, our styles must be imported
+ * after them to preserve our overrides (e.g., PF4 overrides).
+ *
+ * Because mini-css-extract-plugin maintains the order of the imported CSS,
+ * adding the overrides here ensures the overrides will be placed appropriately
+ * at the end of our stylesheet when it extracts the CSS from dist/index.js
+ *
+ * See https://github.com/cockpit-project/starter-kit/blob/aeb81718e75b54e5d50ee450fe2abfbcfa58f5e6/src/index.js
+ */
+import "~/assets/styles/index.scss";
 
 // When running in the development server add a special login wrapper which
 // checks whether the user is authenticated. When building the code outside
