ab47c97fd116b34dd67d59c52c88c032b99dc112
Josef Reidinger <jreidinger@suse.cz>
2023-04-13 20:00:22 +0200

    add first struct for serde deserialization

--- rust/agama-locale-data/src/lib.rs
+++ rust/agama-locale-data/src/lib.rs
@@ -1,5 +1,30 @@
+use serde::Deserialize;
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
+}
+
+#[derive(Debug, Deserialize)]
+pub struct RankedLanguage {
+    pub id: String,
+    pub rank: u8
+}
+
+#[derive(Debug, Deserialize)]
+pub struct RankedTerritory {
+    #[serde(rename(deserialize = "territoryId"))]
+    pub id: String,
+    pub rank: u8
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Keyboard {
+    pub id: String,
+    pub description: String,
+    pub ascii: bool,
+    pub comment: String,
+    pub languages: Vec<RankedLanguage>,
+    pub territories: Vec<RankedTerritory>
 }
 
 #[cfg(test)]
