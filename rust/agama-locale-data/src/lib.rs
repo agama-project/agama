475a75576d2b16344788bfe23328806aa3c566d6
Josef Reidinger <jreidinger@suse.cz>
2023-04-14 10:43:42 +0200

    first working code to get list of keyboards

--- rust/agama-locale-data/src/lib.rs
+++ rust/agama-locale-data/src/lib.rs
@@ -1,4 +1,8 @@
 use serde::Deserialize;
+use std::fs::File;
+use std::io::BufReader;
+use quick_xml::de::Deserializer;
+use flate2::bufread::GzDecoder;
 
 pub fn add(left: usize, right: usize) -> usize {
     left + right
@@ -6,25 +10,50 @@ pub fn add(left: usize, right: usize) -> usize {
 
 #[derive(Debug, Deserialize)]
 pub struct RankedLanguage {
+    #[serde(rename(deserialize = "languageId"))]
     pub id: String,
-    pub rank: u8
+    pub rank: u16
+}
+
+#[derive(Debug, Deserialize)]
+pub struct RankedLanguages {
+    pub language: Option<Vec<RankedLanguage>>
 }
 
 #[derive(Debug, Deserialize)]
 pub struct RankedTerritory {
     #[serde(rename(deserialize = "territoryId"))]
     pub id: String,
-    pub rank: u8
+    pub rank: u16
+}
+
+#[derive(Debug, Deserialize)]
+pub struct RankedTerritories {
+    pub territory: Option<Vec<RankedTerritory>>
 }
 
 #[derive(Debug, Deserialize)]
 pub struct Keyboard {
+    #[serde(rename(deserialize = "keyboardId"))]
     pub id: String,
     pub description: String,
     pub ascii: bool,
-    pub comment: String,
-    pub languages: Vec<RankedLanguage>,
-    pub territories: Vec<RankedTerritory>
+    pub comment: Option<String>,
+    pub languages: RankedLanguages,
+    pub territories: RankedTerritories
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Keyboards {
+    pub keyboard: Vec<Keyboard>
+}
+
+pub fn get_keyboards() -> Keyboards {
+    const FILE_PATH: &str = "/usr/share/langtable/data/keyboards.xml.gz";
+    let file = File::open(FILE_PATH).expect("Failed to read langtable-data.");
+    let reader = BufReader::new(GzDecoder::new(BufReader::new(&file)));
+    let mut deserializer = Deserializer::from_reader(reader);
+    Keyboards::deserialize(&mut deserializer).expect("Failed to deserialize keyboard entry")
 }
 
 #[cfg(test)]
@@ -33,7 +62,9 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let result = get_keyboards();
+        assert_eq!(result.keyboard.len(), 247);
+        let first = result.keyboard.first().expect("no keyboards");
+        assert_eq!(first.id, "ad")
     }
 }
