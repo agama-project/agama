bd572419ed1c2241fef980b5168ac810d2e05072
Ladislav Slezák <lslezak@suse.cz>
2023-04-27 10:23:25 +0200

    Added unit tests

---
+++ web/src/components/core/Description.test.jsx
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) [2023] SUSE LLC
+ *
+ * All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of version 2 of the GNU General Public License as published
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, contact SUSE LLC.
+ *
+ * To contact SUSE LLC about this file by physical or electronic mail, you may
+ * find current contact information at www.suse.com.
+ */
+
+import React from "react";
+import { screen } from "@testing-library/react";
+import { plainRender } from "~/test-utils";
+import { Description } from "~/components/core";
+
+describe("Description", () => {
+  const description = "Some great description";
+  const item = "Item with description";
+
+  it("displays the description after clicking the object", async () => {
+    const { user } = plainRender(<Description description={description}>{item}</Description>);
+
+    // the description is not displayed just after the render
+    expect(screen.queryByText(description)).not.toBeInTheDocument();
+    expect(screen.queryByRole("dialog")).not.toBeInTheDocument();
+
+    // click it
+    const item_node = screen.getByText(item);
+    await user.click(item_node);
+
+    // then the description is visible in a dialog
+    screen.getByRole("dialog");
+    screen.getByText(description);
+  });
+
+  const empty_tester = async (subject) => {
+    const { user } = plainRender(subject);
+
+    const item_node = screen.getByText(item);
+    await user.click(item_node);
+
+    // do not display empty popup
+    expect(screen.queryByRole("dialog")).not.toBeInTheDocument();
+  };
+
+  it("displays the object without description when it is undefined", async () => {
+    empty_tester(<Description>{item}</Description>);
+  });
+
+  it("displays the object without description when it is null", async () => {
+    empty_tester(<Description description={null}>{item}</Description>);
+  });
+
+  it("displays the object without description when it is empty", async () => {
+    empty_tester(<Description description="">{item}</Description>);
+  });
+});
