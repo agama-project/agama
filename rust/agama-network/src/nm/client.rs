7aaf53fb063a15213779095a569777a7a494440f
Imobach González Sosa <igonzalezsosa@suse.com>
2023-05-09 11:25:05 +0100

    Test the conversion from D-Bus to the NetworkManager model

--- rust/agama-network/src/nm/client.rs
+++ rust/agama-network/src/nm/client.rs
@@ -59,82 +59,166 @@ impl<'a> NetworkManagerClient<'a> {
                 .build()
                 .await?;
             let settings = proxy.get_settings().await?;
-            if let Some(connection) = self.connection_from_dbus(settings) {
+            if let Some(connection) = connection_from_dbus(settings) {
                 connections.push(connection);
             }
         }
         Ok(connections)
     }
+}
 
-    fn connection_from_dbus(
-        &self,
-        conn: HashMap<String, HashMap<String, zvariant::OwnedValue>>,
-    ) -> Option<NmConnection> {
-        let mut nm_connection = NmConnection::default();
+fn connection_from_dbus(
+    conn: HashMap<String, HashMap<String, zvariant::OwnedValue>>,
+) -> Option<NmConnection> {
+    let Some(connection) = conn.get("connection") else {
+        return None;
+    };
 
-        if let Some(connection) = conn.get("connection") {
-            let id: &str = connection.get("id")?.downcast_ref()?;
-            nm_connection.id = id.to_string();
+    let mut nm_connection = NmConnection::default();
+    let id: &str = connection.get("id")?.downcast_ref()?;
+    nm_connection.id = id.to_string();
+
+    if let Some(connection) = conn.get("connection") {
+        let id: &str = connection.get("id")?.downcast_ref()?;
+        nm_connection.id = id.to_string();
+    }
+
+    if let Some(ipv4) = conn.get("ipv4") {
+        let method: &str = ipv4.get("method")?.downcast_ref()?;
+        let address_data = ipv4.get("address-data")?;
+        let address_data = address_data.downcast_ref::<zbus::zvariant::Array>()?;
+        let mut addresses: Vec<(String, u32)> = vec![];
+        for addr in address_data.get() {
+            let dict = addr.downcast_ref::<zvariant::Dict>()?;
+            let map = <HashMap<String, zvariant::Value<'_>>>::try_from(dict.clone()).unwrap();
+            let addr_str: &str = map.get("address")?.downcast_ref()?;
+            let prefix: &u32 = map.get("prefix")?.downcast_ref()?;
+            addresses.push((addr_str.to_string(), *prefix))
+        }
+        let mut nm_ipv4 = NmIp4Config {
+            method: NmMethod(method.to_string()),
+            addresses,
+            ..Default::default()
+        };
+
+        if let Some(dns_data) = ipv4.get("dns-data") {
+            let dns_data = dns_data.downcast_ref::<zbus::zvariant::Array>()?;
+            for server in dns_data.get() {
+                let server: &str = server.downcast_ref()?;
+                nm_ipv4.nameservers.push(server.to_string());
+            }
         }
 
-        if let Some(ipv4) = conn.get("ipv4") {
-            let method: &str = ipv4.get("method")?.downcast_ref()?;
-            let address_data = ipv4.get("address-data")?;
-            let address_data = address_data.downcast_ref::<zbus::zvariant::Array>()?;
-            let mut addresses: Vec<(String, u32)> = vec![];
-            for addr in address_data.get() {
-                let dict = addr.downcast_ref::<zvariant::Dict>()?;
-                let map = <HashMap<String, zvariant::Value<'_>>>::try_from(dict.clone()).unwrap();
-                let addr_str: &str = map.get("address")?.downcast_ref()?;
-                let prefix: &u32 = map.get("prefix")?.downcast_ref()?;
-                addresses.push((addr_str.to_string(), *prefix))
-            }
-            let mut nm_ipv4 = NmIp4Config {
-                method: NmMethod(method.to_string()),
-                addresses,
-                ..Default::default()
-            };
-
-            if let Some(dns_data) = ipv4.get("dns-data") {
-                dbg!(&dns_data);
-                let dns_data = dns_data.downcast_ref::<zbus::zvariant::Array>()?;
-                for server in dns_data.get() {
-                    let server: &str = server.downcast_ref()?;
-                    nm_ipv4.nameservers.push(server.to_string());
-                }
-            }
-
-            if let Some(gateway) = ipv4.get("gateway") {
-                let gateway: &str = gateway.downcast_ref()?;
-                nm_ipv4.gateway = Some(gateway.to_string());
-            }
-
-            nm_connection.ipv4 = Some(nm_ipv4);
+        if let Some(gateway) = ipv4.get("gateway") {
+            let gateway: &str = gateway.downcast_ref()?;
+            nm_ipv4.gateway = Some(gateway.to_string());
         }
 
-        if let Some(wireless) = conn.get("802-11-wireless") {
-            let mode: &str = wireless.get("mode")?.downcast_ref()?;
-            let ssid = wireless.get("ssid")?;
-            let ssid: &zvariant::Array = ssid.downcast_ref()?;
-            let ssid: Vec<u8> = ssid
-                .get()
-                .iter()
-                .map(|u| *u.downcast_ref::<u8>().unwrap())
-                .collect();
-            let mut wireless_settings = NmWireless {
-                mode: mode.into(),
-                ssid,
-                ..Default::default()
-            };
+        nm_connection.ipv4 = Some(nm_ipv4);
+    }
 
-            if let Some(security) = conn.get("802-11-wireless-security") {
-                let key_mgmt: &str = security.get("key-mgmt")?.downcast_ref()?;
-                wireless_settings.key_mgmt = key_mgmt.into();
-            }
+    if let Some(wireless) = conn.get("802-11-wireless") {
+        let mode: &str = wireless.get("mode")?.downcast_ref()?;
+        let ssid = wireless.get("ssid")?;
+        let ssid: &zvariant::Array = ssid.downcast_ref()?;
+        let ssid: Vec<u8> = ssid
+            .get()
+            .iter()
+            .map(|u| *u.downcast_ref::<u8>().unwrap())
+            .collect();
+        let mut wireless_settings = NmWireless {
+            mode: mode.into(),
+            ssid,
+            ..Default::default()
+        };
 
-            nm_connection.wireless = Some(wireless_settings);
+        if let Some(security) = conn.get("802-11-wireless-security") {
+            let key_mgmt: &str = security.get("key-mgmt")?.downcast_ref()?;
+            wireless_settings.key_mgmt = key_mgmt.into();
         }
 
-        Some(nm_connection)
+        nm_connection.wireless = Some(wireless_settings);
+    }
+
+    Some(nm_connection)
+}
+
+#[cfg(test)]
+mod test {
+    use super::connection_from_dbus;
+    use crate::nm::{NmKeyManagement, NmMethod, NmWirelessMode};
+    use std::collections::HashMap;
+    use zbus::zvariant::{OwnedValue, Value};
+
+    #[test]
+    fn test_connection_from_dbus() {
+        let connection_section = HashMap::from([("id".to_string(), Value::new("eth0").to_owned())]);
+
+        let address_data = vec![HashMap::from([
+            ("address".to_string(), Value::new("192.168.0.10")),
+            ("prefix".to_string(), Value::new(24 as u32)),
+        ])];
+
+        let ipv4_section = HashMap::from([
+            ("method".to_string(), Value::new("auto").to_owned()),
+            (
+                "address-data".to_string(),
+                Value::new(address_data).to_owned(),
+            ),
+            ("gateway".to_string(), Value::new("192.168.0.1").to_owned()),
+            (
+                "dns-data".to_string(),
+                Value::new(vec!["192.168.0.2"]).to_owned(),
+            ),
+        ]);
+
+        let dbus_conn = HashMap::from([
+            ("connection".to_string(), connection_section),
+            ("ipv4".to_string(), ipv4_section),
+        ]);
+
+        let nm_connection = connection_from_dbus(dbus_conn).unwrap();
+
+        assert_eq!(nm_connection.id, "eth0");
+        let nm_ipv4 = nm_connection.ipv4.unwrap();
+        assert_eq!(nm_ipv4.addresses, vec![("192.168.0.10".to_string(), 24)]);
+        assert_eq!(nm_ipv4.nameservers, vec!["192.168.0.2"]);
+        assert_eq!(nm_ipv4.method, NmMethod("auto".to_string()));
+    }
+
+    #[test]
+    fn test_connection_from_dbus_missing_connection() {
+        let dbus_conn: HashMap<String, HashMap<String, OwnedValue>> = HashMap::new();
+        let nm_connection = connection_from_dbus(dbus_conn);
+        assert_eq!(nm_connection, None);
+    }
+
+    #[test]
+    fn test_connection_from_dbus_wireless() {
+        let connection_section =
+            HashMap::from([("id".to_string(), Value::new("wlan0").to_owned())]);
+
+        let wireless_section = HashMap::from([
+            ("mode".to_string(), Value::new("infra").to_owned()),
+            (
+                "ssid".to_string(),
+                Value::new("agama".as_bytes()).to_owned(),
+            ),
+        ]);
+
+        let security_section =
+            HashMap::from([("key-mgmt".to_string(), Value::new("wpa").to_owned())]);
+
+        let dbus_conn = HashMap::from([
+            ("connection".to_string(), connection_section),
+            ("802-11-wireless".to_string(), wireless_section),
+            ("802-11-wireless-security".to_string(), security_section),
+        ]);
+
+        let nm_connection = connection_from_dbus(dbus_conn).unwrap();
+        let wireless = nm_connection.wireless.unwrap();
+        assert_eq!(wireless.ssid, vec![97, 103, 97, 109, 97]);
+        assert_eq!(wireless.mode, NmWirelessMode("infra".to_string()));
+        assert_eq!(wireless.key_mgmt, NmKeyManagement("wpa".to_string()));
     }
 }
