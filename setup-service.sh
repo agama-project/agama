98a87d08d13b73b1ff2176f9a4e5536235e1d635
Imobach González Sosa <igonzalezsosa@suse.com>
2023-06-07 06:45:37 +0100

    Install cargo only when it is missing

--- setup-service.sh
+++ setup-service.sh
@@ -38,9 +38,12 @@ sudosed() {
 test -f /etc/zypp/repos.d/d_l_python.repo || \
   $SUDO zypper --non-interactive \
     addrepo https://download.opensuse.org/repositories/devel:/languages:/python/openSUSE_Tumbleweed/ d_l_python
-$SUDO zypper --non-interactive --gpg-auto-import-keys install cargo gcc gcc-c++ make openssl-devel ruby-devel \
+$SUDO zypper --non-interactive --gpg-auto-import-keys install gcc gcc-c++ make openssl-devel ruby-devel \
   python-langtable-data \
   git augeas-devel jemalloc-devel || exit 1
+
+# only install cargo if it is not available (avoid conflicts with rustup)
+test -f /usr/bin/cargo || $SUDO zypper --non-interactive install cargo
 
 # - Install service rubygem dependencies
 (
