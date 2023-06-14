e34671c94785c025abd72321cc6a7e3f9b1f64ad
Josef Reidinger <jreidinger@suse.cz>
2023-06-14 16:37:28 +0200

    Add initial logging capability to agam-dbus-server

--- rust/agama-dbus-server/src/main.rs
+++ rust/agama-dbus-server/src/main.rs
@@ -4,15 +4,22 @@ pub mod questions;
 
 use agama_network::NetworkService;
 use std::future::pending;
+use systemd_journal_logger::JournalLog;
+use log::LevelFilter;
 
 const ADDRESS: &str = "unix:path=/run/agama/bus";
 
 #[async_std::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    JournalLog::default().install().unwrap();
+    log::set_max_level(LevelFilter::Info);
     // When adding more services here, the order might be important.
     crate::questions::start_service(ADDRESS).await?;
+    log::info!("Started questions interface");
     let _conn = crate::locale::start_service(ADDRESS).await?;
+    log::info!("Started locale interface");
     NetworkService::start(ADDRESS).await?;
+    log::info!("Started network interface");
 
     // Do other things or go to wait forever
     pending::<()>().await;
