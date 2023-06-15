501fc0cfe3c73fdd7f3b7c425b27b9b39cca1436
Josef Reidinger <jreidinger@suse.cz>
2023-06-15 14:45:20 +0200

    Adapt to code reviews

--- rust/agama-dbus-server/src/main.rs
+++ rust/agama-dbus-server/src/main.rs
@@ -4,15 +4,25 @@ pub mod questions;
 
 use agama_network::NetworkService;
 use std::future::pending;
-use systemd_journal_logger::JournalLog;
 use log::LevelFilter;
 
 const ADDRESS: &str = "unix:path=/run/agama/bus";
 
 #[async_std::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    JournalLog::default().install().unwrap();
-    log::set_max_level(LevelFilter::Info);
+    // be smart with logging and log directly to journal if connected to it
+    if systemd_journal_logger::connected_to_journal(){
+        // unwrap here is intentional as we are sure no other logger is active yet
+        systemd_journal_logger::JournalLog::default().install().unwrap();
+        log::set_max_level(LevelFilter::Info); // log only info for journal logger
+    } else {
+        simplelog::TermLogger::init(
+            LevelFilter::Info,  // lets use info, trace provides too much output from libraries
+            simplelog::Config::default(),
+            simplelog::TerminalMode::Stderr, // only stderr output for easier filtering
+            simplelog::ColorChoice::Auto
+        ).unwrap(); // unwrap here as we are sure no other logger active
+    }
     // When adding more services here, the order might be important.
     crate::questions::start_service(ADDRESS).await?;
     log::info!("Started questions interface");
