e44808e0a95b7ff9e0c7c5c470ab5ecf3ebdbd27
Balsa Asanovic <balsaasanovic95@gmail.com>
2023-06-20 23:32:35 +0200

    modified desc of the hook; added noops for return

--- web/src/hooks/useNodeSiblings.js
+++ web/src/hooks/useNodeSiblings.js
@@ -1,15 +1,32 @@
+import { noop } from "~/utils";
+
 /**
- * Returns two functions, one which adds given attributes to siblings
- * of the node passed as the hook parameter, and another one which removes attributes.
+ * Function for adding an attribute to a sibling
+ *
+ * @typedef {function} addAttributeFn
+ * @param {string} attribute - attribute name
+ * @param {*} value - value to set
+ */
+
+/**
+ * Function for removing an attribute from a sibling
+ *
+ * @typedef {function} removeAttributeFn
+ * @param {string} attribute - attribute name
+ */
+
+/**
+ * A hook for working with siblings of the node passed as parameter
+ *
+ * It returns an array with exactly two functions:
+ *   - First for adding given attribute to siblings
+ *   - Second for removing given attributes from siblings
  *
  * @param {HTMLElement} node
- * @returns {Array}
+ * @returns {[addAttributeFn, removeAttributeFn]}
  */
 const useNodeSiblings = (node) => {
-  if (!node) return [
-    (a, b) => { return [a, b] },
-    (a) => { return [a] }
-  ];
+  if (!node) return [noop, noop];
 
   const siblings = [...node.parentNode.children].filter(n => n !== node);
 
