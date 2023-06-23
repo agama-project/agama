070f7891980c113523ee6fb057b80bef1237cc81
Imobach González Sosa <igonzalezsosa@suse.com>
2023-06-23 15:29:38 +0100

    Make Clippy happy

--- rust/agama-lib/src/network/client.rs
+++ rust/agama-lib/src/network/client.rs
@@ -55,7 +55,7 @@ impl<'a> NetworkClient<'a> {
             .build()
             .await?;
 
-        /// TODO: consider using the `IPMethod` struct from `agama-network`.
+        // TODO: consider using the `IPMethod` struct from `agama-network`.
         let method = match ipv4_proxy.method().await? {
             0 => "auto",
             1 => "manual",
