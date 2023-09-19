a763f85496775098e543b6f339410afb8fc34741
José Iván López González <jlopez@suse.com>
2023-09-19 11:54:33 +0100

    [rust] Adapt to new storage D-Bus API
    - Bonus: make lvm and encryption password configurable.

--- rust/agama-lib/src/storage/settings.rs
+++ rust/agama-lib/src/storage/settings.rs
@@ -1,6 +1,6 @@
 //! Representation of the storage settings
 
-use agama_settings::{error::ConversionError, SettingObject, Settings};
+use agama_settings::Settings;
 use serde::{Deserialize, Serialize};
 
 /// Storage settings for installation
@@ -11,34 +11,6 @@ pub struct StorageSettings {
     pub lvm: Option<bool>,
     /// Encryption password for the storage devices (in clear text)
     pub encryption_password: Option<String>,
-    /// Devices to use in the installation
-    #[settings(collection)]
-    pub devices: Vec<Device>,
-}
-
-/// Device to use in the installation
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct Device {
-    /// Device name (e.g., "/dev/sda")
-    pub name: String,
-}
-
-impl From<String> for Device {
-    fn from(value: String) -> Self {
-        Self { name: value }
-    }
-}
-
-impl TryFrom<SettingObject> for Device {
-    type Error = ConversionError;
-
-    fn try_from(value: SettingObject) -> Result<Self, Self::Error> {
-        match value.get("name") {
-            Some(name) => Ok(Device {
-                name: name.clone().try_into()?,
-            }),
-            _ => Err(ConversionError::MissingKey("name".to_string())),
-        }
-    }
+    /// Boot device to use in the installation
+    pub boot_device: Option<String>,
 }
