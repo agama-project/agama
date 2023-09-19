2d74e8bd2d6daca2dc7fb2f9a1f2e9cdb2bb879f
balsa-asanovic <balsaasanovic95@gmail.com>
2023-09-19 20:46:35 +0200

    removed split prop and div wrapper for it reverted to using FormGroup

--- web/src/components/core/PasswordAndConfirmationInput.jsx
+++ web/src/components/core/PasswordAndConfirmationInput.jsx
@@ -20,10 +20,11 @@
  */
 
 import React, { useState } from "react";
+import { FormGroup } from "@patternfly/react-core";
 import { PasswordInput } from "~/components/core";
 import { _ } from "~/i18n";
 
-const PasswordAndConfirmationInput = ({ value, onChange, onValidation, isDisabled, split = false }) => {
+const PasswordAndConfirmationInput = ({ value, onChange, onValidation, isDisabled }) => {
   const [confirmation, setConfirmation] = useState(value || "");
   const [error, setError] = useState("");
 
@@ -51,32 +52,36 @@ const PasswordAndConfirmationInput = ({ value, onChange, onValidation, isDisable
   };
 
   return (
-    <div className={split ? "split" : "stack"}>
-      <PasswordInput
-        id="password"
-        name="password"
-        fieldId="password"
-        label={_("Password")}
-        ariaLabel={_("User password")}
-        value={value}
-        isDisabled={isDisabled}
-        onChange={onChangeValue}
-        onBlur={() => validate(value, confirmation)}
-      />
-      <PasswordInput
-        id="passwordConfirmation"
-        name="passwordConfirmation"
+    <>
+      <FormGroup fieldId="password" label={_("Password")}>
+        <PasswordInput
+          id="password"
+          name="password"
+          aria-label={_("User password")}
+          value={value}
+          isDisabled={isDisabled}
+          onChange={onChangeValue}
+          onBlur={() => validate(value, confirmation)}
+        />
+      </FormGroup>
+      <FormGroup
         fieldId="passwordConfirmation"
         label={_("Password confirmation")}
         helperTextInvalid={error}
-        ariaLabel={_("User password confirmation")}
-        value={confirmation}
-        isDisabled={isDisabled}
-        onChange={onChangeConfirmation}
-        onBlur={() => validate(value, confirmation)}
         validated={error === "" ? "default" : "error"}
-      />
-    </div>
+      >
+        <PasswordInput
+          id="passwordConfirmation"
+          name="passwordConfirmation"
+          aria-label={_("User password confirmation")}
+          value={confirmation}
+          isDisabled={isDisabled}
+          onChange={onChangeConfirmation}
+          onBlur={() => validate(value, confirmation)}
+          validated={error === "" ? "default" : "error"}
+        />
+      </FormGroup>
+    </>
   );
 };
 
