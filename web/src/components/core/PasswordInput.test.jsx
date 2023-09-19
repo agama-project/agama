107e20f1f15812fc16815499912c8b50f98a2073
balsa-asanovic <balsaasanovic95@gmail.com>
2023-09-19 20:48:44 +0200

    adjusted test considering simplifed component

--- web/src/components/core/PasswordInput.test.jsx
+++ web/src/components/core/PasswordInput.test.jsx
@@ -20,62 +20,41 @@
  */
 
 import React from "react";
-import { screen, fireEvent } from "@testing-library/react";
+import { screen } from "@testing-library/react";
 import { plainRender } from "~/test-utils";
 import userEvent from "@testing-library/user-event";
 import PasswordInput from "./PasswordInput";
 import { _ } from "~/i18n";
 
 describe("PasswordInput Component", () => {
-  it("has initial state for password visibility as false", () => {
+  it("renders a password input", () => {
     plainRender(
       <PasswordInput
         id="password"
         name="password"
-        label={_("Password")}
-        fieldId="password"
-        ariaLabel={_("User password")}
+        aria-label={_("User password")}
       />
     );
-    const inputField = screen.getByLabelText("Password");
+
+    const inputField = screen.getByLabelText("User password");
     expect(inputField).toHaveAttribute("type", "password");
   });
 
-  it("after user clicks the eye icon type switches from password to text", async () => {
+  it("allows revealing the password", async () => {
     plainRender(
       <PasswordInput
         id="password"
         name="password"
-        label={_("Password")}
-        fieldId="password"
-        ariaLabel={_("User password")}
+        aria-label={_("User password")}
       />
     );
 
-    const passwordInput = screen.getByLabelText("Password");
+    const passwordInput = screen.getByLabelText("User password");
     const button = screen.getByRole("button");
 
     expect(passwordInput).toHaveAttribute("type", "password");
     await userEvent.click(button);
     expect(passwordInput).toHaveAttribute("type", "text")
-  });
-
-  it("can be disabled", () => {
-    plainRender(
-      <PasswordInput
-        id="password"
-        name="password"
-        label={_("Password")}
-        fieldId="password"
-        ariaLabel={_("User password")}
-        isDisabled={true}
-      />
-    );
-    const inputField = screen.getByLabelText("Password");
-    const visibilityButton = screen.getByRole("button");
-
-    expect(inputField).toBeDisabled();
-    expect(visibilityButton).toBeDisabled();
   });
 
   it("applies autoFocus behavior correctly", () => {
@@ -84,88 +63,11 @@ describe("PasswordInput Component", () => {
         autoFocus
         id="password"
         name="password"
-        label={_("Password")}
-        fieldId="password"
-        ariaLabel={_("User password")}
+        aria-label={_("User password")}
       />
     );
-    const inputField = screen.getByLabelText("Password");
 
+    const inputField = screen.getByLabelText("User password");
     expect(document.activeElement).toBe(inputField);
-  });
-
-  it("displays helper text for invalid input", () => {
-    plainRender(
-      <PasswordInput
-        id="password"
-        name="password"
-        label={_("Password")}
-        fieldId="password"
-        ariaLabel={_("User password")}
-        helperTextInvalid="Invalid input"
-        validated="error"
-      />
-    );
-    const helperText = screen.getByText("Invalid input");
-
-    expect(helperText).toBeInTheDocument();
-  });
-
-  it("calls onBlur event handler", () => {
-    const onBlurMock = jest.fn();
-    plainRender(
-      <PasswordInput
-        id="password"
-        name="password"
-        label={_("Password")}
-        fieldId="password"
-        ariaLabel={_("User password")}
-        onBlur={onBlurMock}
-      />
-    );
-    const inputField = screen.getByLabelText("Password");
-
-    fireEvent.blur(inputField);
-    expect(onBlurMock).toHaveBeenCalled();
-  });
-
-  it("ensures accessibility attributes are set correctly", () => {
-    plainRender(
-      <PasswordInput
-        id="password"
-        name="password"
-        label={_("Password")}
-        fieldId="password"
-        ariaLabel={_("Password")}
-      />
-    );
-    const inputField = screen.getByLabelText("Password");
-    const visibilityButton = screen.getByLabelText("Password visibility button");
-
-    expect(inputField).toHaveAttribute("aria-label", "Password");
-    expect(visibilityButton).toHaveAttribute("aria-label", "Password visibility button");
-  });
-
-  it("handles setting and changing the input value", async () => {
-    let inputValue = "";
-    const handleChange = (value) => {
-      inputValue = value;
-    };
-
-    plainRender(
-      <PasswordInput
-        id="password"
-        name="password"
-        label={_("Password")}
-        fieldId="password"
-        ariaLabel={_("Password")}
-        value={inputValue}
-        onChange={handleChange}
-      />
-    );
-    const inputField = screen.getByLabelText("Password");
-    
-    await fireEvent.change(inputField, { target: { value: "newpassword" }});
-    expect(inputValue).toBe("newpassword");
   });
 });
