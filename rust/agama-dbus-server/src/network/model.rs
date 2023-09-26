7bc080ad104b2dbba14bc7db8ff495bd95e55d6b
Clemens Famulla-Conrad <cfamullaconrad@suse.de>
2023-09-26 14:03:35 +0200

    Add Bonding model

--- rust/agama-dbus-server/src/network/model.rs
+++ rust/agama-dbus-server/src/network/model.rs
@@ -9,7 +9,7 @@ use std::{
     default::Default,
     fmt,
     net::IpAddr,
-    str::{self, FromStr},
+    str::{self, FromStr}, collections::HashMap,
 };
 use thiserror::Error;
 use uuid::Uuid;
@@ -52,6 +52,7 @@ impl NetworkState {
     pub fn get_connection_mut(&mut self, id: &str) -> Option<&mut Connection> {
         self.connections.iter_mut().find(|c| c.id() == id)
     }
+
 
     /// Adds a new connection.
     ///
@@ -219,6 +220,7 @@ pub enum Connection {
     Ethernet(EthernetConnection),
     Wireless(WirelessConnection),
     Loopback(LoopbackConnection),
+    Bond(BondConnection),
 }
 
 impl Connection {
@@ -234,6 +236,10 @@ impl Connection {
             }),
             DeviceType::Loopback => Connection::Loopback(LoopbackConnection { base }),
             DeviceType::Ethernet => Connection::Ethernet(EthernetConnection { base }),
+            DeviceType::Bond => Connection::Bond(BondConnection {
+                base,
+                ..Default::default()
+            }),
         }
     }
 
@@ -244,6 +250,7 @@ impl Connection {
             Connection::Ethernet(conn) => &conn.base,
             Connection::Wireless(conn) => &conn.base,
             Connection::Loopback(conn) => &conn.base,
+            Connection::Bond(conn) => &conn.base,
         }
     }
 
@@ -252,6 +259,7 @@ impl Connection {
             Connection::Ethernet(conn) => &mut conn.base,
             Connection::Wireless(conn) => &mut conn.base,
             Connection::Loopback(conn) => &mut conn.base,
+            Connection::Bond(conn) => &mut conn.base,
         }
     }
 
@@ -306,6 +314,37 @@ impl Connection {
     }
 }
 
+#[derive(Debug, PartialEq, Clone)]
+pub enum ParentKind {
+    Bond,
+}
+
+impl fmt::Display for ParentKind {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let name = match &self {
+            ParentKind::Bond => "bond",
+        };
+        write!(f, "{}", name)
+    }
+}
+
+impl FromStr for ParentKind {
+    type Err = NetworkStateError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "bond" => Ok(ParentKind::Bond),
+            _ => Err(NetworkStateError::UnknownParentKind(s.to_string())),
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Parent {
+    pub interface: String,
+    pub kind: ParentKind,
+}
+
 #[derive(Debug, Default, Clone)]
 pub struct BaseConnection {
     pub id: String,
@@ -314,6 +353,7 @@ pub struct BaseConnection {
     pub status: Status,
     pub interface: String,
     pub match_config: MatchConfig,
+    pub parent: Option<Parent>,
 }
 
 impl PartialEq for BaseConnection {
@@ -450,6 +490,17 @@ pub struct LoopbackConnection {
 }
 
 #[derive(Debug, Default, PartialEq, Clone)]
+pub struct BondConnection {
+    pub base: BaseConnection,
+    pub bond: BondConfig,
+}
+
+#[derive(Debug, Default, PartialEq, Clone)]
+pub struct BondConfig {
+    pub options: HashMap<String, String>
+}
+
+#[derive(Debug, Default, Clone, PartialEq)]
 pub struct WirelessConfig {
     pub mode: WirelessMode,
     pub ssid: SSID,
