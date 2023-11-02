116c8b476d306ca83331805db2dfa2d25e9c30ed
David Díaz González <dgonzalez@suse.de>
2023-11-02 08:23:31 +0000

    [web] Simplify Section component
    Using less internal components.

--- web/src/components/core/Section.jsx
+++ web/src/components/core/Section.jsx
@@ -27,53 +27,6 @@ import { Icon } from '~/components/layout';
 import { ValidationErrors } from "~/components/core";
 
 /**
- * Internal component for rendering the section icon
- *
- * @param {object} props
- * @param {string} [props.name] - the name of the icon
- *
- * @return {React.ReactElement}
- */
-const SectionIcon = ({ name }) => {
-  if (!name) return null;
-
-  return <Icon name={name} size={32} />;
-};
-
-/**
- * Internal component for rendering the section title
- *
- * @param {object} props
- * @param {string} props.id - the id for the header.
- * @param {string} props.text - the title for the section.
- * @param {string} props.path - the path where the section links to.
- *
- * @return {JSX.Element}
- */
-const SectionTitle = ({ id, text, path }) => {
-  if (!text?.trim()) return null;
-
-  const title = !path?.trim() ? <>{text}</> : <Link to={path}>{text}</Link>;
-
-  return <h2 id={id}>{title}</h2>;
-};
-
-/**
- * Internal component for wrapping and rendering the section content
- *
- * @param {object} props
- * @param {React.ReactElement|React.ReactElement[]} props.children - the content to be wrapped
- * @return {JSX.Element}
- */
-const SectionContent = ({ children }) => {
-  return (
-    <div className="stack content">
-      {children}
-    </div>
-  );
-};
-
-/**
  * Renders children into an HTML section
  * @component
  *
@@ -121,13 +74,15 @@ export default function Section({
     console.error("The Section component must have either, a 'title' or an 'aria-label'");
   }
 
-  const SectionHeader = () => {
-    if (!title) return;
+  const Header = () => {
+    if (!title?.trim()) return;
+
+    const header = !path?.trim() ? <>{title}</> : <Link to={path}>{title}</Link>;
 
     return (
       <>
-        <SectionIcon name={loading ? "loading" : icon} />
-        <SectionTitle id={headerId} text={title} path={path} />
+        <Icon name={loading ? "loading" : icon} />
+        <h2 id={headerId}>{header}</h2>
       </>
     );
   };
@@ -139,12 +94,12 @@ export default function Section({
       aria-label={ariaLabel || undefined}
       aria-labelledby={ title && !ariaLabel ? headerId : undefined}
     >
-      <SectionHeader />
-      <SectionContent>
+      <Header />
+      <div className="stack content">
         {errors?.length > 0 &&
           <ValidationErrors errors={errors} title={`${title} errors`} />}
         {children}
-      </SectionContent>
+      </div>
     </section>
   );
 }
