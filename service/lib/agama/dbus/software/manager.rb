f32952cfd4eac27d8b975b08c5080fb5f27fb452
Josef Reidinger <jreidinger@suse.cz>
2023-12-13 16:41:32 +0100

    fix dbus marshalling

--- service/lib/agama/dbus/software/manager.rb
+++ service/lib/agama/dbus/software/manager.rb
@@ -89,7 +89,7 @@ module Agama
 
           dbus_method(:AddPattern, "in id:s, out result:b") { |p| backend.add_pattern(p) }
           dbus_method(:RemovePattern, "in id:s, out result:b") { |p| backend.remove_pattern(p) }
-          dbus_method(:SetUserPatterns, "in ids:as, out wrong:as") { |ids| backend.assign_patterns(ids) }
+          dbus_method(:SetUserPatterns, "in ids:as, out wrong:as") { |ids| [backend.assign_patterns(ids)] }
 
           dbus_method :ProvisionsSelected, "in Provisions:as, out Result:ab" do |provisions|
             [provisions.map { |p| backend.provision_selected?(p) }]
