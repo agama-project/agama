6edbeed28defd9ef6642eb612a529a23ef88ff3a
David Díaz González <dgonzalez@suse.de>
2024-01-08 19:24:40 +0000

    [web][doc] Remove leaked wrong comment

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -17,8 +17,6 @@
  *
  * To contact SUSE LLC about this file by physical or electronic mail, you may
  * find current contact information at www.suse.com.
- *
- *  ".*\/src\/components\/([\\w\\-_]+)\/",
  */
 
 import React, { useEffect, useState } from "react";
