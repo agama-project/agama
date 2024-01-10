bb79e2c6f14e2cfd43ba55b2b2d39e1d82924518
Nagender Rao <125825782+nagu165@users.noreply.github.com>
2024-01-10 23:41:38 +0530

    Update VolumeForm.jsx
    Fix eslint error that was previously missed.

--- web/src/components/storage/VolumeForm.jsx
+++ web/src/components/storage/VolumeForm.jsx
@@ -122,7 +122,6 @@ const MountPointFormSelect = ({ id, volumes, ...selectProps }) => {
   );
 };
 
-
 /**
  * Btrfs file system type can be offered with two flavors, with and without snapshots.
  */
