04ebb2576915568536a614d1c6cd1f2f0c66fb3f
Imobach González Sosa <igonzalezsosa@suse.com>
2024-02-14 11:11:21 +0000

    rust: add tracing information to journald

--- rust/agama-dbus-server/src/agama-web-server.rs
+++ rust/agama-dbus-server/src/agama-web-server.rs
@@ -1,15 +1,21 @@
 use axum::{routing::get, Router};
 use tokio;
+use tower_http::trace::TraceLayer;
+use tracing_subscriber::prelude::*;
 
 mod http;
-
 use http::ping;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new()
-        .route("/ping", get(ping));
+    let journald = tracing_journald::layer().expect("could not connect to journald");
+    tracing_subscriber::registry()
+        .with(journald)
+        .init();
 
+    let app = Router::new()
+        .route("/ping", get(ping))
+        .layer(TraceLayer::new_for_http());
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
