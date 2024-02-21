e708f47070e31e2e4711c2ffbfbbbc7372be41bb
David Díaz González <dgonzalez@suse.de>
2024-02-21 17:18:14 +0000

    [web] Allow sections to have a description
    Intended for adding, when needed, a short explanation about its usefulness.

--- web/src/components/core/Section.jsx
+++ web/src/components/core/Section.jsx
@@ -48,6 +48,7 @@ import { ValidationErrors } from "~/components/core";
  * @typedef { Object } SectionProps
  * @property {string} [icon] - Name of the section icon. Not rendered if title is not provided.
  * @property {string} [title] - The section title. If not given, aria-label must be provided.
+ * @property {string} [description] - A section description. Use only if really needed.
  * @property {string} [name] - The section name. Used to build the header id.
  * @property {string} [path] - Path where the section links to.
  *  when user clicks on the title, used for opening a dialog.
@@ -63,6 +64,7 @@ import { ValidationErrors } from "~/components/core";
 export default function Section({
   icon,
   title,
+  description,
   name,
   path,
   loading,
@@ -86,7 +88,11 @@ export default function Section({
     const headerText = !path?.trim() ? title : <Link to={path}>{title}</Link>;
 
     return (
-      <h2 id={headerId}>{headerIcon}<span>{headerText}</span></h2>
+      <header>
+        <h2 id={headerId}>{headerIcon}<span>{headerText}</span></h2>
+        <p>{description}</p>
+      </header>
+
     );
   };
 
