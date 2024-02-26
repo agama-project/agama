916c07d29baa768255f1c3455919ca04b09dcbfe
Michal Filka <mfilka@suse.cz>
2024-02-26 20:31:08 +0100

    Skeleton for Server/{Login,Logout} subcommands

--- rust/agama-cli/src/server.rs
+++ rust/agama-cli/src/server.rs
@@ -2,6 +2,15 @@ use clap::Subcommand;
 
 #[derive(Subcommand, Debug)]
 pub enum ServerCommands {
+    /// Login with defined server. Result is JWT stored and used in all subsequent commands
+    Login {
+      #[clap(long, short = 'u')]
+      user: Option<String>,
+      #[clap(long, short = 'p')]
+      password: Option<String>,
+    },
+    /// Release currently stored JWT
+    Logout,
 }
 
 /// Main entry point called from agama CLI main loop
