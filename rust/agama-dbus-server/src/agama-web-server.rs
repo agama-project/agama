92e1009beaaa12c5dc58d137393e39d9f047325a
Imobach González Sosa <igonzalezsosa@suse.com>
2024-02-26 07:01:27 +0000

    rust: improve error handling

--- rust/agama-dbus-server/src/agama-web-server.rs
+++ rust/agama-dbus-server/src/agama-web-server.rs
@@ -1,3 +1,5 @@
+use std::process::{ExitCode, Termination};
+
 use agama_dbus_server::web::{self, run_monitor};
 use clap::{Parser, Subcommand};
 use tokio::sync::broadcast::channel;
@@ -27,7 +29,7 @@ struct Cli {
 }
 
 /// Start serving the API.
-async fn serve_command(address: &str) {
+async fn serve_command(address: &str) -> anyhow::Result<()> {
     let journald = tracing_journald::layer().expect("could not connect to journald");
     tracing_subscriber::registry().with(journald).init();
 
@@ -36,26 +38,51 @@ async fn serve_command(address: &str) {
         .unwrap_or_else(|_| panic!("could not listen on {}", address));
 
     let (tx, _) = channel(16);
-    run_monitor(tx.clone()).await;
+    run_monitor(tx.clone()).await?;
 
     let config = web::ServiceConfig::load().unwrap();
     let service = web::service(config, tx);
     axum::serve(listener, service)
         .await
         .expect("could not mount app on listener");
+    Ok(())
 }
 
 /// Display the API documentation in OpenAPI format.
-fn openapi_command() {
+fn openapi_command() -> anyhow::Result<()> {
     println!("{}", web::ApiDoc::openapi().to_pretty_json().unwrap());
+    Ok(())
 }
 
-#[tokio::main]
-async fn main() {
-    let cli = Cli::parse();
-
+async fn run_command(cli: Cli) -> anyhow::Result<()> {
     match cli.command {
         Commands::Serve { address } => serve_command(&address).await,
         Commands::Openapi => openapi_command(),
     }
+}
+
+/// Represents the result of execution.
+pub enum CliResult {
+    /// Successful execution.
+    Ok = 0,
+    /// Something went wrong.
+    Error = 1,
+}
+
+impl Termination for CliResult {
+    fn report(self) -> ExitCode {
+        ExitCode::from(self as u8)
+    }
+}
+
+#[tokio::main]
+async fn main() -> CliResult {
+    let cli = Cli::parse();
+
+    if let Err(error) = run_command(cli).await {
+        eprintln!("{:?}", error);
+        return CliResult::Error;
+    }
+
+    CliResult::Ok
 }
--- rust/agama-dbus-server/src/web.rs
+++ rust/agama-dbus-server/src/web.rs
@@ -14,7 +14,7 @@ mod service;
 mod state;
 mod ws;
 
-use agama_lib::{connection, progress::ProgressMonitor};
+use agama_lib::{connection, error::ServiceError, progress::ProgressMonitor};
 pub use auth::generate_token;
 pub use config::ServiceConfig;
 pub use docs::ApiDoc;
@@ -29,7 +29,7 @@ use self::progress::EventsProgressPresenter;
 /// Returns a service that implements the web-based Agama API.
 ///
 /// * `config`: service configuration.
-/// * `dbus`: D-Bus connection.
+/// * `events`: D-Bus connection.
 pub fn service(config: ServiceConfig, events: EventsSender) -> Router {
     MainServiceBuilder::new(events.clone())
         .add_service("/l10n", l10n_service(events))
@@ -37,11 +37,19 @@ pub fn service(config: ServiceConfig, events: EventsSender) -> Router {
         .build()
 }
 
-pub async fn run_monitor(events: EventsSender) {
+/// Starts monitoring the D-Bus service progress.
+///
+/// The events are sent to the `events` channel.
+///
+/// * `events`: channel to send the events to.
+pub async fn run_monitor(events: EventsSender) -> Result<(), ServiceError> {
     let presenter = EventsProgressPresenter::new(events);
-    let connection = connection().await.unwrap();
-    let mut monitor = ProgressMonitor::new(connection).await.unwrap();
+    let connection = connection().await?;
+    let mut monitor = ProgressMonitor::new(connection).await?;
     tokio::spawn(async move {
-        _ = monitor.run(presenter).await;
+        if let Err(error) = monitor.run(presenter).await {
+            eprintln!("Could not monitor the D-Bus server: {}", error);
+        }
     });
+    Ok(())
 }
