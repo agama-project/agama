0bc558c06ba12c8f50f63df223d28dc582cca2bf
Michal Filka <mfilka@suse.cz>
2024-02-27 13:49:16 +0100

    Subcommand match skeleton

--- rust/agama-cli/src/server.rs
+++ rust/agama-cli/src/server.rs
@@ -14,6 +14,35 @@ pub enum ServerCommands {
 }
 
 /// Main entry point called from agama CLI main loop
-pub async fn run(_subcommand: ServerCommands) -> anyhow::Result<()> {
+pub async fn run(subcommand: ServerCommands) -> anyhow::Result<()> {
+    match subcommand {
+        ServerCommands::Login {
+            user,
+            password,
+        }=> {
+            // actions to do:
+            // 1) somehow obtain credentials (interactive, commandline, from a file)
+            // 2) pass credentials to the web server
+            // 3) receive JWT
+            // 4) store the JWT in a well known location
+            login(user.unwrap_or(String::new()), password.unwrap_or(String::new()))
+        },
+        ServerCommands::Logout => {
+            // actions to do:
+            // 1) release JWT from the well known location if any
+            logout()
+        },
+    }
+}
+
+/// Asks user to enter user name / password or read it from a file
+fn get_credentials() {
+}
+
+fn login(user: String, password: String) -> anyhow::Result<()> {
+    Err(anyhow::anyhow!("Not implemented"))
+}
+
+fn logout() -> anyhow::Result<()> {
     Err(anyhow::anyhow!("Not implemented"))
 }
