b5b90ead7bb7471c1203e9aaf72b8a3a9f694f21
José Iván López González <jlopez@suse.com>
2024-03-25 12:33:27 +0000

    web: Adapt storage client to changes in D-Bus API

--- web/src/client/storage.js
+++ web/src/client/storage.js
@@ -23,8 +23,8 @@
 // cspell:ignore ptable
 
 import DBusClient from "./dbus";
+import { compact, hex, uniq } from "~/utils";
 import { WithIssues, WithStatus, WithProgress } from "./mixins";
-import { hex } from "~/utils";
 
 const STORAGE_OBJECT = "/org/opensuse/Agama/Storage1";
 const STORAGE_IFACE = "org.opensuse.Agama.Storage1";
@@ -111,13 +111,15 @@ const ZFCP_DISK_IFACE = "org.opensuse.Agama.Storage1.ZFCP.Disk";
  * @property {boolean} delete
  *
  * @typedef {object} ProposalSettings
+ * @property {string} target
+ * @property {string} [targetDevice]
+ * @property {string[]} [targetPVDevices]
+ * @property {boolean} configureBoot
  * @property {string} bootDevice
  * @property {string} encryptionPassword
  * @property {string} encryptionMethod
- * @property {boolean} lvm
  * @property {string} spacePolicy
  * @property {SpaceAction[]} spaceActions
- * @property {string[]} systemVGDevices
  * @property {Volume[]} volumes
  * @property {StorageDevice[]} installationDevices
  *
@@ -127,6 +129,8 @@ const ZFCP_DISK_IFACE = "org.opensuse.Agama.Storage1.ZFCP.Disk";
  *
  * @typedef {object} Volume
  * @property {string} mountPath
+ * @property {string} target
+ * @property {string} [targetDevice]
  * @property {string} fsType
  * @property {number} minSize
  * @property {number} [maxSize]
@@ -457,8 +461,15 @@ class ProposalManager {
         };
       };
 
-      const buildInstallationDevices = (proxy, devices) => {
-        const findDevice = (devices, name) => {
+      const buildTargetPVDevices = dbusTargetPVDevices => {
+        if (!dbusTargetPVDevices) return [];
+
+        return dbusTargetPVDevices.v.map(d => d.v);
+      };
+
+      // TODO: Read installation devices from D-Bus.
+      const buildInstallationDevices = (dbusSettings, devices) => {
+        const findDevice = (name) => {
           const device = devices.find(d => d.name === name);
 
           if (device === undefined) console.log("D-Bus object not found: ", name);
@@ -466,26 +477,38 @@ class ProposalManager {
           return device;
         };
 
-        const names = proxy.SystemVGDevices.filter(n => n !== proxy.BootDevice).concat([proxy.BootDevice]);
+        const values = [
+          dbusSettings.TargetDevice?.v,
+          buildTargetPVDevices(dbusSettings.TargetPVDevices),
+          dbusSettings.BootDevice.v,
+          dbusSettings.Volumes.v.map(vol => vol.v.TargetDevice.v)
+        ].flat();
+
+        const names = uniq(compact(values)).filter(d => d.length > 0);
+
         // #findDevice returns undefined if no device is found with the given name.
-        return names.map(dev => findDevice(devices, dev)).filter(dev => dev !== undefined);
+        return compact(names.map(n => findDevice(n)));
       };
+
+      const dbusSettings = proxy.Settings;
 
       return {
         settings: {
-          bootDevice: proxy.BootDevice,
-          lvm: proxy.LVM,
-          spacePolicy: proxy.SpacePolicy,
-          spaceActions: proxy.SpaceActions.map(buildSpaceAction),
-          systemVGDevices: proxy.SystemVGDevices,
-          encryptionPassword: proxy.EncryptionPassword,
-          encryptionMethod: proxy.EncryptionMethod,
-          volumes: proxy.Volumes.map(this.buildVolume),
+          target: dbusSettings.Target.v,
+          targetDevice: dbusSettings.TargetDevice?.v,
+          targetPVDevices: buildTargetPVDevices(dbusSettings.TargetPVDevices),
+          configureBoot: dbusSettings.ConfigureBoot.v,
+          bootDevice: dbusSettings.BootDevice.v,
+          spacePolicy: dbusSettings.SpacePolicy.v,
+          spaceActions: dbusSettings.SpaceActions.v.map(a => buildSpaceAction(a.v)),
+          encryptionPassword: dbusSettings.EncryptionPassword.v,
+          encryptionMethod: dbusSettings.EncryptionMethod.v,
+          volumes: dbusSettings.Volumes.v.map(vol => this.buildVolume(vol.v)),
           // NOTE: strictly speaking, installation devices does not belong to the settings. It
           // should be a separate method instead of an attribute in the settings object.
           // Nevertheless, it was added here for simplicity and to avoid passing more props in some
           // react components. Please, do not use settings as a jumble.
-          installationDevices: buildInstallationDevices(proxy, systemDevices)
+          installationDevices: buildInstallationDevices(proxy.Settings, systemDevices)
         },
         actions: proxy.Actions.map(buildAction)
       };
@@ -502,13 +525,15 @@ class ProposalManager {
    */
   async calculate(settings) {
     const {
+      target,
+      targetDevice,
+      targetPVDevices,
+      configureBoot,
       bootDevice,
       encryptionPassword,
       encryptionMethod,
-      lvm,
       spacePolicy,
       spaceActions,
-      systemVGDevices,
       volumes
     } = settings;
 
@@ -538,13 +563,15 @@ class ProposalManager {
     };
 
     const dbusSettings = removeUndefinedCockpitProperties({
+      Target: { t: "s", v: target },
+      TargetDevice: { t: "s", v: targetDevice },
+      TargetPVDevices: { t: "as", v: targetPVDevices },
+      ConfigureBoot: { t: "b", v: configureBoot },
       BootDevice: { t: "s", v: bootDevice },
       EncryptionPassword: { t: "s", v: encryptionPassword },
       EncryptionMethod: { t: "s", v: encryptionMethod },
-      LVM: { t: "b", v: lvm },
       SpacePolicy: { t: "s", v: spacePolicy },
       SpaceActions: { t: "aa{sv}", v: dbusSpaceActions() },
-      SystemVGDevices: { t: "as", v: systemVGDevices },
       Volumes: { t: "aa{sv}", v: volumes?.map(dbusVolume) }
     });
 
@@ -559,6 +586,8 @@ class ProposalManager {
    * @param {DBusVolume} dbusVolume
    *
    * @typedef {Object} DBusVolume
+   * @property {CockpitString} Target
+   * @property {CockpitString} [TargetDevice]
    * @property {CockpitString} MountPath
    * @property {CockpitString} FsType
    * @property {CockpitNumber} MinSize
@@ -614,6 +643,8 @@ class ProposalManager {
     };
 
     return {
+      target: dbusVolume.Target.v,
+      targetDevice: dbusVolume.TargetDevice?.v,
       mountPath: dbusVolume.MountPath.v,
       fsType: dbusVolume.FsType.v,
       minSize: dbusVolume.MinSize.v,
