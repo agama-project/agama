80ee6a5a6aff02f30adccced285e913b8692da04
Josef Reidinger <jreidinger@suse.cz>
2024-04-30 14:20:31 +0200

    rust format

--- rust/agama-server/src/storage/web.rs
+++ rust/agama-server/src/storage/web.rs
@@ -111,13 +111,11 @@ async fn product_params(
     let params = ProductParams {
         mount_points: state.client.product_mount_points().await?,
         encryption_methods: state.client.encryption_methods().await?,
-   };
+    };
     Ok(Json(params))
 }
 
-async fn usable_devices(
-    State(state): State<StorageState<'_>>,
-) -> Result<Json<String>, Error> {
+async fn usable_devices(State(state): State<StorageState<'_>>) -> Result<Json<String>, Error> {
     let devices = state.client.available_devices().await?;
     let devices_names = devices.into_iter().map(|d| d.name).collect();
 
