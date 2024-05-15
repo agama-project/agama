354b05f5a6905aa65e6555a69b02eec3d33fb1c6
Ladislav Slezák <lslezak@suse.com>
2024-05-15 16:00:08 +0200

    Fixed endless loop in hot module reloading

--- web/src/components/storage/DASDPage.jsx
+++ web/src/components/storage/DASDPage.jsx
@@ -23,7 +23,8 @@ import React, { useEffect, useReducer } from "react";
 
 import { _ } from "~/i18n";
 import { If, Page } from "~/components/core";
-import { DASDFormatProgress, DASDTable } from "~/components/storage";
+import DASDFormatProgress from "~/components/storage/DASDFormatProgress";
+import DASDTable from "~/components/storage/DASDTable";
 import { useCancellablePromise } from "~/utils";
 import { useInstallerClient } from "~/context/installer";
 
