01e9ad83a51e299511c2d18c16aada8ac2c0558c
Josef Reidinger <jreidinger@suse.cz>
2024-05-28 10:57:48 +0200

    initial non working import

--- rust/agama-cli/src/profile.rs
+++ rust/agama-cli/src/profile.rs
@@ -1,7 +1,9 @@
 use agama_lib::profile::{ProfileEvaluator, ProfileReader, ProfileValidator, ValidationResult};
 use anyhow::Context;
 use clap::Subcommand;
-use std::path::Path;
+use tempfile::TempDir;
+use std::{fs::File, io::Write, path::{Path, PathBuf}, process::Command};
+use std::os::unix::process::CommandExt;
 
 #[derive(Subcommand, Debug)]
 pub enum ProfileCommands {
@@ -13,6 +15,15 @@ pub enum ProfileCommands {
 
     /// Evaluate a profile, injecting the hardware information from D-Bus
     Evaluate { path: String },
+
+    /// Import autoinstallation profile from given location into agama configuration
+    /// 
+    /// This is top level command that do all autoinstallation processing beside starting
+    /// installation. Unless there is a need to inject additional commands between processing
+    /// use this command instead of set of underlaying commands.
+    /// Optional dir argument is location where profile is processed. Useful for debugging
+    /// if something goes wrong.
+    Import { url: String, dir: Option<PathBuf> },
 }
 
 fn download(url: &str) -> anyhow::Result<()> {
@@ -50,10 +61,34 @@ fn evaluate(path: String) -> anyhow::Result<()> {
     Ok(())
 }
 
+fn import(url: String, dir: Option<PathBuf>) -> anyhow::Result<()> {
+    let tmpdir = TempDir::new()?; // TODO: create it only if dir is not passed
+    let output_file = if url.ends_with(".sh") {
+        "profile.sh"
+    } else {
+        "profile.json"
+    };
+    let output_dir = dir.map_or(tmpdir.path(), |dir| dir.as_ref());
+    let reader = ProfileReader::new(url.as_str())?;
+    let contents = reader.read()?;
+    let output_path = output_dir.join(output_file);
+    let mut output_fd = File::create(output_dir.join(output_file))?;
+    output_fd.write_all(contents.as_bytes());
+    if output_file.ends_with(".sh") {
+        let err = Command::new("bash")
+        .args([output_path.to_str().context("Wrong path to shell script")?])
+        .exec();
+        println!("Error: {}", err);
+    }
+    
+    Ok(())
+}
+
 pub fn run(subcommand: ProfileCommands) -> anyhow::Result<()> {
     match subcommand {
         ProfileCommands::Download { url } => download(&url),
         ProfileCommands::Validate { path } => validate(path),
         ProfileCommands::Evaluate { path } => evaluate(path),
+        ProfileCommands::Import { url, dir } => import(url, dir),
     }
 }
