a09a56bdaa4ff3bac9665100f0029636c2bd0a64
David Díaz González <dgonzalez@suse.de>
2024-06-06 09:35:44 +0100

    web: Missing custom component

---
+++ web/src/components/core/CardField.jsx
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) [2024] SUSE LLC
+ *
+ * All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of version 2 of the GNU General Public License as published
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, contact SUSE LLC.
+ *
+ * To contact SUSE LLC about this file by physical or electronic mail, you may
+ * find current contact information at www.suse.com.
+ */
+
+// @ts-check
+
+import React from "react";
+import {
+  Card, CardHeader, CardTitle, CardBody, CardFooter,
+  Flex, FlexItem,
+} from "@patternfly/react-core";
+import textStyles from '@patternfly/react-styles/css/utilities/Text/text';
+
+// FIXME: improve name and documentation
+// TODO: allows having a drawer, see storage/ProposalResultActions
+
+/**
+ * Field wrapper built on top of PF/Card
+ * @component
+ *
+ * @todo write documentation
+ */
+export default function CardField({
+  label,
+  value,
+  description,
+  actions,
+  children = [],
+  cardProps = {},
+  cardHeaderProps = {},
+
+}) {
+  return (
+    <Card isCompact isFullHeight isRounded {...cardProps}>
+      <CardHeader {...cardHeaderProps}>
+        <CardTitle>
+          <Flex alignItems={{ default: "alignItemsCenter" }}>
+            <FlexItem spacer={{ default: "spacerSm" }}>
+              <h3>{label}</h3>
+            </FlexItem>
+            <FlexItem grow={{ default: "grow" }} className={textStyles.fontSizeXl}>
+              {value}
+            </FlexItem>
+          </Flex>
+        </CardTitle>
+      </CardHeader>
+      <CardBody>
+        {description && <div className={textStyles.color_200}>{description}</div>}
+      </CardBody>
+      {children}
+      <CardFooter>
+        {actions}
+      </CardFooter>
+    </Card>
+  );
+}
