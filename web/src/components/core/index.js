9e772ce81b4b2f364205af3d782ca7673479a68e
José Iván López González <jlopez@suse.com>
2024-06-10 15:44:47 +0100

    web: drop Tag component

--- web/src/components/core/index.js
+++ web/src/components/core/index.js
@@ -55,7 +55,6 @@ export { default as ServerError } from "./ServerError";
 export { default as ExpandableSelector } from "./ExpandableSelector";
 export { default as OptionsPicker } from "./OptionsPicker";
 export { default as Reminder } from "./Reminder";
-export { default as Tag } from "./Tag";
 export { default as TreeTable } from "./TreeTable";
 export { default as CardField } from "./CardField";
 export { default as ButtonLink } from "./ButtonLink";
