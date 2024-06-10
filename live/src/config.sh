e15343b081dd1cf5d649323707eb9f36a9595cdd
Ladislav Slezák <lslezak@suse.com>
2024-06-10 17:59:16 +0200

    feat(Live): Display more info in console

--- live/src/config.sh
+++ live/src/config.sh
@@ -21,6 +21,10 @@ systemctl enable agama-web-server.service
 systemctl enable agama-auto.service
 systemctl enable agama-hostname.service
 systemctl enable agama-proxy-setup.service
+systemctl enable agama-certificate-issue.path
+systemctl enable agama-welcome-issue.service
+systemctl enable agama-avahi-issue.service
+systemctl enable agama-ssh-issue.service
 systemctl enable live-password-cmdline.service
 systemctl enable live-password-dialog.service
 systemctl enable live-password-iso.service
