19acdfcbb4a88de5c8f058547b2cc13cf943b605
Imobach González Sosa <igonzalezsosa@suse.com>
2024-06-26 14:55:58 +0100

    fix: use proper Rust comments

--- rust/agama-server/src/network/nm/client.rs
+++ rust/agama-server/src/network/nm/client.rs
@@ -173,7 +173,7 @@ impl<'a> NetworkManagerClient<'a> {
                 .build()
                 .await?;
             let flags = proxy.flags().await?;
-            # https://networkmanager.dev/docs/api/latest/nm-dbus-types.html#NMSettingsConnectionFlags
+            // https://networkmanager.dev/docs/api/latest/nm-dbus-types.html#NMSettingsConnectionFlags
             if flags & 8 != 0 {
                 log::warn!("Skipped connection because of flags: {}", flags);
                 continue;
