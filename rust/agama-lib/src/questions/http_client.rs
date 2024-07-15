84120fb05eb276254325ac8cbbbbd79141af3887
Josef Reidinger <jreidinger@suse.cz>
2024-07-15 09:52:52 +0200

    implement getting answer

--- rust/agama-lib/src/questions/http_client.rs
+++ rust/agama-lib/src/questions/http_client.rs
@@ -1,6 +1,11 @@
+use std::time::Duration;
+
+use reqwest::StatusCode;
+use tokio::time::sleep;
+
 use crate::{base_http_client::BaseHTTPClient, error::ServiceError};
 
-use super::model::{self, Question};
+use super::model::{self, Answer, Question};
 
 pub struct HTTPClient {
     client: BaseHTTPClient,
@@ -25,6 +30,35 @@ impl HTTPClient {
             Ok(question)
         } else {
             Err(self.client.build_backend_error(response).await)
+        }
+    }
+
+    /// non blocking varient of checking if question has already answer
+    pub async fn try_answer(&self, question_id: u32) -> Result<Option<Answer>, ServiceError> {
+        let path = format!("/questions/{}/answer", question_id);
+        let response = self.client.get_response(path.as_str()).await?;
+        if response.status() == StatusCode::NOT_FOUND {
+            Ok(None)
+        } else if response.status().is_success() {
+            let answer = response.json().await?;
+            Ok(answer)
+        } else {
+            Err(self.client.build_backend_error(response).await)
+        }
+    }
+
+    /// Blocking variant of getting answer for given question.
+    pub async fn get_answer(&self, question_id: u32) -> Result<Answer, ServiceError> {
+        loop {
+            let answer = self.try_answer(question_id).await?;
+            if let Some(result) = answer {
+                return Ok(result);
+            }
+            let duration = Duration::from_secs(1);
+            sleep(duration).await;
+            // TODO: use websocket to get events instead of polling, but be aware of race condition that
+            // auto answer can answer faster before we connect to socket. So ask for answer
+            // and meanwhile start checking for events
         }
     }
 }
