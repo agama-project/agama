24c414442a6e8e0b1dc8cdd60f272cb2568be6e3
Imobach González Sosa <igonzalezsosa@suse.com>
2024-07-23 12:19:34 +0100

    test(web): fix root auth tests

--- web/src/components/users/RootSSHKeyPopup.test.jsx
+++ web/src/components/users/RootSSHKeyPopup.test.jsx
@@ -22,54 +22,51 @@
 import React from "react";
 
 import { screen, waitFor, within } from "@testing-library/react";
-import { installerRender } from "~/test-utils";
-import { createClient } from "~/client";
+import { plainRender } from "~/test-utils";
 import { RootSSHKeyPopup } from "~/components/users";
 
-jest.mock("~/client");
+const mockRootUserMutation = { mutateAsync: jest.fn() };
+let mockSSHKey;
+
+jest.mock("~/queries/users", () => ({
+  ...jest.requireActual("~/queries/users"),
+  useRootUser: () => ({ data: { sshkey: mockSSHKey } }),
+  useRootUserMutation: () => mockRootUserMutation,
+  useRootUserChanges: () => jest.fn(),
+}));
 
 const onCloseCallback = jest.fn();
 const setRootSSHKeyFn = jest.fn();
 const testKey = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDM+ test@example";
 
-beforeEach(() => {
-  createClient.mockImplementation(() => {
-    return {
-      users: {
-        setRootSSHKey: setRootSSHKeyFn,
-      },
-    };
-  });
-});
-
 describe("when it is closed", () => {
-  it("renders nothing", async () => {
-    const { container } = installerRender(<RootSSHKeyPopup />);
-    await waitFor(() => expect(container).toBeEmptyDOMElement());
+  it("renders nothing", () => {
+    const { container } = plainRender(<RootSSHKeyPopup />);
+    expect(container).toBeEmptyDOMElement();
   });
 });
 
 describe("when it is open", () => {
-  it("renders default title when none if given", async () => {
-    installerRender(<RootSSHKeyPopup isOpen />);
-    const dialog = await screen.findByRole("dialog");
+  it("renders default title when none if given", () => {
+    plainRender(<RootSSHKeyPopup isOpen />);
+    const dialog = screen.getByRole("dialog");
     within(dialog).getByText("Set root SSH public key");
   });
 
-  it("renders the given title", async () => {
-    installerRender(<RootSSHKeyPopup isOpen title="Root SSHKey" />);
-    const dialog = await screen.findByRole("dialog");
+  it("renders the given title", () => {
+    plainRender(<RootSSHKeyPopup isOpen title="Root SSHKey" />);
+    const dialog = screen.getByRole("dialog");
     within(dialog).getByText("Root SSHKey");
   });
 
-  it("contains the given key, if any", async () => {
-    installerRender(<RootSSHKeyPopup isOpen currentKey={testKey} />);
-    const dialog = await screen.findByRole("dialog");
+  it("contains the given key, if any", () => {
+    plainRender(<RootSSHKeyPopup isOpen currentKey={testKey} />);
+    const dialog = screen.getByRole("dialog");
     within(dialog).getByText(testKey);
   });
 
   it("allows defining a new root SSH public key", async () => {
-    const { user } = installerRender(<RootSSHKeyPopup isOpen onClose={onCloseCallback} />);
+    const { user } = plainRender(<RootSSHKeyPopup isOpen onClose={onCloseCallback} />);
 
     const dialog = await screen.findByRole("dialog");
     const sshKeyInput = within(dialog).getByLabelText("Root SSH public key");
@@ -80,12 +77,12 @@ describe("when it is open", () => {
     expect(confirmButton).toBeEnabled();
     await user.click(confirmButton);
 
-    expect(setRootSSHKeyFn).toHaveBeenCalledWith(testKey);
+    expect(mockRootUserMutation.mutateAsync).toHaveBeenCalledWith({ sshkey: testKey });
     expect(onCloseCallback).toHaveBeenCalled();
   });
 
   it("does not change anything if the user cancels", async () => {
-    const { user } = installerRender(<RootSSHKeyPopup isOpen onClose={onCloseCallback} />);
+    const { user } = plainRender(<RootSSHKeyPopup isOpen onClose={onCloseCallback} />);
     const dialog = await screen.findByRole("dialog");
     const sshKeyInput = within(dialog).getByLabelText("Root SSH public key");
     const cancelButton = within(dialog).getByRole("button", { name: /Cancel/i });
@@ -93,7 +90,7 @@ describe("when it is open", () => {
     await user.type(sshKeyInput, testKey);
     await user.click(cancelButton);
 
-    expect(setRootSSHKeyFn).not.toHaveBeenCalled();
+    expect(mockRootUserMutation.mutateAsync).not.toHaveBeenCalled();
     expect(onCloseCallback).toHaveBeenCalled();
   });
 });
