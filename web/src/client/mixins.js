ae08dddeb2cfd2032e4ab494d5ea97ca03dfb7c6
Imobach González Sosa <igonzalezsosa@suse.com>
2024-07-26 15:45:45 +0100

    refactor(web): drop the WithProgress mixin

--- web/src/client/mixins.js
+++ web/src/client/mixins.js
@@ -114,89 +114,6 @@ const WithStatus = (superclass, status_path, service_name) =>
   };
 
 /**
- * @typedef {object} Progress
- * @property {number} total - number of steps
- * @property {number} current - current step
- * @property {string} message - message of the current step
- * @property {boolean} finished - whether the progress already finished
- */
-
-/**
- * @typedef {object} ProgressSequence
- * @property {string[]} steps - sequence steps if known in advance
- * @property {number} total - number of steps
- * @property {number} current - current step
- * @property {string} message - message of the current step
- * @property {boolean} finished - whether the progress already finished
- */
-
-/**
- * @callback ProgressHandler
- * @param {Progress} progress - progress status
- * @return {void}
- */
-
-/**
- * Extends the given class with methods to get and track the service progress
- *
- * @template {!WithHTTPClient} T
- * @param {T} superclass - superclass to extend
- * @param {string} progress_path - status resource path (e.g., "/manager/status").
- * @param {string} service_name - service name (e.g., "org.opensuse.Agama.Manager1").
- */
-const WithProgress = (superclass, progress_path, service_name) =>
-  class extends superclass {
-    /**
-     * Returns the service progress
-     *
-     * @return {Promise<ProgressSequence>} an object containing the total steps,
-     *   the current step and whether the service finished or not.
-     */
-    async getProgress() {
-      const response = await this.client.get(progress_path);
-      if (!response.ok) {
-        console.log("get progress failed with:", response);
-        return {
-          steps: [],
-          total: 0,
-          current: 0,
-          message: "Failed to get progress",
-          finished: false,
-        };
-      } else {
-        const { steps, currentStep, maxSteps, currentTitle, finished } = await response.json();
-        return {
-          steps,
-          total: maxSteps,
-          current: currentStep,
-          message: currentTitle,
-          finished,
-        };
-      }
-    }
-
-    /**
-     * Register a callback to run when the progress changes
-     *
-     * @param {ProgressHandler} handler - callback function
-     * @return {import ("./http").RemoveFn} function to disable the callback
-     */
-    onProgressChange(handler) {
-      return this.client.onEvent("Progress", ({ service, ...progress }) => {
-        if (service === service_name) {
-          const { currentStep, maxSteps, currentTitle, finished } = progress;
-          handler({
-            total: maxSteps,
-            current: currentStep,
-            message: currentTitle,
-            finished,
-          });
-        }
-      });
-    }
-  };
-
-/**
  * @typedef {object} ValidationError
  * @property {string} message - Error message
  */
@@ -214,4 +131,4 @@ const createError = (message) => {
   return { message };
 };
 
-export { WithProgress, WithStatus };
+export { WithStatus };
