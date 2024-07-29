03d9c4b72794d8bfedb5490e050f529bf1d1a694
David Díaz González <dgonzalez@suse.de>
2024-07-29 09:48:04 +0100

    refactor(web): extract change product link to a component

---
+++ web/src/components/core/ChangeProductLink.tsx
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) [2024] SUSE LLC
+ *
+ * All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of version 2 of the GNU General Public License as published
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, contact SUSE LLC.
+ *
+ * To contact SUSE LLC about this file by physical or electronic mail, you may
+ * find current contact information at www.suse.com.
+ */
+
+import React from "react";
+import { Link, LinkProps } from "react-router-dom";
+import { useProduct } from "~/queries/software";
+import { PATHS } from "~/routes/products";
+import { _ } from "~/i18n";
+
+/**
+ * Link for navigating to the selection product.
+ */
+export default function ChangeProductLink({ children, ...props }: Omit<LinkProps, "to">) {
+  const { products } = useProduct();
+
+  if (products.length <= 1) return null;
+
+  return (
+    <Link to={PATHS.changeProduct} {...props}>
+      {children || _("Change product")}
+    </Link>
+  );
+}
