6fcbf2f477046079294842692b2ca979c2c5475e
Martin Vidner <mvidner@suse.com>
2024-07-29 12:34:13 +0200

    remove LocalizationClient (D-Bus), no longer used

--- rust/agama-lib/src/localization.rs
+++ rust/agama-lib/src/localization.rs
@@ -1,13 +1,11 @@
 //! Implements support for handling the localization settings
 
-mod client;
 mod http_client;
 pub mod model;
 mod proxies;
 mod settings;
 mod store;
 
-pub use client::LocalizationClient;
 pub use http_client::LocalizationHTTPClient;
 pub use proxies::LocaleProxy;
 pub use settings::LocalizationSettings;
