4d67bb4c8655440ce8dd828c69535561b6a9d174
David Díaz González <dgonzalez@suse.de>
2024-07-30 12:35:22 +0100

    refactor(web): add questions queries

---
+++ web/src/queries/questions.ts
@@ -0,0 +1,119 @@
+/*
+ * Copyright (c) [2024] SUSE LLC
+ *
+ * All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of version 2 of the GNU General Public License as published
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, contact SUSE LLC.
+ *
+ * To contact SUSE LLC about this file by physical or electronic mail, you may
+ * find current contact information at www.suse.com.
+ */
+
+import React from "react";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useInstallerClient } from "~/context/installer";
+import { Answer, Question, QuestionType } from "~/types/questions";
+
+type APIQuestion = {
+  generic?: Question;
+  withPassword?: Pick<Question, "password">;
+};
+
+/**
+ * Internal method to build proper question objects
+ *
+ * TODO: improve/simplify it once the backend API is improved.
+ */
+function buildQuestion(httpQuestion: APIQuestion) {
+  let question: Question;
+
+  if (httpQuestion.generic) {
+    question = {
+      ...httpQuestion.generic,
+      type: QuestionType.generic,
+      answer: httpQuestion.generic.answer,
+    };
+  }
+
+  if (httpQuestion.withPassword) {
+    question = {
+      id: httpQuestion.generic.id,
+      type: QuestionType.withPassword,
+      password: httpQuestion.withPassword.password,
+    };
+  }
+
+  return question;
+}
+
+/**
+ * Query to retrieve questions
+ */
+const questionsQuery = () => ({
+  queryKey: ["questions"],
+  queryFn: () => fetch("/api/questions").then((res) => res.json()),
+});
+
+/**
+ * Hook that builds a mutation given question, allowing to answer it
+
+ * TODO: improve/simplify it once the backend API is improved.
+ */
+const useQuestionsConfig = () => {
+  const query = {
+    mutationFn: (question: Question) => {
+      const answer: Answer = { generic: { answer: question.answer } };
+
+      if (question.type === QuestionType.withPassword) {
+        answer.withPassword = { password: question.password };
+      }
+
+      return fetch(`/api/questions/${question.id}/answer`, {
+        method: "PATCH",
+        body: JSON.stringify(answer),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+    },
+  };
+  return useMutation(query);
+};
+
+/**
+ * Hook for listening questions changes and performing proper invalidations
+ */
+const useQuestionsChanges = () => {
+  const queryClient = useQueryClient();
+  const client = useInstallerClient();
+
+  React.useEffect(() => {
+    if (!client) return;
+
+    return client.ws().onEvent((event) => {
+      if (event.type === "QuestionsChanged") {
+        queryClient.invalidateQueries({ queryKey: ["questions"] });
+      }
+    });
+  }, [client, queryClient]);
+};
+
+/**
+ * Hook for retrieving available questions
+ */
+const useQuestions = () => {
+  const { data, isPending } = useQuery(questionsQuery());
+  return isPending ? [] : data.map(buildQuestion);
+};
+
+export { questionsQuery, useQuestions, useQuestionsConfig, useQuestionsChanges };
