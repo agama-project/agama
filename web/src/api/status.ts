35dbb5dc95360a308f517953c96199ee05ba9efe
Josef Reidinger <jreidinger@suse.cz>
2024-08-19 10:59:59 +0200

    fix parsing json for installer status that cause early finish of installation (fix#1542)

--- web/src/api/status.ts
+++ web/src/api/status.ts
@@ -26,8 +26,8 @@ import { InstallerStatus } from "~/types/status";
  * Returns the installer status information
  */
 const fetchInstallerStatus = async (): Promise<InstallerStatus> => {
-  const { phase, isBusy, useIguana, canInstall } = await get("/api/manager/installer");
-  return { phase, isBusy, useIguana, canInstall };
+  const { phase, busy, iguana, canInstall } = await get("/api/manager/installer");
+  return { phase, isBusy: busy.length !== 0, useIguana: iguana, canInstall };
 };
 
 export { fetchInstallerStatus };
