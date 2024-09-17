6ad237bc77cc1eeacc9f7e41d67c4797e670e51d
David Díaz González <dgonzalez@suse.de>
2024-09-17 12:46:45 +0100

    doc(website): adds the Agama social card
    For replacing the docusaurus-social-card, which has been removed.
    
    Please note that the agma-social-card.png is just a PoC made with the
    help of a couple of free tools on the web, namely
    
      * https://social.cards/
      * https://browserframe.com/
      * https://mdigi.tools/gradient-generator/
    
    But of course, the final version should be done with more time and love
    and, maybe, using an image manipulation software.
    
    The background gradient and texts colors were chosen from https://brand.suse.com/brand-system/color-palette

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -45,7 +45,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: "img/docusaurus-social-card.jpg",
+    image: "img/agama-social-card.png",
     navbar: {
       title: "My Site",
       logo: {
