4d2404a9a867d9b9a3404f465b00d98e73a640f4
Josef Reidinger <jreidinger@suse.cz>
2024-09-17 10:08:17 +0200

    improve example

--- rust/libzypp/src/bin.rs
+++ rust/libzypp/src/bin.rs
@@ -1,21 +1,44 @@
 use std::process::exit;
 
+use glib::Error;
 use libzypp::InfoBaseExt;
 
-fn main() -> () {
-    let zcontext = libzypp::Context::builder().build();
-    if let Err(err) = zcontext.load_system(Some("/")) {
-        println!("Failed to load system: {}", err.to_string());
-        exit(1);
-    }
+fn print_system() -> Result<(), Error> {
+    println!("System repos:");
+    print_with_root("/")?;
+    println!("");
+    Ok(())
+}
 
-    let repo_manager = libzypp::RepoManager::new(&zcontext);
-    if let Err(err) = repo_manager.initialize() {
-        println!("Failed to load repos: {}", err.to_string());
-        exit(1);
-    }
+fn print_host() -> Result<(), Error> {
+    println!("Host repos:");
+    print_with_root("/run/host")?;
+    println!("");
+    Ok(())
+}
+
+fn print_with_root(root: &str) -> Result<(), Error> {
+    let context = libzypp::Context::builder().build();
+    context.load_system(Some(root))?;
+
+    let repo_manager = libzypp::RepoManager::new(&context);
+    repo_manager.initialize()?;
 
     for repo in repo_manager.known_repos() {
         println!("{:?}", repo.name());
+    }
+
+    Ok(())
+}
+
+fn main() -> () {
+    if let Err(err) = print_system() {
+        println!("Failed to system: {}", err.to_string());
+        exit(1);
+    }
+
+    if let Err(err) = print_host() {
+        println!("Failed to system: {}", err.to_string());
+        exit(1);
     }
 }
