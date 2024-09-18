da2487ea4ebda32874e11aba1207cac24e9c6211
Josef Reidinger <jreidinger@suse.cz>
2024-09-18 13:24:13 +0200

    first version of setup script and try to improve submodules

---
+++ rust/libzypp/setup_env.sh
@@ -0,0 +1,75 @@
+#!/usr/bin/bash
+
+# Script that do initial setup for libzypp ng bindings development.
+# To update git submodules use
+# git submodule update --remote
+
+BASEDIR=$(dirname "$0")
+
+# install all required packages
+sudo zypper --non-interactive install \
+  git \
+  cmake \
+  openssl \
+  libudev1 \
+  libboost_headers-devel \
+  libboost_program_options-devel \
+  libboost_test-devel \
+  libboost_thread-devel \
+  dejagnu \
+  doxygen \
+  texlive-latex \
+  texlive-xcolor \
+  texlive-newunicodechar \
+  texlive-dvips \
+  ghostscript \
+  gcc-c++ \
+  gettext-devel \
+  graphviz \
+  libxml2-devel \
+  yaml-cpp-devel \
+  gobject-introspection-devel \
+  libproxy-devel \
+  pkg-config \
+  libsolv-devel \
+  libsolv-tools-base \
+  glib2-devel \
+  libsigc++2-devel \
+  readline-devel \
+  nginx \
+  vsftpd \
+  rpm \
+  rpm-devel \
+  libgpgme-devel \
+  FastCGI-devel \
+  libcurl-devel \
+  "rubygem(ruby:3.3.0:abstract_method)" \
+  libzck-devel \
+  libzstd-devel \
+  libbz2-devel \
+  xz-devel \
+  rustup
+
+cd "$BASEDIR"
+# checkout submodules
+git submodule update --checkout
+
+# lets build libzypp
+cd libzypp
+make -f Makefile.cvs
+make
+sudo make install
+cd -
+
+# now lets make rust working
+rustup install stable
+# lets install gir
+cd gir
+cargo install --path .
+cd -
+
+# to use gir follow https://gtk-rs.org/gir/book/tutorial/sys_library.html
+
+cargo build
+
+echo "To test if everything work run `../target/debug/zypprs`"
