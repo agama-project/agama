0b5aabdb97c4a0edf3da3fa33bd4accf7fb58466
Imobach González Sosa <igonzalezsosa@suse.com>
2024-09-19 00:05:36 +0100

    chore(rust): add a xtask to generate a markdown reference

--- rust/xtask/src/main.rs
+++ rust/xtask/src/main.rs
@@ -1,3 +1,25 @@
+use std::env;
+
+mod tasks {
+    use agama_cli::Cli;
+    use clap_markdown::MarkdownOptions;
+
+    pub fn generate_markdown() {
+        let options = MarkdownOptions::new()
+            .title("Command-line reference".to_string())
+            .show_footer(false);
+        let markdown = clap_markdown::help_markdown_custom::<Cli>(&options);
+        print!("{}", markdown);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let Some(task) = env::args().nth(1) else {
+        panic!("You must specify a task");
+    };
+
+    match task.as_str() {
+        "markdown" => tasks::generate_markdown(),
+        _ => eprintln!("Unknown task"),
+    }
 }
