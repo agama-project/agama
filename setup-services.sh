9405f2be46ca780b5454cdb76cd438c9893cf089
Imobach González Sosa <igonzalezsosa@suse.com>
2024-11-05 15:33:57 +0000

    fix(setup): do not use pre-installed gems in distrobox

--- setup-services.sh
+++ setup-services.sh
@@ -117,7 +117,7 @@ fi
       sed -e '/gemspec/a gem "ruby-dbus", path: "/checkout-ruby-dbus"' -i Gemfile
   fi
 
-  if [ -n "${CI:-}" ] || [ -n "${DISTROBOX_ENTER_PATH:-}" ]; then
+  if [ -n "${CI:-}" ]; then
     # in CI reuse the pre-installed system gems from RPMs
     bundle config set --local disable_shared_gems 0
     sudo bundle install
