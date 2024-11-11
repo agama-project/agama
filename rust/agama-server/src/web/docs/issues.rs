da996b2f03cd22137a1918da43c0aad3d510f845
Imobach González Sosa <igonzalezsosa@suse.com>
2024-11-11 06:56:22 +0000

    fix(rust): add issues API to OpenAPI spec

---
+++ rust/agama-server/src/web/docs/issues.rs
@@ -0,0 +1,43 @@
+// Copyright (c) [2024] SUSE LLC
+//
+// All Rights Reserved.
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation; either version 2 of the License, or (at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+// more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, contact SUSE LLC.
+//
+// To contact SUSE LLC about this file by physical or electronic mail, you may
+// find current contact information at www.suse.com.
+
+use utoipa::openapi::{Components, ComponentsBuilder, Paths, PathsBuilder};
+
+use super::ApiDocBuilder;
+
+pub struct IssuesApiDocBuilder;
+
+impl ApiDocBuilder for IssuesApiDocBuilder {
+    fn title(&self) -> String {
+        "Issues HTTP API".to_string()
+    }
+
+    fn paths(&self) -> Paths {
+        PathsBuilder::new()
+            .path_from::<crate::web::common::__path_issues>()
+            .build()
+    }
+
+    fn components(&self) -> Components {
+        ComponentsBuilder::new()
+            .schema_from::<crate::web::common::Issue>()
+            .build()
+    }
+}
