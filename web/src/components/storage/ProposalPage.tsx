79e0290bac0b8f968cf5f09956fd819de3014815
Ancor Gonzalez Sosa <ancor@suse.com>
2024-11-14 13:09:39 +0000

    web: Delete ProposalActionsSummary

--- web/src/components/storage/ProposalPage.tsx
+++ web/src/components/storage/ProposalPage.tsx
@@ -27,7 +27,6 @@ import ProposalResultSection from "./ProposalResultSection";
 import ProposalTransactionalInfo from "./ProposalTransactionalInfo";
 import ConfigEditor from "./ConfigEditor";
 import ConfigEditorMenu from "./ConfigEditorMenu";
-import ProposalActionsSummary from "~/components/storage/ProposalActionsSummary";
 import EncryptionField from "~/components/storage/EncryptionField"
 import { _ } from "~/i18n";
 import { toValidationError } from "~/utils";
@@ -58,7 +57,6 @@ export const NOT_AFFECTED = {
   // the ProposalResultSection is refreshed always
   InstallationDeviceField: [CHANGING.ENCRYPTION, CHANGING.BOOT, CHANGING.POLICY, CHANGING.VOLUMES],
   PartitionsField: [CHANGING.ENCRYPTION, CHANGING.POLICY],
-  ProposalActionsSummary: [CHANGING.ENCRYPTION, CHANGING.TARGET],
 };
 
 export default function ProposalPage() {
