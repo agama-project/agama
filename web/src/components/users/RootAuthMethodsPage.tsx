040d4de3a35b8e916bb9ea7a6918b62842415c77
David Díaz González <dgonzalez@suse.com>
2024-11-27 19:34:21 +0000

    feat(web): add initial content for RootAuthMethodsPage
    Still missing the logic for validating and sending data provided by
    user.

--- web/src/components/users/RootAuthMethodsPage.tsx
+++ web/src/components/users/RootAuthMethodsPage.tsx
@@ -20,25 +20,83 @@
  * find current contact information at www.suse.com.
  */
 
-import React from "react";
-import { Grid, GridItem } from "@patternfly/react-core";
-import { Page } from "~/components/core";
+import React, { useRef, useState } from "react";
+import { FileUpload, Flex, Form, FormGroup, Grid, GridItem } from "@patternfly/react-core";
+import { Page, PasswordInput } from "~/components/core";
+import textStyles from "@patternfly/react-styles/css/utilities/Text/text";
 import { _ } from "~/i18n";
 
 function RootAuthMethodsPage() {
-  console.log("RootAuthMethodsPage");
+  const passwordRef = useRef();
+  const [password, setPassword] = useState("");
+  const [sshKey, setSSHKey] = useState("");
+  const [isUploading, setIsUploading] = useState(false);
+
+  const startUploading = () => setIsUploading(true);
+  const stopUploading = () => setIsUploading(false);
+  const clearKey = () => setSSHKey("");
+
+  const accept = (e) => {
+    e.preventDefault();
+    console.log("TODO: perform a check and submit if at least one method has been provided");
+  };
 
   return (
     <Page>
       <Page.Content>
         <Grid>
           <GridItem>
-            <Page.Section>
-              {_("Provide at least one authentication method for root user")}
+            <Page.Section
+              headerLevel="h2"
+              title={_("Setup root user authentication")}
+              description={
+                <p className={textStyles.fontSizeXl}>
+                  {_(
+                    "To continue with the installation settings, define at least one authentication method for the root user. You can still edit them anytime before installation.",
+                  )}
+                </p>
+              }
+              pfCardProps={{ isCompact: false }}
+              pfCardBodyProps={{ isFilled: true }}
+            >
+              <Form id="rootAuthMethods" onSubmit={accept}>
+                <Flex direction={{ default: "column" }} rowGap={{ default: "rowGapXl" }}>
+                  <FormGroup fieldId="rootPassword" label={_("Password")} className="autoWidth">
+                    <PasswordInput
+                      inputRef={passwordRef}
+                      id="rootPassword"
+                      value={password}
+                      className="autoInlineSize"
+                      onChange={(_, value) => setPassword(value)}
+                    />
+                  </FormGroup>
+                  <FormGroup fieldId="sshKey" label={_("SSH public key")}>
+                    <FileUpload
+                      id="sshKey"
+                      value={sshKey}
+                      type="text"
+                      filenamePlaceholder={_("Upload, paste, or drop here an SSH public key")}
+                      // TRANSLATORS: push button label
+                      browseButtonText={_("Upload")}
+                      // TRANSLATORS: push button label, clears the related input field
+                      clearButtonText={_("Clear")}
+                      isLoading={isUploading}
+                      onDataChange={(_, value) => setSSHKey(value)}
+                      onTextChange={(_, value) => setSSHKey(value)}
+                      onReadStarted={startUploading}
+                      onReadFinished={stopUploading}
+                      onClearClick={clearKey}
+                    />
+                  </FormGroup>
+                </Flex>
+              </Form>
             </Page.Section>
           </GridItem>
         </Grid>
       </Page.Content>
+      <Page.Actions>
+        <Page.Submit form="rootAuthMethods" onClick={accept} />
+      </Page.Actions>
     </Page>
   );
 }
