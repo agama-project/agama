815415dab66734f542fddf8218cf773e4bb9e16f
David Díaz González <dgonzalez@suse.com>
2024-12-30 14:15:22 +0000

    refactor(web): migrate hooks to TypeScript

--- web/src/hooks/useNodeSiblings.ts
+++ web/src/hooks/useNodeSiblings.ts
@@ -1,42 +1,26 @@
 import { noop } from "~/utils";
 
 /**
- * Function for adding an attribute to a sibling
- *
- * @typedef {function} addAttributeFn
- * @param {string} attribute - attribute name
- * @param {*} value - value to set
- */
-
-/**
- * Function for removing an attribute from a sibling
- *
- * @typedef {function} removeAttributeFn
- * @param {string} attribute - attribute name
- */
-
-/**
  * A hook for working with siblings of the node passed as parameter
  *
  * It returns an array with exactly two functions:
  *   - First for adding given attribute to siblings
  *   - Second for removing given attributes from siblings
- *
- * @param {HTMLElement} node
- * @returns {[addAttributeFn, removeAttributeFn]}
  */
-const useNodeSiblings = (node) => {
+const useNodeSiblings = (
+  node: HTMLElement,
+): [(attribute: string, value) => void, (attribute: string) => void] => {
   if (!node) return [noop, noop];
 
   const siblings = [...node.parentNode.children].filter((n) => n !== node);
 
-  const addAttribute = (attribute, value) => {
+  const addAttribute = (attribute: string, value) => {
     siblings.forEach((sibling) => {
       sibling.setAttribute(attribute, value);
     });
   };
 
-  const removeAttribute = (attribute) => {
+  const removeAttribute = (attribute: string) => {
     siblings.forEach((sibling) => {
       sibling.removeAttribute(attribute);
     });
