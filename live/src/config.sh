aa696e39d2b5830787843121bc837b159de82899
Michal Filka <mfilka@suse.cz>
2025-01-04 22:09:10 +0100

    Boot GNOME Shell & autologin root

--- live/src/config.sh
+++ live/src/config.sh
@@ -59,7 +59,6 @@ systemctl enable live-root-shell.service
 systemctl enable checkmedia.service
 systemctl enable qemu-guest-agent.service
 systemctl enable setup-systemd-proxy-env.path
-systemctl enable x11-autologin.service
 systemctl enable spice-vdagentd.service
 systemctl enable zramswap
 
