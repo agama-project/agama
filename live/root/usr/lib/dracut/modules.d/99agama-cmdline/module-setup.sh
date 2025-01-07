9aedc04976eefb2d42c9a3f72e18c91a0b775a58
Knut Anderssen <kanderssen@suse.com>
2025-01-07 10:18:35 +0000

    Initial implementation for agama cmdline conf

---
+++ live/root/usr/lib/dracut/modules.d/99agama-cmdline/module-setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# called by dracut
+check() {
+	return 0
+}
+
+# called by dracut
+depends() {
+	return 0
+}
+
+installkernel() {
+	return 0
+}
+
+# called by dracut
+install() {
+	inst_hook cmdline 99 "$moddir/agama-cmdline-conf.sh"
+	inst_hook pre-pivot 99 "$moddir/save-agama-conf.sh"
+}
