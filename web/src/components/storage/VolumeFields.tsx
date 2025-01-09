9f849dd3581022da9d4fc43b4adf697d2c80878b
David Díaz González <dgonzalez@suse.com>
2025-01-09 08:44:44 +0000

    fix(web): rename labelIcon to labelHelp
    As part of the PF v6 migration.

--- web/src/components/storage/VolumeFields.tsx
+++ web/src/components/storage/VolumeFields.tsx
@@ -258,7 +258,7 @@ const FsField = ({ value, volume, isDisabled = false, onChange }: FsFieldProps) 
   }
 
   return (
-    <FormGroup isRequired label={label} labelIcon={<Info />} fieldId="fsType">
+    <FormGroup isRequired label={label} labelHelp={<Info />} fieldId="fsType">
       <FsSelect
         id="fsType"
         value={value}
