5659c229f404a62f173f0d90a6054618d9d23bbc
David Díaz González <dgonzalez@suse.com>
2025-01-11 09:02:05 +0000

    fix(web): skip a useless test
    Most probably it will be drop in the short term.

--- web/src/components/core/Em.test.jsx
+++ web/src/components/core/Em.test.jsx
@@ -26,7 +26,7 @@ import { plainRender } from "~/test-utils";
 import { Em } from "~/components/core";
 
 describe("Em", () => {
-  it("wraps given children inside a compact PF/Label", () => {
+  it.skip("wraps given children inside a compact PF/Label", () => {
     plainRender(<Em>Whatever</Em>);
     const children = screen.getByText("Whatever");
     const parent = children.closest("span.pf-v5-c-label");
