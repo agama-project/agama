10a9a8f7164cc3d613dca5e345e6e8ac77fdfd7d
Josef Reidinger <jreidinger@suse.cz>
2025-01-17 16:54:37 +0100

    first version of info expansion script

---
+++ live/root/usr/bin/info-cmdline-conf.sh
@@ -0,0 +1,25 @@
+#! /bin/sh
+
+set -e
+
+TARGET="${1:-/etc/agama.d/cmdline.conf}"
+INFO_CONTENT="${2:-/etc/agama.d/cmdline.info.conf}"
+
+expand_info_arg() {
+  INFO_URL=$(sed -n 's/.*[ ^]info=\([^[:space:]]\+\).*/\1/p' "$TARGET")
+  if [ -z "${INFO_URL}" ]; then
+    return 0
+  fi
+
+  curl --silent "${INFO_URL}" > "${INFO_CONTENT}"
+  # remove info param
+  sed -in 's/[ ^]info=[^[:space:]]\+//' "${TARGET}"
+  # and add content of info file
+  cat "${INFO_CONTENT}"
+  cat "${INFO_CONTENT}" >> "${TARGET}"
+  cat "${INFO_CONTENT}"
+
+  return 0
+}
+
+expand_info_arg
