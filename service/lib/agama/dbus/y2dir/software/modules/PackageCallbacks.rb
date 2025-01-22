e8b00a1040a14485ca0864414f14e7d1ab1546de
Imobach González Sosa <igonzalezsosa@suse.com>
2025-01-22 11:00:02 +0000

    feat(ruby): implement the SourceCreateError callback

--- service/lib/agama/dbus/y2dir/software/modules/PackageCallbacks.rb
+++ service/lib/agama/dbus/y2dir/software/modules/PackageCallbacks.rb
@@ -34,15 +34,19 @@ module Yast
     def InitPackageCallbacks(logger = nil)
       @logger = logger || ::Logger.new($stdout)
 
-      Agama::Software::Callbacks::Signature.new(
-        questions_client, logger
-      ).setup
-
       Agama::Software::Callbacks::Media.new(
         questions_client, logger
       ).setup
 
       Agama::Software::Callbacks::Provide.new(
+        questions_client, logger
+      ).setup
+
+      Agama::Software::Callbacks::Signature.new(
+        questions_client, logger
+      ).setup
+
+      Agama::Software::Callbacks::Source.new(
         questions_client, logger
       ).setup
     end
---
+++ service/test/agama/software/callbacks/source_test.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+# Copyright (c) [2022-2023] SUSE LLC
+#
+# All Rights Reserved.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of version 2 of the GNU General Public License as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, contact SUSE LLC.
+#
+# To contact SUSE LLC about this file by physical or electronic mail, you may
+# find current contact information at www.suse.com.
+
+require_relative "../../../test_helper"
+require "agama/software/callbacks/source"
+require "agama/dbus/clients/questions"
+require "agama/dbus/clients/question"
+
+describe Agama::Software::Callbacks::Source do
+  subject { described_class.new(questions_client, logger) }
+
+  let(:questions_client) { instance_double(Agama::DBus::Clients::Questions) }
+
+  let(:logger) { Logger.new($stdout, level: :warn) }
+
+  let(:answer) { :Retry }
+
+  describe "#source_create_error" do
+    before do
+      allow(questions_client).to receive(:ask).and_yield(question_client)
+      allow(question_client).to receive(:answer).and_return(answer)
+    end
+
+    let(:question_client) { instance_double(Agama::DBus::Clients::Question) }
+
+    it "registers a question with the details" do
+      expect(questions_client).to_not receive(:ask)
+      subject.source_create_error("https://example.net/repo", :NOT_FOUND, "Some description")
+    end
+
+    context "when the user answers :Retry" do
+      let(:answer) { :Retry }
+
+      it "returns 'R'" do
+        ret = subject.source_create_error("https://example.net/repo", :NOT_FOUND,
+          "Some description")
+        expect(ret).to eq(:RETRY)
+      end
+    end
+  end
+end
