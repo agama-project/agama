86721b6932f3cc3b2264910e60278f00f29e86e3
Imobach González Sosa <igonzalezsosa@suse.com>
2025-01-22 11:32:52 +0000

    feat(ruby): implement the ScriptProblem callback

--- service/lib/agama/software/callbacks.rb
+++ service/lib/agama/software/callbacks.rb
@@ -30,5 +30,6 @@ end
 require "agama/software/callbacks/media"
 require "agama/software/callbacks/progress"
 require "agama/software/callbacks/provide"
+require "agama/software/callbacks/script"
 require "agama/software/callbacks/signature"
 require "agama/software/callbacks/source"
