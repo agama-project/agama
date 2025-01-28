b293f64e9c396f309ae091d00acd7aa149ca012f
José Iván López González <jlopez@suse.com>
2025-01-28 12:34:04 +0000

    web: add SelectToggle component

---
+++ web/src/components/core/SelectToggle.tsx
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) [2025] SUSE LLC
+ *
+ * All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, contact SUSE LLC.
+ *
+ * To contact SUSE LLC about this file by physical or electronic mail, you may
+ * find current contact information at www.suse.com.
+ */
+
+import React from "react";
+import { Select, MenuToggle, MenuToggleElement } from "@patternfly/react-core";
+
+export type SelectToggleProps = {
+  value: number | string;
+  label?: React.ReactNode;
+  onChange?: (value: number | string) => void;
+  isDisabled?: boolean;
+  children?: React.ReactNode;
+};
+
+export default function SelectToggle({
+  value,
+  label,
+  onChange,
+  isDisabled = false,
+  children,
+}: SelectToggleProps): React.ReactElement {
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  const onToggleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const onSelect = (
+    _event: React.MouseEvent<Element, MouseEvent> | undefined,
+    value: string | number | undefined,
+  ) => {
+    setIsOpen(false);
+    onChange && onChange(value as string);
+  };
+
+  const toggle = (toggleRef: React.Ref<MenuToggleElement>) => {
+    return (
+      <MenuToggle
+        ref={toggleRef}
+        onClick={onToggleClick}
+        isExpanded={isOpen}
+        isDisabled={isDisabled}
+      >
+        {label || value}
+      </MenuToggle>
+    );
+  };
+
+  return (
+    <Select
+      id="option-variations-select"
+      isOpen={isOpen}
+      selected={value}
+      onSelect={onSelect}
+      onOpenChange={(isOpen) => setIsOpen(isOpen)}
+      toggle={toggle}
+      shouldFocusToggleOnSelect
+    >
+      {children}
+    </Select>
+  );
+}
