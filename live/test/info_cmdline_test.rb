780ab8255b0b5df840e8aff57d1efa80cd9015da
Josef Reidinger <jreidinger@suse.cz>
2025-02-20 16:54:53 +0100

    fix test and also test failure

--- live/test/info_cmdline_test.rb
+++ live/test/info_cmdline_test.rb
@@ -1,5 +1,6 @@
 #! /usr/bin/rspec
 require "tmpdir"
+require "cheetah"
 
 describe "info-cmdline-conf.sh" do
   let(:script_path) { File.expand_path("../root/usr/bin/info-cmdline-conf.sh", __dir__, ) }
@@ -14,9 +15,7 @@ describe "info-cmdline-conf.sh" do
         target_path = File.join(tmpdir, "cmdline")
         FileUtils.cp(source_path, target_path)
         info_path = File.join(tmpdir, "cmdline.info")
-        command = "#{script_path} #{target_path} #{info_path}"
-        cmd_result = system(command)
-        expect(cmd_result).to eq true
+        Cheetah.run(script_path, target_path, info_path)
         expected = File.read(expected_path)
         result = File.read(target_path)
         expect(result).to eq expected
@@ -35,9 +34,7 @@ describe "info-cmdline-conf.sh" do
         target_path = File.join(tmpdir, "cmdline")
         FileUtils.cp(source_path, target_path)
         info_path = File.join(tmpdir, "cmdline.info")
-        command = "#{script_path} #{target_path} #{info_path}"
-        cmd_result = system(command)
-        expect(cmd_result).to eq true
+        Cheetah.run(script_path, target_path, info_path)
         expected = File.read(expected_path)
         result = File.read(target_path)
         expect(result).to eq expected
