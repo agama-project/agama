b2d15758fac2ed3ec958dd0cd67e0894aa7d73e1
Knut Anderssen <kanderssen@suse.com>
2025-03-11 11:19:19 +0000

    Allow to set the persistent hostname through cmdline

--- live/root/usr/lib/dracut/modules.d/99agama-cmdline/module-setup.sh
+++ live/root/usr/lib/dracut/modules.d/99agama-cmdline/module-setup.sh
@@ -18,4 +18,5 @@ installkernel() {
 install() {
   inst_hook cmdline 99 "$moddir/agama-cmdline-conf.sh"
   inst_hook cmdline 99 "$moddir/agama-network-compat.sh"
+  inst_hook pre-pivot 99 "$moddir/save-agama-conf.sh"
 }
