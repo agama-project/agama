1ca34bdd1e73b6d11c4b488b3d9a10cbfa0ff13f
José Iván López González <jlopez@suse.com>
2025-03-11 11:14:04 +0000

    fix(storage): improve boot config solver

--- service/lib/agama/storage/config_solvers/boot.rb
+++ service/lib/agama/storage/config_solvers/boot.rb
@@ -39,49 +39,53 @@ module Agama
       private
 
         # Finds a device for booting and sets its alias, if needed.
+        #
+        # A boot device cannot be automatically inferred in the following scenarios:
+        #   * The root partition or logical volume is missing.
+        #   * A disk is directly formated and mounted as root.
+        #   * The volume group allocating the root logical volume uses whole drives as physical
+        #     volumes.
         def solve_device_alias
           return unless config.boot.configure? && config.boot.device.default?
 
-          drive_config = root_drive_config
-          return unless drive_config
+          drive = root_drive
+          return unless drive
 
-          drive_config.ensure_alias
-          config.boot.device.device_alias = drive_config.alias
+          drive.ensure_alias
+          config.boot.device.device_alias = drive.alias
         end
 
         # Config of the drive used for allocating root, directly or inderectly.
         #
         # @return [Configs::Drive, nil] nil if the boot device cannot be inferred from the config.
-        def root_drive_config
-          drive_config = config.drives.find { |d| root_drive_config?(d) }
-
-          drive_config || root_lvm_device_config
+        def root_drive
+          drive = config.drives.find { |d| root_drive?(d) }
+          drive || root_lvm_device
         end
 
-        # Config of the first drive used to allocate the root volume group config, if any.
+        # Config of the first drive used for allocating the physical volumes of the root volume
+        # group.
         #
         # @return [Configs::Drive, nil]
-        def root_lvm_device_config
-          volume_group_config = root_volume_group_config
-          return unless volume_group_config
+        def root_lvm_device
+          volume_group = root_volume_group
+          return unless volume_group
 
-          config.drives
-            .select { |d| candidate_for_physical_volumes?(d, volume_group_config) }
-            .first
+          first_target_lvm_device(volume_group) || first_physical_volume_device(volume_group)
         end
 
         # Config of the volume group containing the root logical volume, if any.
         #
         # @return [Configs::VolumeGroup, nil]
-        def root_volume_group_config
-          config.volume_groups.find { |v| root_volume_group_config?(v) }
+        def root_volume_group
+          config.volume_groups.find { |v| root_volume_group?(v) }
         end
 
         # Whether the given drive config contains a root partition config.
         #
         # @param config [Configs::Drive]
         # @return [Boolean]
-        def root_drive_config?(config)
+        def root_drive?(config)
           config.partitions.any?(&:root?)
         end
 
@@ -89,22 +93,40 @@ module Agama
         #
         # @param config [Configs::VolumeGroup]
         # @return [Boolean]
-        def root_volume_group_config?(config)
+        def root_volume_group?(config)
           config.logical_volumes.any?(&:root?)
         end
 
-        # Whether the given drive config can be used to allocate physcial volumes.
+        # Config of the first target device for creating physical volumes.
         #
-        # @param drive [Configs::Drive]
-        # @param volume_group [Configs::VolumeGroup]
-        #
-        # @return [Boolean]
-        def candidate_for_physical_volumes?(drive, volume_group)
-          return true if volume_group.physical_volumes_devices.any? { |d| drive.alias?(d) }
+        # @param config [Configs::VolumeGroup]
+        # @return [Configs::Drive, nil]
+        def first_target_lvm_device(config)
+          device_alias = config.physical_volumes_devices.first
+          return unless device_alias
 
-          volume_group.physical_volumes.any? do |pv|
-            drive.partitions.any? { |p| p.alias?(pv) }
+          self.config.drives.find { |d| d.alias?(device_alias) }
+        end
+
+        # Config of the device of the first partition used as physical volume.
+        #
+        # @param config [Configs::VolumeGroup]
+        # @return [Configs::Drive, nil]
+        def first_physical_volume_device(config)
+          device_alias = config.physical_volumes.find { |p| partition_alias?(p) }
+          return unless device_alias
+
+          self.config.drives.find do |drive|
+            drive.partitions.any? { |p| p.alias?(device_alias) }
           end
+        end
+
+        # Whether there is a partition with the given alias.
+        #
+        # @param device_alias [String]
+        # @return [Boolean]
+        def partition_alias?(device_alias)
+          config.partitions.any? { |p| p.alias?(device_alias) }
         end
       end
     end
