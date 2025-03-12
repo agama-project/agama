b5b470338d38b67952dd92dbad6180c584996da9
José Iván López González <jlopez@suse.com>
2025-03-12 12:42:37 +0000

    feat(storage): improve code to generate config from model

--- service/lib/agama/storage/config_conversions/from_model_conversions/config.rb
+++ service/lib/agama/storage/config_conversions/from_model_conversions/config.rb
@@ -49,37 +49,30 @@ module Agama
           end
 
           # @see Base#conversions
-          # The conversion of boot and drives is special because the relationship between them.
-          #
-          # A boot model can indicates a boot device name. If so, the following steps are needed:
-          # * Add a drive model for the boot device if there is no drive model for it, see
-          #   {#convert_drives}.
-          # * Add an alias to the drive config of the boot device if it has no alias, and set that
-          #   alias to the boot device config, see #{convert_boot_device_alias}.
-          #
           # @return [Hash]
           def conversions
-            boot_config = convert_boot
-            drive_configs = convert_drives
-            drive_config = boot_drive_config(drive_configs)
-
-            convert_boot_device_alias(boot_config, drive_config)
+            drives = convert_drives
 
             {
-              boot:   boot_config,
-              drives: drive_configs
+              boot:   convert_boot(drives || []),
+              drives: drives
             }
           end
 
+          # @param drives [Array<Configs::Drive>]
           # @return [Configs::Boot, nil]
-          def convert_boot
+          def convert_boot(drives)
             boot_model = model_json[:boot]
             return unless boot_model
 
-            FromModelConversions::Boot.new(boot_model).convert
+            FromModelConversions::Boot.new(boot_model, drives).convert
           end
 
+          # @return [Array<Configs::Drive>, nil]
           def convert_drives
+            add_missing_drives
+
+            drive_models = model_json[:drives]
             return unless drive_models
 
             drive_models.map { |d| convert_drive(d) }
@@ -93,87 +86,72 @@ module Agama
               .convert
           end
 
-          # Conversion for the boot device alias.
+          # Add missing drives to the model.
           #
-          # It requieres both boot and drives already converted.
-          #
-          # @param boot_config [Configs::Boot, nil] The boot config can be modified.
-          # @param drive_config [Configs::Drive, nil] The drive config can be modifed.
-          def convert_boot_device_alias(boot_config, drive_config)
-            return unless boot_config && drive_config
-            return unless boot_config.configure? && !boot_config.device.default?
-
-            drive_config.ensure_alias
-            boot_config.device.device_alias = drive_config.alias
-          end
-
-          # Drive config for the boot device, if any.
-          #
-          # @param drive_configs [Array<Configs::Drive>, nil]
-          # @return [Configs::Drive, nil]
-          def boot_drive_config(drive_configs)
-            return unless drive_configs && boot_device_name
-
-            drive_configs.find { |d| d.search.name == boot_device_name }
-          end
-
-          # Drive models to convert to drive configs.
-          #
-          # It includes all the drives from the drive section of the model, adding a drive for the
-          # selected boot device if needed. See {#calculate_drive_models}.
+          # Adds a drive for the selected boot device and for the LVM target devices if needed.
           #
           # @return [Array<Hash>, nil]
-          def drive_models
-            return @drive_models if @calculated_drive_models
+          def add_missing_drives
+            model_json[:drives] ||= []
+            drives = model_json[:drives]
 
-            @drive_models = calculate_drive_models
+            # Add boot device, if needed.
+            drives << boot_device if missing_boot_device?
+
+            # Add target devices, if needed.
+            lvm_target_names.each do |name|
+              drives << lvm_target_device(name) if missing_drive?(name)
+            end
           end
 
-          # @see #drive_models
-          # @return [Array<Hash>, nil]
-          def calculate_drive_models
-            @calculated_drive_models = true
-
-            models = model_json[:drives]
-            return if models.nil? && !missing_boot_drive?
-
-            models ||= []
-            # The main use case for using a specific device for booting is to share the boot
-            # partition with other installed systems. So let's ensure the partitions are not deleted
-            # by setting the "keep" space policy.
-            models << { name: boot_device_name, spacePolicy: "keep" } if missing_boot_drive?
-            models
-          end
-
-          # Whether a drive model for the boot device is missing in the list of drives. See
-          # {#calculate_missing_boot_device}.
+          # Whether the boot drive is missing in the model.
           #
           # @return [Boolean]
-          def missing_boot_drive?
-            return @missing_boot_drive if @calculated_missing_boot_drive
-
-            @missing_boot_drive ||= calculate_missing_boot_drive
-          end
-
-          # @see #missing_boot_drive?
-          # @return [Boolean]
-          def calculate_missing_boot_drive
-            @calculated_missing_boot_drive = true
-
+          def missing_boot_device?
             configure_boot = model_json.dig(:boot, :configure)
             default_boot = model_json.dig(:boot, :device, :default)
+            boot_device_name = model_json.dig(:boot, :device, :name)
 
             return false unless configure_boot && !default_boot && !boot_device_name.nil?
 
-            drive_models = model_json[:drives] || []
-            drive_models.none? { |d| d[:name] == boot_device_name }
+            missing_drive?(boot_device_name)
           end
 
-          # Name of the device selected for booting, if any.
+          # Whether a drive with the given name is missing in the model.
           #
-          # @return [String, nil]
-          def boot_device_name
-            @boot_device_name ||= model_json.dig(:boot, :device, :name)
+          # @param name [String]
+          # @return [Boolean]
+          def missing_drive?(name)
+            drives = model_json[:drives] || []
+            drives.none? { |d| d[:name] == name }
+          end
+
+          # All the target devices for creating LVM physical volumes.
+          #
+          # @return [Array<String>]
+          def lvm_target_names
+            volume_groups = model_json[:volumeGroups] || []
+            volume_groups.flat_map { |v| v[:targetDevices] || [] }
+          end
+
+          # Drive model for the boot device.
+          #
+          # @return [Hash]
+          def boot_device
+            name = model_json.dig(:boot, :device, :name)
+
+            # The main use case for using a specific device for booting is to share the boot
+            # partition with other installed systems. So let's ensure the partitions are not deleted
+            # by setting the "keep" space policy.
+            { name: name, spacePolicy: "keep" }
+          end
+
+          # Drive model for a LVM target device.
+          #
+          # @param name [String]
+          # @return [Hash]
+          def lvm_target_device(name)
+            { name: name, spacePolicy: product_config.space_policy }
           end
         end
       end
