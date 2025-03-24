0633d14f96de6cbd818437155673b90586c0eaf6
David Díaz González <dgonzalez@suse.com>
2025-03-24 13:17:32 +0000

    fix(web): use Alert instead of EmptyState
    Use Alert to warn users that no configuration has been set yet. If
    possible, we would like to reserve EmptyState for situations where it
    occupies the whole page.

--- web/src/components/storage/ConfigEditor.tsx
+++ web/src/components/storage/ConfigEditor.tsx
@@ -26,17 +26,20 @@ import { useDevices } from "~/queries/storage";
 import { useConfigModel } from "~/queries/storage/config-model";
 import DriveEditor from "~/components/storage/DriveEditor";
 import VolumeGroupEditor from "~/components/storage/VolumeGroupEditor";
-import { List, ListItem, EmptyState } from "@patternfly/react-core";
+import { Alert, List, ListItem } from "@patternfly/react-core";
 
 export default function ConfigEditor() {
   const model = useConfigModel({ suspense: true });
   const devices = useDevices("system", { suspense: true });
-
   const drives = model.drives || [];
   const volumeGroups = model.volumeGroups || [];
 
   if (!drives.length && !volumeGroups.length) {
-    return <EmptyState titleText={_("Select a device for installing the system")} status="info" />;
+    return (
+      <Alert title={_("No configuration set")} variant="custom" isInline>
+        {_("Use the actions below to get started.")}
+      </Alert>
+    );
   }
 
   return (
