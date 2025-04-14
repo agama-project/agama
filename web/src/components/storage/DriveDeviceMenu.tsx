dcc5e38d6693c1143499e253a76e046bb7fe6a62
José Iván López González <jlopez@suse.com>
2025-04-14 12:00:45 +0100

    web: adapt DriveDeviceMenu to use MenuButton

--- web/src/components/storage/DriveDeviceMenu.tsx
+++ web/src/components/storage/DriveDeviceMenu.tsx
@@ -20,32 +20,22 @@
  * find current contact information at www.suse.com.
  */
 
-import React, { useRef, useState } from "react";
-import {
-  MenuToggle,
-  Split,
-  Flex,
-  Label,
-  DrilldownMenu,
-  MenuContent,
-  Divider,
-  MenuContainer,
-  Menu,
-  MenuList,
-  MenuItem,
-} from "@patternfly/react-core";
+import React from "react";
+import { Split, Flex, Label } from "@patternfly/react-core";
+import MenuButton, { MenuButtonItem } from "~/components/core/MenuButton";
 import MenuDeviceDescription from "./MenuDeviceDescription";
 import { useAvailableDevices } from "~/queries/storage";
 import { useDrive, useModel } from "~/queries/storage/config-model";
 import { useDrive as useDriveModel } from "~/hooks/storage/drive";
-import { useModel as useModelHook } from "~/hooks/storage/model";
 import { useConvertToVolumeGroup } from "~/hooks/storage/volume-group";
 import * as driveUtils from "~/components/storage/utils/drive";
 import { deviceBaseName, deviceLabel, formattedPath } from "~/components/storage/utils";
+import { apiModel } from "~/api/storage/types";
+import { StorageDevice } from "~/types/storage";
 import { sprintf } from "sprintf-js";
 import { _, n_, formatList } from "~/i18n";
 
-const UseOnlyOneOption = (drive) => {
+const UseOnlyOneOption = (drive: apiModel.Drive): boolean => {
   const driveModel = useDrive(drive.name);
   if (!driveModel) return false;
 
@@ -55,7 +45,12 @@ const UseOnlyOneOption = (drive) => {
   return driveUtils.hasReuse(drive);
 };
 
-const DiskSelectorTitle = ({ device, isSelected = false }) => {
+type DiskSelectorTitleProps = { device: StorageDevice; isSelected: boolean };
+
+const DiskSelectorTitle = ({
+  device,
+  isSelected = false,
+}: DiskSelectorTitleProps): React.ReactNode => {
   const Name = () => (isSelected ? <b>{deviceLabel(device)}</b> : deviceLabel(device));
   const Systems = () => (
     <Flex columnGap={{ default: "columnGapXs" }}>
@@ -75,58 +70,72 @@ const DiskSelectorTitle = ({ device, isSelected = false }) => {
   );
 };
 
-const SearchSelectorMultipleOptions = ({ selected, onChange }) => {
-  const devices = useAvailableDevices();
+const searchSelectorMultipleOptions = (
+  devices: StorageDevice[],
+  selected: StorageDevice,
+  onChange: (name: StorageDevice["name"]) => void,
+): React.ReactNode[] => {
+  return devices.map((device) => {
+    const isSelected = device.sid === selected.sid;
 
-  return (
-    <>
-      {devices.map((device) => {
-        const isSelected = device.sid === selected.sid;
-
-        return (
-          <MenuItem
-            key={device.sid}
-            itemId={device.sid}
-            isSelected={isSelected}
-            description={<MenuDeviceDescription device={device} />}
-            onClick={() => onChange(device.name)}
-          >
-            <DiskSelectorTitle device={device} isSelected={isSelected} />
-          </MenuItem>
-        );
-      })}
-    </>
-  );
+    return (
+      <MenuButtonItem
+        key={device.sid}
+        itemId={device.sid}
+        isSelected={isSelected}
+        description={<MenuDeviceDescription device={device} />}
+        onClick={() => onChange(device.name)}
+      >
+        <DiskSelectorTitle device={device} isSelected={isSelected} />
+      </MenuButtonItem>
+    );
+  });
 };
 
-const SearchSelectorSingleOption = ({ selected }) => {
+const SearchSelectorSingleOption = ({ selected }: { selected: StorageDevice }): React.ReactNode => {
   return (
-    <MenuItem
+    <MenuButtonItem
       isSelected
       key={selected.sid}
       itemId={selected.sid}
       description={<MenuDeviceDescription device={selected} />}
     >
       <DiskSelectorTitle device={selected} isSelected />
-    </MenuItem>
+    </MenuButtonItem>
   );
 };
 
-const SearchSelectorOptions = ({ drive, selected, onChange }) => {
+const searchSelectorOptions = (
+  drive: apiModel.Drive,
+  devices: StorageDevice[],
+  selected: StorageDevice,
+  onChange: (name: StorageDevice["name"]) => void,
+): React.ReactNode[] => {
   const onlyOneOption = UseOnlyOneOption(drive);
 
-  if (onlyOneOption) return <SearchSelectorSingleOption selected={selected} />;
+  if (onlyOneOption) return [<SearchSelectorSingleOption key="disk-option" selected={selected} />];
 
-  return <SearchSelectorMultipleOptions selected={selected} onChange={onChange} />;
+  return searchSelectorMultipleOptions(devices, selected, onChange);
+};
+
+type DisksDrillDownMenuItemProps = {
+  drive: apiModel.Drive;
+  selected: StorageDevice;
+  onDeviceClick: (name: StorageDevice["name"]) => void;
 };
 
 /**
  * Internal component holding the logic for rendering the disks drilldown menu
  */
-const DisksDrillDownMenuItem = ({ drive, selected, onDeviceClick }) => {
+const DisksDrillDownMenuItem = ({
+  drive,
+  selected,
+  onDeviceClick,
+}: DisksDrillDownMenuItemProps): React.ReactNode => {
   /** @todo Replace the useDrive hook from /queries by the hook from /hooks. */
   const volumeGroups = useDriveModel(drive.name)?.getVolumeGroups() || [];
   const onlyOneOption = UseOnlyOneOption(drive);
+  const devices = useAvailableDevices();
   const driveModel = useDrive(drive.name);
   if (!driveModel) return;
 
@@ -159,7 +168,7 @@ const DisksDrillDownMenuItem = ({ drive, selected, onDeviceClick }) => {
   };
 
   const extraText = (): string => {
-    const name = deviceBaseName(drive, 20);
+    const name = deviceBaseName(selected, 20);
 
     if (driveUtils.hasReuse(drive)) {
       // The current device will be the only option to choose from
@@ -237,26 +246,18 @@ const DisksDrillDownMenuItem = ({ drive, selected, onDeviceClick }) => {
   };
 
   return (
-    <MenuItem
-      itemId="group:disks-menu"
-      direction="down"
+    <MenuButtonItem
       description={extraText()}
-      drilldownMenu={
-        <DrilldownMenu id="disks-menu">
-          <MenuItem itemId="group:disks-menu-back" direction="up">
-            {_("Back")}
-          </MenuItem>
-          <Divider />
-          <SearchSelectorOptions drive={drive} selected={selected} onChange={onDeviceClick} />
-        </DrilldownMenu>
-      }
+      items={[searchSelectorOptions(drive, devices, selected, onDeviceClick)]}
     >
       {mainText()}
-    </MenuItem>
+    </MenuButtonItem>
   );
 };
 
-const RemoveDriveOption = ({ drive }) => {
+type RemoveDriveOptionProps = { drive: apiModel.Drive };
+
+const RemoveDriveOption = ({ drive }: RemoveDriveOptionProps): React.ReactNode => {
   const driveModel = useDrive(drive.name);
   const { hasAdditionalDrives } = useModel();
 
@@ -286,7 +287,7 @@ const RemoveDriveOption = ({ drive }) => {
   }
 
   return (
-    <MenuItem
+    <MenuButtonItem
       key="delete"
       isDanger
       isDisabled={isDisabled}
@@ -294,24 +295,26 @@ const RemoveDriveOption = ({ drive }) => {
       onClick={deleteDrive}
     >
       {_("Do not use")}
-    </MenuItem>
+    </MenuButtonItem>
   );
 };
 
-const NewVgOption = ({ drive }) => {
+type NewVgOptionProps = DriveDeviceMenuProps;
+
+const NewVgOption = ({ drive, selected }: NewVgOptionProps): React.ReactNode => {
   const convertToVg = useConvertToVolumeGroup();
-  const model = useModelHook();
-  const vgs = model.volumeGroups.filter((vg) => vg.targetDevices.includes(drive.name));
+  /** @todo Replace the useDrive hook from /queries by the hook from /hooks. */
+  const vgs = useDriveModel(drive.name)?.getVolumeGroups() || [];
   const mountPaths = drive.partitions.filter((p) => !p.name).map((p) => formattedPath(p.mountPath));
 
   const titleText = () => {
     if (vgs.length) {
       // TRANSLATORS: %s is the short name of a disk, like 'sda'
-      return sprintf(_("Create another LVM volume group on %s"), deviceBaseName(drive, 20));
+      return sprintf(_("Create another LVM volume group on %s"), deviceBaseName(selected, 20));
     }
 
     // TRANSLATORS: %s is the short name of a disk, like 'sda'
-    return sprintf(_("Create LVM volume group on %s"), deviceBaseName(drive, 20));
+    return sprintf(_("Create LVM volume group on %s"), deviceBaseName(selected, 20));
   };
 
   const descriptionText = () => {
@@ -330,7 +333,7 @@ const NewVgOption = ({ drive }) => {
   };
 
   return (
-    <MenuItem
+    <MenuButtonItem
       component="a"
       onClick={() => convertToVg(drive.name)}
       itemId="lvm"
@@ -339,9 +342,11 @@ const NewVgOption = ({ drive }) => {
       <Flex component="span" justifyContent={{ default: "justifyContentSpaceBetween" }}>
         <span>{titleText()}</span>
       </Flex>
-    </MenuItem>
+    </MenuButtonItem>
   );
 };
+
+export type DriveDeviceMenuProps = { drive: apiModel.Drive; selected: StorageDevice };
 
 /**
  * Menu that provides options for users to configure the device used by a given drive.
@@ -349,103 +354,33 @@ const NewVgOption = ({ drive }) => {
  * It uses a drilled-down menu approach for disks, making the available options less
  * overwhelming by presenting them in a more organized manner.
  */
-export default function DriveDeviceMenu({ drive, selected }) {
-  const menuRef = useRef();
-  const toggleRef = useRef();
-  const [isOpen, setIsOpen] = useState(false);
-  const [menuDrilledIn, setMenuDrilledIn] = React.useState<string[]>([]);
-  const [drilldownPath, setDrilldownPath] = React.useState<string[]>([]);
-  const [activeMenu, setActiveMenu] = React.useState<string>("root");
-  const [menuHeights, setMenuHeights] = React.useState({});
-
-  const resetState = () => {
-    setMenuDrilledIn([]);
-    setDrilldownPath([]);
-    setActiveMenu("root");
-  };
-
-  const toggle = () => {
-    setIsOpen(!isOpen);
-    resetState();
-  };
-
+export default function DriveDeviceMenu({
+  drive,
+  selected,
+}: DriveDeviceMenuProps): React.ReactNode {
   const driveHandler = useDrive(drive.name);
   const changeDriveTarget = (newDriveName: string) => {
     driveHandler.switch(newDriveName);
-    setIsOpen(false);
-    resetState();
-  };
-
-  const drillIn = (
-    _: React.KeyboardEvent | React.MouseEvent,
-    fromMenuId: string,
-    toMenuId: string,
-    pathId: string,
-  ) => {
-    setMenuDrilledIn([...menuDrilledIn, fromMenuId]);
-    setDrilldownPath([...drilldownPath, pathId]);
-    setActiveMenu(toMenuId);
-  };
-
-  const drillOut = (_: React.KeyboardEvent | React.MouseEvent, toMenuId: string) => {
-    const menuDrilledInSansLast = menuDrilledIn.slice(0, menuDrilledIn.length - 1);
-    const pathSansLast = drilldownPath.slice(0, drilldownPath.length - 1);
-    setMenuDrilledIn(menuDrilledInSansLast);
-    setDrilldownPath(pathSansLast);
-    setActiveMenu(toMenuId);
-  };
-
-  const setHeight = (menuId: string, height: number) => {
-    // FIXME: look for a better way to avoid test crashing because of this method
-    if (process.env.NODE_ENV === "test") return;
-
-    if (
-      menuHeights[menuId] === undefined ||
-      (menuId !== "root" && menuHeights[menuId] !== height)
-    ) {
-      setMenuHeights({ ...menuHeights, [menuId]: height });
-    }
   };
 
   return (
-    <MenuContainer
-      isOpen={isOpen}
-      onOpenChange={toggle}
-      toggleRef={toggleRef}
-      toggle={
-        <MenuToggle ref={toggleRef} onClick={toggle} isExpanded={isOpen}>
-          <b aria-hidden>{deviceLabel(selected, 20)}</b>
-        </MenuToggle>
-      }
-      menuRef={menuRef}
-      menu={
-        <Menu
-          id="root"
-          containsDrilldown
-          onDrillIn={drillIn}
-          onDrillOut={drillOut}
-          onGetMenuHeight={setHeight}
-          activeMenu={activeMenu}
-          drilledInMenus={menuDrilledIn}
-          drilldownItemPath={drilldownPath}
-          ref={menuRef}
-          onSelect={(_e, id) => {
-            if (!String(id).startsWith("group:disks-menu")) setIsOpen(false);
-          }}
-        >
-          <MenuContent menuHeight={`${menuHeights[activeMenu]}px`}>
-            <MenuList>
-              <DisksDrillDownMenuItem
-                drive={drive}
-                selected={selected}
-                onDeviceClick={changeDriveTarget}
-              />
-              <NewVgOption drive={drive} />
-              <RemoveDriveOption drive={drive} />
-            </MenuList>
-          </MenuContent>
-        </Menu>
-      }
-    />
+    <MenuButton
+      menuProps={{
+        "aria-label": sprintf(_("Device %s menu"), drive.name),
+        popperProps: { minWidth: "300px", width: "max-content" },
+      }}
+      items={[
+        <DisksDrillDownMenuItem
+          key="change-disk-option"
+          drive={drive}
+          selected={selected}
+          onDeviceClick={changeDriveTarget}
+        />,
+        <NewVgOption key="add-vg-option" drive={drive} selected={selected} />,
+        <RemoveDriveOption key="delete-disk-option" drive={drive} />,
+      ]}
+    >
+      {<b>{deviceLabel(selected, 20)}</b>}
+    </MenuButton>
   );
 }
