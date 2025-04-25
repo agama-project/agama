bad634b6e850c924f1a4b15d8c952ec326a3dfb9
José Iván López González <jlopez@suse.com>
2025-04-25 09:32:44 +0100

    refactor(storage): reorganize tests

--- service/test/agama/storage/config_conversions/from_json_test.rb
+++ service/test/agama/storage/config_conversions/from_json_test.rb
@@ -22,475 +22,50 @@
 require_relative "../../../test_helper"
 require "agama/storage/config"
 require "agama/storage/config_conversions/from_json"
-require "y2storage/encryption_method"
 
 describe Agama::Storage::ConfigConversions::FromJSON do
   subject do
     described_class.new(config_json, default_paths: default_paths, mandatory_paths: mandatory_paths)
   end
 
-  let(:default_paths) { [] }
+  let(:default_paths) { ["/", "swap"] }
 
-  let(:mandatory_paths) { [] }
+  let(:mandatory_paths) { ["/"] }
 
   describe "#convert" do
-    let(:config_json) { {} }
+    let(:config_json) do
+      {
+        drives:       [
+          {
+            partitions: [
+              { generate: "default" }
+            ]
+          }
+        ],
+        volumeGroups: [
+          {
+            name:           "vg0",
+            logicalVolumes: [
+              { filesystem: { path: "/home" } }
+            ]
+          }
+        ]
+      }
+    end
 
     it "returns a storage config" do
       config = subject.convert
+      boot = config.boot
+      drive = config.drives.first
+      volume_group = config.volume_groups.first
+
       expect(config).to be_a(Agama::Storage::Config)
-    end
-
-    shared_examples "with generate" do |configs_proc|
-      context "with 'default' value" do
-        let(:generate) { "default" }
-
-        let(:default_paths) { ["/default1", "/default2"] }
-
-        it "adds volumes for the default paths" do
-          configs = configs_proc.call(subject.convert)
-
-          default1 = configs.find { |c| c.filesystem.path == "/default1" }
-          expect(default1).to_not be_nil
-          expect(default1.encryption).to be_nil
-
-          default2 = configs.find { |c| c.filesystem.path == "/default2" }
-          expect(default2).to_not be_nil
-          expect(default2.encryption).to be_nil
-        end
-      end
-
-      context "with 'mandatory' value" do
-        let(:generate) { "mandatory" }
-
-        let(:mandatory_paths) { ["/mandatory1"] }
-
-        it "adds volumes for the mandatory paths" do
-          configs = configs_proc.call(subject.convert)
-
-          mandatory1 = configs.find { |c| c.filesystem.path == "/mandatory1" }
-          expect(mandatory1).to_not be_nil
-          expect(mandatory1.encryption).to be_nil
-        end
-      end
-    end
-
-    context "generating partitions" do
-      let(:config_json) do
-        {
-          drives:       drives,
-          volumeGroups: volume_groups
-        }
-      end
-
-      let(:drives) { [] }
-
-      let(:volume_groups) { [] }
-
-      let(:default_paths) { ["/", "swap", "/home"] }
-
-      let(:mandatory_paths) { ["/", "swap"] }
-
-      context "if a partition specifies 'generate'" do
-        let(:drives) do
-          [
-            {
-              partitions: [
-                { generate: generate }
-              ]
-            }
-          ]
-        end
-
-        partitions_proc = proc { |c| c.drives.first.partitions }
-        include_examples "with generate", partitions_proc
-
-        context "with a generate section" do
-          let(:generate) do
-            {
-              partitions: "default",
-              encryption: {
-                luks2: { password: "12345" }
-              }
-            }
-          end
-
-          let(:default_paths) { ["/", "swap"] }
-
-          it "adds the expected partitions" do
-            partitions = partitions_proc.call(subject.convert)
-            expect(partitions.size).to eq(2)
-
-            root_part = partitions.find { |p| p.filesystem.path == "/" }
-            swap_part = partitions.find { |p| p.filesystem.path == "swap" }
-
-            expect(root_part).to_not be_nil
-            expect(root_part.encryption.method).to eq(Y2Storage::EncryptionMethod::LUKS2)
-            expect(root_part.encryption.password).to eq("12345")
-
-            expect(swap_part).to_not be_nil
-            expect(swap_part.encryption.method).to eq(Y2Storage::EncryptionMethod::LUKS2)
-            expect(swap_part.encryption.password).to eq("12345")
-          end
-        end
-      end
-
-      context "if the device already specifies any of the partitions" do
-        let(:drives) do
-          [
-            {
-              partitions: [
-                { generate: "default" },
-                { filesystem: { path: "/home" } }
-              ]
-            }
-          ]
-        end
-
-        it "only adds partitions for the the missing paths" do
-          config = subject.convert
-          partitions = config.drives.first.partitions
-          expect(partitions.size).to eq(3)
-
-          root_part = partitions.find { |p| p.filesystem.path == "/" }
-          swap_part = partitions.find { |p| p.filesystem.path == "swap" }
-          home_part = partitions.find { |p| p.filesystem.path == "/home" }
-          expect(root_part).to_not be_nil
-          expect(swap_part).to_not be_nil
-          expect(home_part).to_not be_nil
-        end
-      end
-
-      context "if other device already specifies any of the partitions" do
-        let(:drives) do
-          [
-            {
-              partitions: [
-                { generate: "default" }
-              ]
-            },
-            {
-              partitions: [
-                { filesystem: { path: "/home" } }
-              ]
-            }
-          ]
-        end
-
-        it "only adds partitions for the the missing paths" do
-          config = subject.convert
-          partitions = config.drives.first.partitions
-          expect(partitions.size).to eq(2)
-
-          root_part = partitions.find { |p| p.filesystem.path == "/" }
-          swap_part = partitions.find { |p| p.filesystem.path == "swap" }
-          expect(root_part).to_not be_nil
-          expect(swap_part).to_not be_nil
-        end
-      end
-
-      context "if a volume group already specifies any of the paths" do
-        let(:drives) do
-          [
-            {
-              partitions: [
-                { generate: "mandatory" }
-              ]
-            }
-          ]
-        end
-
-        let(:volume_groups) do
-          [
-            {
-              logicalVolumes: [
-                { filesystem: { path: "swap" } }
-              ]
-            }
-          ]
-        end
-
-        it "only adds partitions for the the missing paths" do
-          config = subject.convert
-          partitions = config.drives.first.partitions
-          expect(partitions.size).to eq(1)
-
-          root_part = partitions.find { |p| p.filesystem.path == "/" }
-          expect(root_part).to_not be_nil
-        end
-      end
-
-      context "if the device specifies several partitions with 'generate'" do
-        let(:drives) do
-          [
-            {
-              partitions: [
-                { generate: "mandatory" },
-                { generate: "default" }
-              ]
-            }
-          ]
-        end
-
-        it "only adds partitions for the first 'generate'" do
-          config = subject.convert
-          partitions = config.drives.first.partitions
-          expect(partitions.size).to eq(2)
-
-          root_part = partitions.find { |p| p.filesystem.path == "/" }
-          swap_part = partitions.find { |p| p.filesystem.path == "swap" }
-          expect(root_part).to_not be_nil
-          expect(swap_part).to_not be_nil
-        end
-      end
-
-      context "if several devices specify partitions with 'generate'" do
-        let(:drives) do
-          [
-            {
-              partitions: [
-                { generate: "mandatory" }
-              ]
-            },
-            {
-              partitions: [
-                { generate: "default" }
-              ]
-            }
-          ]
-        end
-
-        it "only adds partitions to the first device with a 'generate'" do
-          config = subject.convert
-          drive1, drive2 = config.drives
-          expect(drive1.partitions.size).to eq(2)
-          expect(drive2.partitions.size).to eq(0)
-        end
-      end
-    end
-
-    context "generating logical volumes" do
-      let(:config_json) do
-        {
-          drives:       drives,
-          volumeGroups: volume_groups
-        }
-      end
-
-      let(:drives) { [] }
-
-      let(:volume_groups) { [] }
-
-      let(:default_paths) { ["/", "swap", "/home"] }
-
-      let(:mandatory_paths) { ["/", "swap"] }
-
-      context "if a logical volume specifies 'generate'" do
-        let(:volume_groups) do
-          [
-            {
-              logicalVolumes: [
-                { generate: generate }
-              ]
-            }
-          ]
-        end
-
-        logical_volumes_proc = proc { |c| c.volume_groups.first.logical_volumes }
-        include_examples "with generate", logical_volumes_proc
-
-        context "with a generate section" do
-          let(:generate) do
-            {
-              logicalVolumes: "default",
-              encryption:     {
-                luks2: { password: "12345" }
-              },
-              stripes:        8,
-              stripeSize:     "16 KiB"
-            }
-          end
-
-          let(:default_paths) { ["/", "swap"] }
-
-          it "adds the expected logical volumes" do
-            lvs = logical_volumes_proc.call(subject.convert)
-            expect(lvs.size).to eq(2)
-
-            root_lv = lvs.find { |v| v.filesystem.path == "/" }
-            swap_lv = lvs.find { |v| v.filesystem.path == "swap" }
-
-            expect(root_lv).to_not be_nil
-            expect(root_lv.encryption.method).to eq(Y2Storage::EncryptionMethod::LUKS2)
-            expect(root_lv.encryption.password).to eq("12345")
-
-            expect(swap_lv).to_not be_nil
-            expect(swap_lv.encryption.method).to eq(Y2Storage::EncryptionMethod::LUKS2)
-            expect(swap_lv.encryption.password).to eq("12345")
-          end
-        end
-      end
-
-      context "if the volume group already specifies any of the logical volumes" do
-        let(:volume_groups) do
-          [
-            {
-              logicalVolumes: [
-                { generate: "default" },
-                { filesystem: { path: "/home" } }
-              ]
-            }
-          ]
-        end
-
-        it "only adds logical volumes for the the missing paths" do
-          config = subject.convert
-          lvs = config.volume_groups.first.logical_volumes
-          expect(lvs.size).to eq(3)
-
-          root_lv = lvs.find { |v| v.filesystem.path == "/" }
-          swap_lv = lvs.find { |v| v.filesystem.path == "swap" }
-          home_lv = lvs.find { |v| v.filesystem.path == "/home" }
-          expect(root_lv).to_not be_nil
-          expect(swap_lv).to_not be_nil
-          expect(home_lv).to_not be_nil
-        end
-      end
-
-      context "if other volume group already specifies any of the logical volumes" do
-        let(:volume_groups) do
-          [
-            {
-              logicalVolumes: [
-                { generate: "default" }
-              ]
-            },
-            {
-              logicalVolumes: [
-                { filesystem: { path: "/home" } }
-              ]
-            }
-          ]
-        end
-
-        it "only adds logical volumes for the the missing paths" do
-          config = subject.convert
-          lvs = config.volume_groups.first.logical_volumes
-          expect(lvs.size).to eq(2)
-
-          root_lv = lvs.find { |v| v.filesystem.path == "/" }
-          swap_lv = lvs.find { |v| v.filesystem.path == "swap" }
-          expect(root_lv).to_not be_nil
-          expect(swap_lv).to_not be_nil
-        end
-      end
-
-      context "if a device already specifies a partition for any of the paths" do
-        let(:drives) do
-          [
-            {
-              partitions: [
-                { filesystem: { path: "swap" } }
-              ]
-            }
-          ]
-        end
-
-        let(:volume_groups) do
-          [
-            {
-              logicalVolumes: [
-                { generate: "mandatory" }
-              ]
-            }
-          ]
-        end
-
-        it "only adds logical volumes for the the missing paths" do
-          config = subject.convert
-          lvs = config.volume_groups.first.logical_volumes
-          expect(lvs.size).to eq(1)
-
-          root_lv = lvs.find { |v| v.filesystem.path == "/" }
-          expect(root_lv).to_not be_nil
-        end
-      end
-
-      context "if the volume group specifies several logical volumes with 'generate'" do
-        let(:volume_groups) do
-          [
-            {
-              logicalVolumes: [
-                { generate: "mandatory" },
-                { generate: "default" }
-              ]
-            }
-          ]
-        end
-
-        it "only adds logical volumes for the first 'generate'" do
-          config = subject.convert
-          lvs = config.volume_groups.first.logical_volumes
-          expect(lvs.size).to eq(2)
-
-          root_lv = lvs.find { |v| v.filesystem.path == "/" }
-          swap_lv = lvs.find { |v| v.filesystem.path == "swap" }
-          expect(root_lv).to_not be_nil
-          expect(swap_lv).to_not be_nil
-        end
-      end
-
-      context "if several volume groups specify logical volumes with 'generate'" do
-        let(:volume_groups) do
-          [
-            {
-              logicalVolumes: [
-                { generate: "mandatory" }
-              ]
-            },
-            {
-              logicalVolumes: [
-                { generate: "default" }
-              ]
-            }
-          ]
-        end
-
-        it "only adds logical volumes to the first volume group with a 'generate'" do
-          config = subject.convert
-          vg1, vg2 = config.volume_groups
-          expect(vg1.logical_volumes.size).to eq(2)
-          expect(vg2.logical_volumes.size).to eq(0)
-        end
-      end
-
-      context "if a drive specifies a partition with 'generate'" do
-        let(:drives) do
-          [
-            {
-              partitions: [
-                { generate: "mandatory" }
-              ]
-            }
-          ]
-        end
-
-        let(:volume_groups) do
-          [
-            {
-              logicalVolumes: [
-                { generate: "mandatory" }
-              ]
-            }
-          ]
-        end
-
-        it "does not add logical volumes to the volume group" do
-          config = subject.convert
-          vg = config.volume_groups.first
-          expect(vg.logical_volumes.size).to eq(0)
-        end
-      end
+      expect(boot.configure).to eq(true)
+      expect(boot.device.default).to eq(true)
+      expect(boot.device.device_alias).to be_nil
+      expect(drive.partitions.map { |p| p.filesystem.path }).to contain_exactly("/", "swap")
+      expect(volume_group.name).to eq("vg0")
+      expect(volume_group.logical_volumes.map { |l| l.filesystem.path }).to contain_exactly("/home")
     end
   end
 end
---
+++ service/test/agama/storage/config_json_solver_test.rb
@@ -0,0 +1,302 @@
+# frozen_string_literal: true
+
+# Copyright (c) [2025] SUSE LLC
+#
+# All Rights Reserved.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of version 2 of the GNU General Public License as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, contact SUSE LLC.
+#
+# To contact SUSE LLC about this file by physical or electronic mail, you may
+# find current contact information at www.suse.com.
+
+require_relative "../../test_helper"
+require "agama/storage/config_json_solver"
+
+describe Agama::Storage::ConfigJSONSolver do
+  subject do
+    described_class.new(default_paths: default_paths, mandatory_paths: mandatory_paths)
+  end
+
+  let(:default_paths) { [] }
+  let(:mandatory_paths) { [] }
+
+  describe "#solve" do
+    shared_examples "generate" do |devices_key, volumes_key|
+      let(:default_paths) { ["/", "swap", "/home"] }
+      let(:mandatory_paths) { ["/", "swap"] }
+
+      let(:config_json) do
+        { devices_key => devices }
+      end
+
+      context "if '#{volumes_key}' contains a config with 'generate'" do
+        let(:devices) do
+          [
+            {
+              volumes_key => [
+                { generate: generate }
+              ]
+            }
+          ]
+        end
+
+        context "with 'default' value" do
+          let(:generate) { "default" }
+
+          it "adds '#{volumes_key}' for the default paths" do
+            subject.solve(config_json)
+            volumes_json = config_json[devices_key][0][volumes_key]
+
+            expect(volumes_json).to contain_exactly(
+              { filesystem: { path: "/" } },
+              { filesystem: { path: "swap" } },
+              { filesystem: { path: "/home" } }
+            )
+          end
+        end
+
+        context "with 'mandatory' value" do
+          let(:generate) { "mandatory" }
+
+          it "adds '#{volumes_key}' for the mandatory paths" do
+            subject.solve(config_json)
+            volumes_json = config_json[devices_key][0][volumes_key]
+
+            expect(volumes_json).to contain_exactly(
+              { filesystem: { path: "/" } },
+              { filesystem: { path: "swap" } }
+            )
+          end
+        end
+
+        context "with a 'generate' section" do
+          let(:generate) do
+            {
+              volumes_key => "mandatory",
+              encryption: {
+                luks2: { password: "12345" }
+              }
+            }
+          end
+
+          it "adds '#{volumes_key}' with the specified properties" do
+            subject.solve(config_json)
+            volumes_json = config_json[devices_key][0][volumes_key]
+
+            expect(volumes_json).to contain_exactly(
+              {
+                filesystem: { path: "/" },
+                encryption: {
+                  luks2: { password: "12345" }
+                }
+              },
+              {
+                filesystem: { path: "swap" },
+                encryption: {
+                  luks2: { password: "12345" }
+                }
+              }
+            )
+          end
+        end
+      end
+
+      context "if '#{volumes_key}' contains several configs with 'generate'" do
+        let(:devices) do
+          [
+            {
+              volumes_key => [
+                { generate: "mandatory" },
+                { generate: "default" }
+              ]
+            }
+          ]
+        end
+
+        it "only adds '#{volumes_key}' for the first 'generate'" do
+          subject.solve(config_json)
+          volumes_json = config_json[devices_key][0][volumes_key]
+
+          expect(volumes_json).to contain_exactly(
+            { filesystem: { path: "/" } },
+            { filesystem: { path: "swap" } }
+          )
+        end
+      end
+
+      context "if several '#{devices_key}' contain '#{volumes_key}' with 'generate'" do
+        let(:devices) do
+          [
+            {
+              volumes_key => [
+                { generate: "mandatory" }
+              ]
+            },
+            {
+              volumes_key => [
+                { generate: "default" }
+              ]
+            }
+          ]
+        end
+
+        it "only adds '#{volumes_key}' to the first '#{devices_key}' with a 'generate'" do
+          subject.solve(config_json)
+          devices_json = config_json[devices_key]
+
+          expect(devices_json).to contain_exactly(
+            {
+              volumes_key => [
+                { filesystem: { path: "/" } },
+                { filesystem: { path: "swap" } }
+              ]
+            },
+            {
+              volumes_key => []
+            }
+          )
+        end
+      end
+
+      context "if '#{volumes_key}' already contains a config for any of the paths" do
+        let(:devices) do
+          [
+            {
+              volumes_key => [
+                { generate: "default" },
+                { filesystem: { path: "/home" } }
+              ]
+            }
+          ]
+        end
+
+        it "only adds '#{volumes_key}' for the the missing paths" do
+          subject.solve(config_json)
+          volumes_json = config_json[devices_key][0][volumes_key]
+
+          expect(volumes_json).to contain_exactly(
+            { filesystem: { path: "/" } },
+            { filesystem: { path: "swap" } },
+            { filesystem: { path: "/home" } }
+          )
+        end
+      end
+
+      context "if other '#{devices_key}' config already contains any of the paths" do
+        let(:devices) do
+          [
+            {
+              volumes_key => [
+                { generate: "default" }
+              ]
+            },
+            {
+              volumes_key => [
+                { filesystem: { path: "/home" } }
+              ]
+            }
+          ]
+        end
+
+        it "only adds '#{volumes_key}' for the the missing paths" do
+          subject.solve(config_json)
+          devices_json = config_json[devices_key]
+
+          expect(devices_json).to contain_exactly(
+            {
+              volumes_key => [
+                { filesystem: { path: "/" } },
+                { filesystem: { path: "swap" } }
+              ]
+            },
+            {
+              volumes_key => [
+                { filesystem: { path: "/home" } }
+              ]
+            }
+          )
+        end
+      end
+    end
+
+    context "if a drive generates partitions" do
+      include_examples "generate", :drives, :partitions
+
+      context "if a 'volumeGroups' config already specifies any of the paths" do
+        let(:default_paths) { ["/", "swap", "/home"] }
+        let(:mandatory_paths) { ["/", "swap"] }
+
+        let(:config_json) do
+          {
+            drives:       [
+              {
+                partitions: [
+                  { generate: "default" }
+                ]
+              }
+            ],
+            volumeGroups: [
+              {
+                logicalVolumes: [
+                  { filesystem: { path: "swap" } }
+                ]
+              }
+            ]
+          }
+        end
+
+        it "only adds 'partitions' for the the missing paths" do
+          subject.solve(config_json)
+          partitions_json = config_json[:drives][0][:partitions]
+
+          expect(partitions_json).to contain_exactly(
+            { filesystem: { path: "/" } },
+            { filesystem: { path: "/home" } }
+          )
+        end
+      end
+    end
+
+    context "if a volume group generates logical volumes" do
+      include_examples "generate", :volumeGroups, :logicalVolumes
+
+      context "if a 'drives' config specifies a partition with 'generate'" do
+        let(:config_json) do
+          {
+            drives:       [
+              {
+                partitions: [
+                  { generate: "mandatory" }
+                ]
+              }
+            ],
+            volumeGroups: [
+              {
+                logicalVolumes: [
+                  { generate: "mandatory" }
+                ]
+              }
+            ]
+          }
+        end
+
+        it "does not add logical volumes to the volume group" do
+          subject.solve(config_json)
+          logical_volumes_json = config_json[:volumeGroups][0][:logicalVolumes]
+
+          expect(logical_volumes_json).to eq([])
+        end
+      end
+    end
+  end
+end
