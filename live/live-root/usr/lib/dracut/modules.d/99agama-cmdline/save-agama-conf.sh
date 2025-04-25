cfe514d4858dad5fc0f1fce0bdea918eb7e22542
Knut Anderssen <kanderssen@suse.com>
2025-04-25 10:47:54 +0100

    Copy only explicit dracut NetworkManger ip= config

--- live/live-root/usr/lib/dracut/modules.d/99agama-cmdline/save-agama-conf.sh
+++ live/live-root/usr/lib/dracut/modules.d/99agama-cmdline/save-agama-conf.sh
@@ -7,3 +7,15 @@
 if [ -e /etc/hostname ]; then
   cp /etc/hostname "$NEWROOT/etc/hostname"
 fi
+
+if [ -e /run/agama/copy_network ]; then
+  if getargbool 1 inst.copy_network; then
+    mkdir -p /run/NetworkManager/conf.d
+    echo '[main]' >/run/NetworkManager/conf.d/00-agama-server.conf
+    echo 'no-auto-default=*' >>/run/NetworkManager/conf.d/00-agama-server.conf
+    echo 'ignore-carrier=*' >>/run/NetworkManager/conf.d/00-agama-server.conf
+
+    mkdir -p "$NEWROOT/etc/NetworkManager/system-connections/"
+    cp /run/NetworkManager/system-connections/* "$NEWROOT/etc/NetworkManager/system-connections/"
+  fi
+fi
