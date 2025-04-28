4f821d7d85d6142bca7aef81d7936017552da4ce
Knut Anderssen <kanderssen@suse.com>
2025-04-28 13:02:10 +0100

    Modify copy_network flag to not_copy_network

--- live/live-root/usr/lib/dracut/modules.d/99agama-cmdline/save-agama-conf.sh
+++ live/live-root/usr/lib/dracut/modules.d/99agama-cmdline/save-agama-conf.sh
@@ -9,7 +9,6 @@ if [ -e /etc/hostname ]; then
 fi
 
 if getargbool 1 inst.copy_network; then
-  : >/run/agama/copy_network
   # If there is some explicit network configuration provided through the ip= kernel cmdline option
   # then we set the config server configuration diabling the DHCP auto configuration for ethernet
   # devices and also copying the configuration persistently (bsc#1241224, bsc#122486, bsc#1239777,
@@ -23,4 +22,6 @@ if getargbool 1 inst.copy_network; then
     mkdir -p "$NEWROOT/etc/NetworkManager/system-connections/"
     cp /run/NetworkManager/system-connections/* "$NEWROOT/etc/NetworkManager/system-connections/"
   fi
+else
+  : >/run/agama/not_copy_network
 fi
