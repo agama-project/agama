e333d1b4b22d02569b20f0ea3b740008b800d5dd
Imobach González Sosa <igonzalezsosa@suse.com>
2025-05-14 06:49:17 +0100

    refactor(rust): remove outdated documentation

--- rust/agama-network/src/lib.rs
+++ rust/agama-network/src/lib.rs
@@ -18,6 +18,10 @@
 // To contact SUSE LLC about this file by physical or electronic mail, you may
 // find current contact information at www.suse.com.
 
+//! Network configuration service for Agama
+//!
+//! This library implements the network configuration service for Agama.
+
 pub mod action;
 pub mod adapter;
 pub mod error;
