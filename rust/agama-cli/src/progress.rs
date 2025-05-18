48516d71f68838eb04be37d96a5e40979c3bde3a
Imobach González Sosa <igonzalezsosa@suse.com>
2025-05-18 17:06:25 +0100

    feat(rust): add a struct to show the monitor progress

--- rust/agama-cli/src/progress.rs
+++ rust/agama-cli/src/progress.rs
@@ -1,4 +1,4 @@
-// Copyright (c) [2024] SUSE LLC
+// Copyright (c) [2024-2025] SUSE LLC
 //
 // All Rights Reserved.
 //
@@ -18,11 +18,17 @@
 // To contact SUSE LLC about this file by physical or electronic mail, you may
 // find current contact information at www.suse.com.
 
-use agama_lib::progress::{Progress, ProgressPresenter};
+use agama_lib::{
+    monitor::{MonitorClient, MonitorStatus},
+    progress::{Progress, ProgressPresenter},
+};
 use async_trait::async_trait;
 use console::style;
 use indicatif::{ProgressBar, ProgressStyle};
 use std::time::Duration;
+
+const MANAGER_SERVICE: &str = "org.opensuse.Agama.Manager1";
+const SOFTWARE_SERVICE: &str = "org.opensuse.Agama.Software1";
 
 /// Reports the installer progress through the terminal
 pub struct InstallerProgress {
@@ -78,6 +84,108 @@ impl ProgressPresenter for InstallerProgress {
     async fn finish(&mut self) {
         if let Some(bar) = self.bar.take() {
             bar.finish_and_clear();
+        }
+    }
+}
+
+/// Represents the progress of the Agama service.
+pub struct MonitorProgress {
+    monitor: MonitorClient,
+    bar: Option<ProgressBar>,
+    current_step: u32,
+    running: bool,
+    stop_on_idle: bool,
+}
+
+impl MonitorProgress {
+    /// Builds a new instance.
+    ///
+    /// * `MonitorClient`: client to access the Agama monitor.
+    pub fn new(monitor: MonitorClient) -> Self {
+        Self {
+            monitor,
+            bar: None,
+            current_step: 0,
+            running: false,
+            stop_on_idle: true,
+        }
+    }
+
+    /// Determines whether the progress should stop when the service becomes idle.
+    pub fn stop_on_idle(mut self, stop_on_idle: bool) -> Self {
+        self.stop_on_idle = stop_on_idle;
+        self
+    }
+
+    /// Starts the UI representing the progress.
+    pub async fn run(&mut self) {
+        let mut updates = self.monitor.subscribe();
+        let status = self.monitor.get_status().await.unwrap();
+        self.update(status).await;
+
+        loop {
+            if let Ok(status) = updates.recv().await {
+                if !self.update(status).await {
+                    return;
+                }
+            }
+        }
+    }
+
+    /// Updates the progress.
+    ///
+    /// It returns true if the monitor should continue.
+    async fn update(&mut self, status: MonitorStatus) -> bool {
+        if status.progress.get(MANAGER_SERVICE).is_none() && self.running {
+            self.finish();
+            if self.stop_on_idle {
+                return false;
+            }
+        }
+
+        if let Some(progress) = status.progress.get(MANAGER_SERVICE) {
+            self.running = true;
+            if self.current_step != progress.current_step {
+                self.update_main(&progress).await;
+                self.current_step = progress.current_step;
+            }
+        }
+
+        if let Some(progress) = status.progress.get(SOFTWARE_SERVICE) {
+            self.update_bar(&progress);
+        }
+
+        true
+    }
+
+    /// Updates the main bar.
+    async fn update_main(&mut self, progress: &Progress) {
+        let counter = format!("[{}/{}]", &progress.current_step, &progress.max_steps);
+
+        println!(
+            "{} {}",
+            style(&counter).bold().green(),
+            &progress.current_title
+        );
+    }
+
+    fn update_bar(&mut self, progress: &Progress) {
+        let bar = self.bar.get_or_insert_with(|| {
+            let style = ProgressStyle::with_template("{spinner:.green} {msg}").unwrap();
+            let bar = ProgressBar::new(0).with_style(style);
+            bar.enable_steady_tick(Duration::from_millis(120));
+            bar
+        });
+        bar.set_length(progress.max_steps.into());
+        bar.set_position(progress.current_step.into());
+        bar.set_message(progress.current_title.to_owned());
+    }
+
+    /// Stops the representation.
+    fn finish(&mut self) {
+        self.running = false;
+        if let Some(bar) = self.bar.take() {
+            bar.finish_with_message("Done");
         }
     }
 }
