da6f82efff7aa3f7046c6cb32980305b6b154b27
Imobach González Sosa <igonzalezsosa@suse.com>
2025-05-19 13:13:10 +0100

    refactor(rust): drop unused monitoring code

--- rust/agama-cli/src/progress.rs
+++ rust/agama-cli/src/progress.rs
@@ -20,73 +20,14 @@
 
 use agama_lib::{
     monitor::{MonitorClient, MonitorStatus},
-    progress::{Progress, ProgressPresenter},
+    progress::Progress,
 };
-use async_trait::async_trait;
 use console::style;
 use indicatif::{ProgressBar, ProgressStyle};
 use std::time::Duration;
 
 const MANAGER_SERVICE: &str = "org.opensuse.Agama.Manager1";
 const SOFTWARE_SERVICE: &str = "org.opensuse.Agama.Software1";
-
-/// Reports the installer progress through the terminal
-pub struct InstallerProgress {
-    bar: Option<ProgressBar>,
-}
-
-impl InstallerProgress {
-    pub fn new() -> Self {
-        Self { bar: None }
-    }
-
-    fn update_bar(&mut self, progress: &Progress) {
-        let bar = self.bar.get_or_insert_with(|| {
-            let style = ProgressStyle::with_template("{spinner:.green} {msg}").unwrap();
-            let bar = ProgressBar::new(0).with_style(style);
-            bar.enable_steady_tick(Duration::from_millis(120));
-            bar
-        });
-        bar.set_length(progress.max_steps.into());
-        bar.set_position(progress.current_step.into());
-        bar.set_message(progress.current_title.to_owned());
-    }
-}
-
-#[async_trait]
-impl ProgressPresenter for InstallerProgress {
-    async fn start(&mut self, progress: &Progress) {
-        if !progress.finished {
-            self.update_main(progress).await;
-        }
-    }
-
-    async fn update_main(&mut self, progress: &Progress) {
-        let counter = format!("[{}/{}]", &progress.current_step, &progress.max_steps);
-
-        println!(
-            "{} {}",
-            style(&counter).bold().green(),
-            &progress.current_title
-        );
-    }
-
-    async fn update_detail(&mut self, progress: &Progress) {
-        if progress.finished {
-            if let Some(bar) = self.bar.take() {
-                bar.finish_and_clear();
-            }
-        } else {
-            self.update_bar(progress);
-        }
-    }
-
-    async fn finish(&mut self) {
-        if let Some(bar) = self.bar.take() {
-            bar.finish_and_clear();
-        }
-    }
-}
 
 /// Displays the progress on the terminal.
 pub struct ProgressMonitor {
