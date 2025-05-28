7368f8947e063197581e4997f3d588141d51f821
José Iván López González <jlopez@suse.com>
2025-05-28 14:34:25 +0100

    storage: improve test contexts

--- service/test/agama/storage/product_config_context.rb
+++ service/test/agama/storage/product_config_context.rb
@@ -19,6 +19,7 @@
 # To contact SUSE LLC about this file by physical or electronic mail, you may
 # find current contact information at www.suse.com.
 
+require_relative "../../test_helper"
 require "agama/config"
 
 shared_context "product config" do
