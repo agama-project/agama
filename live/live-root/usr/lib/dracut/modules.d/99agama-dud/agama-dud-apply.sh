48e776664b9a9daff5a26629415cd928d748c50d
Imobach González Sosa <igonzalezsosa@suse.com>
2025-06-12 14:31:10 +0100

    feat(live): add support for RPM-based updates

--- live/live-root/usr/lib/dracut/modules.d/99agama-dud/agama-dud-apply.sh
+++ live/live-root/usr/lib/dracut/modules.d/99agama-dud/agama-dud-apply.sh
@@ -5,67 +5,113 @@
 . /lib/dracut-lib.sh
 . /lib/img-lib.sh
 
-DUD_DIR="/run/agama/dud"
+DUD_DIR="$NEWROOT/run/agama/dud"
 AGAMA_CLI="$NEWROOT/usr/bin/agama"
 AGAMA_DUD_INFO="/tmp/agamadud.info"
 DUD_RPM_REPOSITORY="$NEWROOT/var/lib/agama/dud/repo"
 
+shopt -s nullglob
+
 # Apply all the updates.
 #
-# Read the URL of the updates from $AGAMA_DUD_INFO and process each one:
-#
-#   1. Download and unpack the update in $DUD_DIR.
-#   2. Copy the inst-sys updates to the $NEWROOT system.
-#   3. Update agamactl, agama-autoyast and agama-proxy-setup alternative links.
-#   4. Copy the packages to the $DUD_RPM_REPOSITORY.
+# Read the URL of the updates from $AGAMA_DUD_INFO and process each one.
 apply_updates() {
   local file
   local dud_url
+  local dud_root
   index=0
 
   while read -r dud_url; do
     mkdir -p "$DUD_DIR"
-    file=${dud_url##*/}
+    filename=${dud_url##*/}
+    file="${DUD_DIR}/${filename}"
     # FIXME: use an index because two updates, coming from different places, can have the same name.
-    echo "Fetching a Driver Update Disk from $dud_url to $file"
-    if ! $AGAMA_CLI download "$dud_url" "$DUD_DIR/$file"; then
+    echo "Fetching a Driver Update Disk from $dud_url to ${file}"
+    if ! $AGAMA_CLI download "$dud_url" "${file}"; then
       warn "Failed to fetch the Driver Update Disk"
       exit 1
     fi
 
+    # The $dir could be a temporary one created by each function.
     dir="$DUD_DIR/dud_$(printf "%03d" $index)"
     mkdir -p "$dir"
-    echo "Unpacking ${file} to ${dir}"
-    unpack_img "$DUD_DIR/$file" "$dir"
+
+    format=$("$NEWROOT/usr/bin/chroot" "$NEWROOT" file "${file##"$NEWROOT"}")
+    case "$format" in
+    *RPM*)
+      apply_rpm_update "$file" "$dir"
+      ;;
+
+    *)
+      apply_dud_update "$file" "$dir"
+      ;;
+    esac
+
     ((index++))
-
-    # FIXME: do not ignore the dist (e.g., "tw" in "x86_64-tw").
-    arch=$(uname -m)
-    dud_root=$(echo "${dir}/linux/suse/${arch}"-*)
-    echo "Detected DUD root at ${dud_root}"
-
-    apply_update "$dud_root"
-    copy_packages "$dud_root" "$DUD_RPM_REPOSITORY"
   done <$AGAMA_DUD_INFO
 
   create_repo "$DUD_RPM_REPOSITORY"
 }
 
-# Apply an update to the installation system.
+# Applies an update from an RPM package
 #
-# inst-sys updates are applied by copying files instead of installing packages. For that reason,
-# it might be needed to do some adjustments "manually", like settings the alternative links.
-apply_update() {
+# It extracts the RPM content and adjust the alternative links.
+apply_rpm_update() {
+  file=$1
+  dir=$2
+
+  echo "Apply update from an RPM package"
+  unpack_rpm "$file" "$dir"
+  install_update "$dir"
+}
+
+# Applies an update from an RPM package
+#
+#   1. Copy the inst-sys updates to the $NEWROOT system.
+#   2. Update agamactl, agama-autoyast and agama-proxy-setup alternative links.
+#   3. Copy the packages to the $DUD_RPM_REPOSITORY.
+apply_dud_update() {
+  file=$1
+  dir=$2
+
+  echo "Apply update from a Driver Update Disk archive"
+  unpack_img "$file" "$dir"
+  # FIXME: do not ignore the dist (e.g., "tw" in "x86_64-tw").
+  arch=$(uname -m)
+  dud_root=$(echo "${dir}/linux/suse/${arch}"-*)
+  install_update "${dud_root}/inst-sys"
+  copy_packages "$dud_root" "$DUD_RPM_REPOSITORY"
+}
+
+# Extracts an RPM file
+#
+# It uses rpm2cpio from the $NEWROOT.
+unpack_rpm() {
+  source=$1
+  dest=$2
+  echo "Unpacking RPM ${source} to ${dest}"
+
+  mkdir -p "$dest"
+  pushd "$dest" || exit 1
+  "$NEWROOT/usr/bin/chroot" "$NEWROOT" /usr/bin/rpm2cpio "${source##"$NEWROOT"}" |
+    cpio --extract --make-directories --preserve-modification-time
+  popd || exit 1
+}
+
+# Applies an update to the installation system.
+#
+# Updates are applied by copying files instead of installing packages. For that
+# reason, it might be needed to do some adjustments "manually", like settings
+# the alternative links.
+install_update() {
   dud_dir=$1
   echo "Apply inst-sys update from ${dud_dir}"
 
-  cp -a "${dud_dir}/inst-sys/"* $NEWROOT
+  cp -a "${dud_dir}"/* "$NEWROOT"
 
-  dud_instsys="${dud_dir}/inst-sys"
-
-  set_alternative "$dud_instsys" "agama-autoyast"
-  set_alternative "$dud_instsys" "agamactl"
-  set_alternative "$dud_instsys" "agama-proxy-setup"
+  set_alternative "$dud_dir" "agama-autoyast"
+  set_alternative "$dud_dir" "agamactl"
+  set_alternative "$dud_dir" "agama-proxy-setup"
 }
 
 # Sets the alternative links.
@@ -75,11 +121,13 @@ set_alternative() {
 
   executables=("$dud_instsys/usr/bin/${name}.ruby"*-*)
   executable=${executables[0]}
-  $NEWROOT/usr/bin/chroot $NEWROOT /usr/sbin/update-alternatives --install "/usr/bin/$name" "$name" "${executable##"$dud_instsys"}" 150000
-  $NEWROOT/usr/bin/chroot $NEWROOT /usr/sbin/update-alternatives --set "$name" "${executable##"$dud_instsys"}"
+  if [ ! -z "$executable" ]; then
+    "$NEWROOT/usr/bin/chroot" "$NEWROOT" /usr/sbin/update-alternatives --install "/usr/bin/$name" "$name" "${executable##"$dud_instsys"}" 150000
+    "$NEWROOT/usr/bin/chroot" "$NEWROOT" /usr/sbin/update-alternatives --set "$name" "${executable##"$dud_instsys"}"
+  fi
 }
 
-# Copy the packages to use during installation
+# Copies the packages to use during installation
 #
 # This function is mainly a PoC.
 #
@@ -101,7 +149,9 @@ copy_packages() {
 create_repo() {
   repo_dir=$1
 
-  $NEWROOT/usr/bin/chroot $NEWROOT createrepo_c "${repo_dir##"$NEWROOT"}"
+  "$NEWROOT/usr/bin/chroot" "$NEWROOT" createrepo_c "${repo_dir##"$NEWROOT"}"
 }
 
-apply_updates
+if [ -f "$AGAMA_DUD_INFO" ]; then
+  apply_updates
+fi
