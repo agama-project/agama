78b81e01099a8483ddb5e08bed67c85775b237cf
Knut Anderssen <kanderssen@suse.com>
2025-06-19 17:20:48 +0100

    feat(web): add wrapper component around PF/Radio
    To avoid code repetition with styles and possibly other stuff in the
    future. The same look&feel is already used in two other places.

---
+++ web/src/components/core/RadioEnhanced.tsx
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) [2025] SUSE LLC
+ *
+ * All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, contact SUSE LLC.
+ *
+ * To contact SUSE LLC about this file by physical or electronic mail, you may
+ * find current contact information at www.suse.com.
+ */
+
+import React from "react";
+import { Content, Radio, RadioProps } from "@patternfly/react-core";
+import textStyles from "@patternfly/react-styles/css/utilities/Text/text";
+
+type RadioEnhancedProps = Omit<RadioProps, "ref" | "label"> & {
+  /** Must describe the isChecked="true" state. */
+  label: React.ReactNode;
+};
+
+export default function RadioEnhanced({ label, isChecked, ...props }: RadioEnhancedProps) {
+  return (
+    <Radio
+      label={
+        <Content
+          className={[textStyles.fontSizeLg, isChecked && textStyles.fontWeightBold].join(" ")}
+        >
+          {label}
+        </Content>
+      }
+      isChecked={isChecked}
+      {...props}
+    />
+  );
+}
