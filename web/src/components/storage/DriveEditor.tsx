3f862740edad3bf81d3bcd413797435bf5ccc63b
David Díaz González <dgonzalez@suse.com>
2025-06-26 16:21:02 +0100

    fix(web): move modal selector mounting out of DriveEditor
    Although the selector somehow belongs to the DriveEditor container
    component, it should be mounted deeper in the tree, closer to the
    core/MenuButton that actually triggers it.
    
    This change may help avoid unnecessary re-renders from the top-level
    component.

--- web/src/components/storage/DriveEditor.tsx
+++ web/src/components/storage/DriveEditor.tsx
@@ -20,7 +20,7 @@
  * find current contact information at www.suse.com.
  */
 
-import React, { useState } from "react";
+import React from "react";
 import { Drive } from "~/types/storage/model";
 import { StorageDevice } from "~/types/storage";
 import DriveDeviceMenu from "~/components/storage/DriveDeviceMenu";
@@ -28,12 +28,7 @@ import PartitionableHeader from "~/components/storage/PartitionableHeader";
 import PartitionsMenu from "~/components/storage/PartitionsMenu";
 import SpacePolicyMenu from "~/components/storage/SpacePolicyMenu";
 import { Card, CardBody, CardHeader, CardTitle, Flex } from "@patternfly/react-core";
-
 import spacingStyles from "@patternfly/react-styles/css/utilities/Spacing/spacing";
-import { _ } from "~/i18n";
-import DeviceSelectorModal from "./DeviceSelectorModal";
-import { useDrive } from "~/queries/storage/config-model";
-import { useAvailableDevices } from "~/queries/storage";
 
 export type DriveEditorProps = { drive: Drive; driveDevice: StorageDevice };
 
@@ -46,26 +41,11 @@ const DriveHeader = ({ drive, driveDevice }: DriveEditorProps) => {
 };
 
 export default function DriveEditor({ drive, driveDevice }: DriveEditorProps) {
-  // FIXME: device selectors could handle fetching available devices themselves
-  // when no device list is passed via prop. This would avoid having to use
-  // `useAvailableDevices` separately for each configured drive in the Config Editor.
-  const devices = useAvailableDevices();
-  const [diskSelectorOpen, setDiskSelectorOpen] = useState(false);
-  const driveHandler = useDrive(drive.name);
-  const openSelector = () => setDiskSelectorOpen(true);
-  const closeSelector = () => setDiskSelectorOpen(false);
-  const onDriveChange = ([drive]) => {
-    driveHandler.switch(drive.name);
-    closeSelector();
-  };
-
-  // FIXME: stop using hardcoded title for modal selector
   return (
     <Card isCompact>
       <CardHeader>
         <CardTitle>
           <DriveHeader drive={drive} driveDevice={driveDevice} />{" "}
-          <button onClick={openSelector}>{_("Change")}</button>
         </CardTitle>
       </CardHeader>
       <CardBody className={spacingStyles.plLg}>
@@ -73,15 +53,6 @@ export default function DriveEditor({ drive, driveDevice }: DriveEditorProps) {
           <SpacePolicyMenu modelDevice={drive} device={driveDevice} />
           <PartitionsMenu device={drive} />
         </Flex>
-        {diskSelectorOpen && (
-          <DeviceSelectorModal
-            devices={devices}
-            title={_("Select a device to install and boot")}
-            selected={driveDevice}
-            onCancel={closeSelector}
-            onAccept={onDriveChange}
-          />
-        )}
       </CardBody>
     </Card>
   );
