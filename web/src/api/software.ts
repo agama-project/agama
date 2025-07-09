fdd1e4c528c3f7aeba36c78345a53a457ad77a61
José Iván López González <jlopez@suse.com>
2025-07-09 15:59:35 +0100

    web: extend hooks to allow configuring the registration url

--- web/src/api/software.ts
+++ web/src/api/software.ts
@@ -112,6 +112,12 @@ const register = ({ key, email }: { key: string; email?: string }) =>
   post("/api/software/registration", { key, email });
 
 /**
+ * Updates the URL for the registration
+ */
+const updateRegistrationUrl = (url: string) =>
+  put("/api/software/registration/url", url, { headers: { "Content-Type": "application/json" } });
+
+/**
  * Request registration of the selected addon
  */
 const registerAddon = (addon: RegisteredAddonInfo) =>
@@ -136,6 +142,7 @@ export {
   fetchRepositories,
   probe,
   register,
+  updateRegistrationUrl,
   registerAddon,
   solveConflict,
   updateConfig,
