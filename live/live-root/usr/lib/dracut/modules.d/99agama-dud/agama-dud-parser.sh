041851f173d9ec10a88ac8979150069170332f1c
Ladislav Slezák <lslezak@suse.com>
2025-07-09 22:35:16 +0200

    Configure network automatically when a remote DUD is used

--- live/live-root/usr/lib/dracut/modules.d/99agama-dud/agama-dud-parser.sh
+++ live/live-root/usr/lib/dracut/modules.d/99agama-dud/agama-dud-parser.sh
@@ -10,6 +10,16 @@ fetch_updates() {
   updates=$(getargs inst.dud=)
   if [ -n "$updates" ]; then
     echo "$updates" >/tmp/agamadud.info
+
+    # automatically configure network when a remote DUD is used,
+    # check for all remote URL scheme supported by the "agama download" command
+    if grep -q -s "https\?://" /tmp/agamadud.info; then
+      echo "rd.neednet=1" > /etc/cmdline.d/agama-generated.conf
+      # use DHCP if there is no network configuration provided by user
+      if ! getarg "ip="; then
+        echo "ip=dhcp" >> /etc/cmdline.d/agama-generated.conf
+      fi
+    fi
   fi
 }
 
