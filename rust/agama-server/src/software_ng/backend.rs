3c9763ff7e4903cdc687a01e78f141d3062eb18d
Josef Reidinger <jreidinger@suse.cz>
2025-09-11 16:45:50 +0200

    remove doctest example for private module

--- rust/agama-server/src/software_ng/backend.rs
+++ rust/agama-server/src/software_ng/backend.rs
@@ -80,18 +80,6 @@ pub enum SoftwareServiceError {
 
 /// Builds and starts the software service.
 ///
-/// ```no_run
-/// # use tokio_test;
-/// use agama_server::{
-///   software::backend::SoftwareService
-/// };
-///
-/// # tokio_test::block_on(async {
-/// let client = SoftwareService::start(products, http, events_tx).await;
-///
-/// let products = client.get_products().await
-///   .expect("Failed to get the products");
-/// # });
 pub struct SoftwareService {}
 
 impl SoftwareService {
