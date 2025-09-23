6ffe691648b36dc730f9d40426581a6137fac33a
David Díaz González <dgonzalez@suse.com>
2025-09-23 09:17:42 +0100

    fix(web): add missing file

---
+++ web/src/components/storage/FindSpaceSection.tsx
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) [2024-2025] SUSE LLC
+ *
+ * All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, contact SUSE LLC.
+ *
+ * To contact SUSE LLC about this file by physical or electronic mail, you may
+ * find current contact information at www.suse.com.
+ */
+
+import React from "react";
+import { ExpandableSection, Flex, FlexItem } from "@patternfly/react-core";
+import Text from "~/components/core/Text";
+import { _ } from "~/i18n";
+import { NestedContent } from "../core";
+import SpacePolicyMenu from "./SpacePolicyMenu";
+
+export default function FindSpaceSection({ modelDevice, device }) {
+  return (
+    <>
+      <ExpandableSection toggleContent={<Text isBold>{_("Deleting everything")}</Text>}>
+        <NestedContent margin="mxXl">
+          <Flex>
+            {_("ANY existing partition will be removed and DATA IN THE DISK WILL BE LOST")}
+            <FlexItem>
+              <SpacePolicyMenu modelDevice={modelDevice} device={device} />
+            </FlexItem>
+          </Flex>
+        </NestedContent>
+      </ExpandableSection>
+    </>
+  );
+}
