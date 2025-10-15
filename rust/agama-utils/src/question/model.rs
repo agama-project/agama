48148a71ef62efe6d61bbfb822f36d9926b2abb4
Imobach González Sosa <igonzalezsosa@suse.com>
2025-10-15 11:11:53 +0100

    Extend questions model documentation

--- rust/agama-utils/src/question/model.rs
+++ rust/agama-utils/src/question/model.rs
@@ -29,17 +29,28 @@ pub enum Error {
     InvalidAnswer(u32),
 }
 
+/// Represents a question including its [specification](QuestionSpec) and [answer](QuestionAnswer).
 #[derive(Clone, Debug, Serialize, Deserialize, utoipa::ToSchema)]
 #[serde(rename_all = "camelCase")]
 pub struct Question {
+    /// Question ID.
     pub id: u32,
+    /// Question specification.
     #[serde(flatten)]
     pub spec: QuestionSpec,
+    /// Question answer.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub answer: Option<QuestionAnswer>,
 }
 
 impl Question {
+    /// Creates a new question using the given ID and spec.
+    ///
+    /// The question does not have an answer yet. Use the [Question::set_answer]
+    /// function to set an answer.
+    ///
+    /// * `id`: question ID.
+    /// * `spec`: question specification.
     pub fn new(id: u32, spec: QuestionSpec) -> Self {
         Self {
             id,
@@ -48,34 +59,53 @@ impl Question {
         }
     }
 
-    // FIXME: check whether the answer is valid.
+    /// Sets an answer for the question.
+    ///
+    /// FIXME: check whether the answer is valid.
+    ///
+    /// * `answer`: question answer.
     pub fn set_answer(&mut self, answer: QuestionAnswer) -> Result<(), Error> {
         self.answer = Some(answer);
         Ok(())
     }
 }
 
+/// Defines how a question should look like.
 #[derive(Clone, Debug, Serialize, Deserialize, utoipa::ToSchema)]
 #[serde(rename_all = "camelCase")]
 pub struct QuestionSpec {
+    /// Question text.
     pub text: String,
+    /// Question class (e.g., "autoyast.unsupported"). It works as a hint for
+    /// the UI or to match pre-defined answers.
     pub class: String,
+    /// Optionally, a question might define an additional field (e.g., a
+    /// password, a selector, etc.).
     #[serde(default)]
     pub field: QuestionField,
+    /// List of available actions.
     pub actions: Vec<Action>,
+    /// Default action.
+    /// Question class (e.g., "autoyast.unsupported"). It works as a hint for
+    /// the UI or as the selected option when not using the "auto" policy.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub default_action: Option<String>,
-    // FIXME: set the proper value_type (or an alternative)
-    #[schema(value_type = String)]
+    /// Additional data that can be set for any question.
+    // FIXME: set the proper value_type.
+    #[schema(value_type = HashMap<String, String>)]
     #[serde(skip_serializing_if = "Option::is_none")]
     pub data: Option<HashMap<String, Box<RawValue>>>,
 }
 
 impl QuestionSpec {
-    pub fn new(text: &str, qclass: &str) -> Self {
+    /// Creates a new question specification.
+    ///
+    /// * `text`: question text.
+    /// * `class`: question class.
+    pub fn new(text: &str, class: &str) -> Self {
         Self {
             text: text.to_string(),
-            class: qclass.to_string(),
+            class: class.to_string(),
             field: QuestionField::None,
             actions: vec![],
             default_action: None,
@@ -83,16 +113,21 @@ impl QuestionSpec {
         }
     }
 
+    /// Sets the question field to be a string.
     pub fn as_string(mut self) -> Self {
         self.field = QuestionField::String;
         self
     }
 
+    /// Sets the question field to be a password.
     pub fn as_password(mut self) -> Self {
         self.field = QuestionField::Password;
         self
     }
 
+    /// Sets the question field to be a selector.
+    ///
+    /// * `options`: available options in `(id, label)` format.
     pub fn as_select(mut self, options: &[(&str, &str)]) -> Self {
         let options: Vec<_> = options
             .iter()
@@ -102,11 +137,19 @@ impl QuestionSpec {
         self
     }
 
+    /// Sets a default action.
+    ///
+    /// FIXME: check whether the action exists.
+    ///
+    /// * `action_id`: action identifier.
     pub fn with_default_action(mut self, action_id: &str) -> Self {
         self.default_action = Some(action_id.to_string());
         self
     }
 
+    /// Sets the available actions.o
+    ///
+    /// * `actions`: available actions in `(id, label)` format.
     pub fn with_actions(mut self, actions: &[(&str, &str)]) -> Self {
         self.actions = actions
             .iter()
@@ -116,18 +159,27 @@ impl QuestionSpec {
     }
 }
 
+/// Question field.
+///
+/// Apart from the actions, a question can include a field. The field can go
+/// from a string to a selector.
+///
+/// The field is usually presented as a control in a form.
 #[derive(Clone, Debug, Default, Serialize, Deserialize, utoipa::ToSchema)]
 #[serde(rename_all = "camelCase", tag = "type")]
 pub enum QuestionField {
+    /// No field.
     #[default]
     None,
+    /// A simple string field.
     String,
+    /// A password field.
     Password,
-    Select {
-        options: Vec<SelectionOption>,
-    },
+    /// A selector field.
+    Select { options: Vec<SelectionOption> },
 }
 
+/// Selector option.
 #[derive(Clone, Debug, Default, Serialize, Deserialize, utoipa::ToSchema)]
 pub struct SelectionOption {
     id: String,
@@ -143,6 +195,9 @@ impl SelectionOption {
     }
 }
 
+/// Question action.
+///
+/// They are usually presented as the button in a form.
 #[derive(Clone, Debug, Serialize, Deserialize, PartialEq, utoipa::ToSchema)]
 pub struct Action {
     /// Action value.
@@ -160,6 +215,12 @@ impl Action {
     }
 }
 
+/// Question answer.
+///
+/// It includes the action and, optionally, and additional value which depends
+/// on the question field. For instance, if you decide to decrypt a device using
+/// a password, the action could be "decrypt" and the value would e the
+/// password.
 #[derive(Clone, Debug, Serialize, Deserialize, utoipa::ToSchema)]
 pub struct QuestionAnswer {
     pub action: String,
