97103b93e6a49cb1628cbe15a3232f92e914067a
José Iván López González <jlopez@suse.com>
2025-10-24 15:14:42 +0100

    Add methods to dbus client

--- rust/agama-storage/src/dbus/client.rs
+++ rust/agama-storage/src/dbus/client.rs
@@ -21,12 +21,7 @@
 //! Implements a client to access Agama's storage service.
 
 use serde_json::value::RawValue;
-use std::collections::HashMap;
-use zbus::{
-    names::BusName,
-    zvariant::{self, OwnedObjectPath},
-    Connection,
-};
+use zbus::{names::BusName, zvariant::OwnedObjectPath, Connection, Message};
 
 const SERVICE_NAME: &str = "org.opensuse.Agama.Storage1";
 const OBJECT_PATH: &str = "/org/opensuse/Agama/Storage1";
@@ -55,40 +50,92 @@ impl Client {
         Self { connection }
     }
 
+    pub async fn activate(&self) -> Result<(), Error> {
+        self.call("Activate", &()).await?;
+        Ok(())
+    }
+
+    pub async fn probe(&self) -> Result<(), Error> {
+        self.call("Probe", &()).await?;
+        Ok(())
+    }
+
+    pub async fn install(&self) -> Result<(), Error> {
+        self.call("Install", &()).await?;
+        Ok(())
+    }
+
+    pub async fn finish(&self) -> Result<(), Error> {
+        self.call("Finish", &()).await?;
+        Ok(())
+    }
+
+    pub async fn get_system(&self) -> Result<Box<RawValue>, Error> {
+        let message = self.call("GetSystem", &()).await?;
+        self.json_from(message)
+    }
+
+    pub async fn get_config(&self) -> Result<Box<RawValue>, Error> {
+        let message = self.call("GetConfig", &()).await?;
+        self.json_from(message)
+    }
+
     pub async fn get_config_model(&self) -> Result<Box<RawValue>, Error> {
-        self.get_json("GetConfigModel").await
+        let message = self.call("GetConfigModel", &()).await?;
+        self.json_from(message)
+    }
+
+    pub async fn get_proposal(&self) -> Result<Box<RawValue>, Error> {
+        let message = self.call("GetProposal", &()).await?;
+        self.json_from(message)
+    }
+
+    pub async fn get_issues(&self) -> Result<Box<RawValue>, Error> {
+        let message = self.call("GetIssues", &()).await?;
+        self.json_from(message)
+    }
+
+    pub async fn set_config(&self, config: Box<RawValue>) -> Result<(), Error> {
+        self.call("SetConfig", &(config.to_string())).await?;
+        Ok(())
+    }
+
+    //TODO: send a product config instead of an id.
+    pub async fn set_product(&self, product_id: &str) -> Result<(), Error> {
+        self.call("SetProduct", &(product_id)).await?;
+        Ok(())
+    }
+
+    pub async fn set_locale(&self, locale: &str) -> Result<(), Error> {
+        self.call("SetLocale", &(locale)).await?;
+        Ok(())
     }
 
     pub async fn set_config_model(&self, model: Box<RawValue>) -> Result<(), Error> {
-        self.set_json("SetConfigModel", model).await
+        self.call("SetConfigModel", &(model.to_string())).await?;
+        Ok(())
     }
 
-    async fn get_json(&self, method: &str) -> Result<Box<RawValue>, Error> {
+    pub async fn solve_config_model(&self, model: Box<RawValue>) -> Result<Box<RawValue>, Error> {
+        let message = self.call("SolveConfigModel", &(model.to_string())).await?;
+        self.json_from(message)
+    }
+
+    async fn call<T: serde::ser::Serialize + zbus::zvariant::DynamicType>(
+        &self,
+        method: &str,
+        body: &T,
+    ) -> Result<Message, Error> {
         let bus = BusName::try_from(SERVICE_NAME.to_string())?;
         let path = OwnedObjectPath::try_from(OBJECT_PATH)?;
-        let message = self
-            .connection
-            .call_method(Some(&bus), &path, Some(INTERFACE), method, &())
-            .await?;
+        self.connection
+            .call_method(Some(&bus), &path, Some(INTERFACE), method, body)
+            .await
+            .map_err(|e| e.into())
+    }
 
+    fn json_from(&self, message: Message) -> Result<Box<RawValue>, Error> {
         let value: String = message.body().deserialize()?;
         RawValue::from_string(value).map_err(|e| e.into())
-    }
-
-    async fn set_json(&self, method: &str, json: Box<RawValue>) -> Result<(), Error> {
-        let bus = BusName::try_from(SERVICE_NAME.to_string())?;
-        let path = OwnedObjectPath::try_from(OBJECT_PATH)?;
-        let data: HashMap<&str, &zvariant::Value> = HashMap::new();
-        self.connection
-            .call_method(
-                Some(&bus),
-                &path,
-                Some(INTERFACE),
-                method,
-                &(json.to_string(), data),
-            )
-            .await?;
-
-        Ok(())
     }
 }
