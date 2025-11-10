b520d861131d4c1f2e6e09445789ee9725f6b3b2
Josef Reidinger <jreidinger@suse.cz>
2025-11-10 21:33:12 +0100

    add to C layer callbacks for download part of commit

--- rust/zypp-agama/zypp-agama-sys/c-layer/callbacks.cxx
+++ rust/zypp-agama/zypp-agama-sys/c-layer/callbacks.cxx
@@ -1,6 +1,7 @@
 #include <boost/bind.hpp>
 #include <zypp/Callback.h>
 #include <zypp/ZYppCallbacks.h>
+#include "zypp/target/rpm/RpmDb.h"
 
 #include "callbacks.h"
 
@@ -118,6 +119,158 @@ struct DownloadProgressReceive : public zypp::callback::ReceiveReport<
 
 static DownloadProgressReceive download_progress_receive;
 
+struct DownloadResolvableReport : public zypp::callback::ReceiveReport<
+                                      zypp::repo::DownloadResolvableReport> {
+  struct DownloadResolvableCallbacks *callbacks;
+
+  DownloadResolvableReport() { callbacks = NULL; }
+
+  void set_callbacks(DownloadResolvableCallbacks *callbacks_) {
+    callbacks = callbacks_;
+  }
+
+  virtual Action problem(zypp::Resolvable::constPtr resolvable_ptr, Error error,
+                         const std::string &description) {
+    if (callbacks != NULL && callbacks->problem != NULL) {
+      enum DownloadResolvableError error_enum;
+      switch (error) {
+      case zypp::repo::DownloadResolvableReport::NO_ERROR:
+        error_enum = DownloadResolvableError::DRE_NO_ERROR;
+        break;
+      case zypp::repo::DownloadResolvableReport::NOT_FOUND:
+        error_enum = DownloadResolvableError::DRE_NOT_FOUND;
+        break;
+      case zypp::repo::DownloadResolvableReport::IO:
+        error_enum = DownloadResolvableError::DRE_IO;
+        break;
+      case zypp::repo::DownloadResolvableReport::INVALID:
+        error_enum = DownloadResolvableError::DRE_INVALID;
+        break;
+      }
+      PROBLEM_RESPONSE response =
+          callbacks->problem(resolvable_ptr->name().c_str(), error_enum,
+                             description.c_str(), callbacks->problem_data);
+
+      switch (response) {
+      case PROBLEM_RETRY:
+        return zypp::repo::DownloadResolvableReport::RETRY;
+      case PROBLEM_ABORT:
+        return zypp::repo::DownloadResolvableReport::ABORT;
+      case PROBLEM_IGNORE:
+        return zypp::repo::DownloadResolvableReport::IGNORE;
+      }
+    }
+    // otherwise return the default value from the parent class
+    return zypp::repo::DownloadResolvableReport::problem(resolvable_ptr, error,
+                                                         description);
+  }
+
+  virtual void pkgGpgCheck(const UserData & userData_r = UserData() )
+    {
+      if (callbacks == NULL || callbacks->gpg_check == NULL) {
+        return;
+      }
+      zypp::ResObject::constPtr resobject = userData_r.get<zypp::ResObject::constPtr>("ResObject");
+      const zypp::RepoInfo repo = resobject->repoInfo();
+      const std::string repo_url = repo.rawUrl().asString();
+      typedef zypp::target::rpm::RpmDb RpmDb;
+      enum GPGCheckPackageResult result;
+      switch (userData_r.get<RpmDb::CheckPackageResult>("CheckPackageResult")){
+        case RpmDb::CHK_OK:
+          result = GPGCheckPackageResult::CHK_OK;
+          break;
+          case RpmDb::CHK_NOTFOUND:
+          result = GPGCheckPackageResult::CHK_NOTFOUND;
+          break;
+          case RpmDb::CHK_FAIL:
+          result = GPGCheckPackageResult::CHK_FAIL;
+          break;
+          case RpmDb::CHK_NOTTRUSTED:
+          result = GPGCheckPackageResult::CHK_NOTTRUSTED;
+          break;
+          case RpmDb::CHK_NOKEY:
+          result = GPGCheckPackageResult::CHK_NOKEY;
+          break;
+          case RpmDb::CHK_ERROR:
+          result = GPGCheckPackageResult::CHK_ERROR;
+          break;
+          case RpmDb::CHK_NOSIG:
+          result = GPGCheckPackageResult::CHK_NOSIG;
+          break;
+      };
+      PROBLEM_RESPONSE response = callbacks->gpg_check(resobject->name().c_str(), repo_url.c_str(), result, callbacks->gpg_check_data);
+      DownloadResolvableReport::Action zypp_action;
+      switch (response) {
+        case PROBLEM_RETRY:
+          zypp_action = zypp::repo::DownloadResolvableReport::RETRY;
+          break;
+        case PROBLEM_ABORT:
+          zypp_action = zypp::repo::DownloadResolvableReport::ABORT;
+          break;
+        case PROBLEM_IGNORE:
+          zypp_action = zypp::repo::DownloadResolvableReport::IGNORE;
+          break;
+      };
+      userData_r.set("Action", zypp_action);
+    }
+};
+
+static DownloadResolvableReport download_resolvable_receive;
+
+struct CommitPreloadReport
+    : public zypp::callback::ReceiveReport<zypp::media::CommitPreloadReport> {
+
+  struct DownloadResolvableCallbacks *callbacks;
+
+  CommitPreloadReport() { callbacks = NULL; }
+
+  void set_callbacks(DownloadResolvableCallbacks *callbacks_) {
+    callbacks = callbacks_;
+  }
+  virtual void start(const UserData &userData = UserData()) {
+    if (callbacks != NULL && callbacks->start_preload != NULL) {
+      callbacks->start_preload(callbacks->start_preload_data);
+    }
+  }
+
+  virtual void fileDone(const zypp::Pathname &localfile, Error error,
+                        const UserData &userData = UserData()) {
+    if (callbacks != NULL && callbacks->file_finish != NULL) {
+      const char *url = "";
+      if (userData.hasvalue("url")) {
+        url = userData.get<zypp::Url>("url").asString().c_str();
+      }
+      const char *local_path = localfile.c_str();
+      const char *error_details = "";
+      if (userData.hasvalue("description")) {
+        error_details = userData.get<std::string>("description").c_str();
+      }
+      enum DownloadResolvableFileError error_enum;
+      switch (error) {
+      case zypp::media::CommitPreloadReport::NO_ERROR:
+        error_enum = DownloadResolvableFileError::DRFE_NO_ERROR;
+        break;
+      case zypp::media::CommitPreloadReport::NOT_FOUND:
+        error_enum = DownloadResolvableFileError::DRFE_NOT_FOUND;
+        break;
+      case zypp::media::CommitPreloadReport::IO:
+        error_enum = DownloadResolvableFileError::DRFE_IO;
+        break;
+      case zypp::media::CommitPreloadReport::ACCESS_DENIED:
+        error_enum = DownloadResolvableFileError::DRFE_ACCESS_DENIED;
+        break;
+      case zypp::media::CommitPreloadReport::ERROR:
+        error_enum = DownloadResolvableFileError::DRFE_ERROR;
+        break;
+      }
+      callbacks->file_finish(url, local_path, error_enum, error_details,
+                             callbacks->file_finish_data);
+    }
+  }
+};
+
+static CommitPreloadReport commit_preload_report;
+
 extern "C" {
 void set_zypp_progress_callback(ZyppProgressCallback progress,
                                 void *user_data) {
@@ -135,6 +288,24 @@ void unset_zypp_download_callbacks() {
   // NULL pointer to struct to be sure it is not called
   download_progress_receive.set_callbacks(NULL);
   download_progress_receive.disconnect();
+}
+
+// Sets both reports as we consolidate download resolvables
+// and commitPreload into one set for easier hooking
+void set_zypp_resolvable_download_callbacks(
+    struct DownloadResolvableCallbacks *callbacks) {
+  download_resolvable_receive.set_callbacks(callbacks);
+  download_resolvable_receive.connect();
+  commit_preload_report.set_callbacks(callbacks);
+  commit_preload_report.connect();
+}
+
+void unset_zypp_resolvable_download_callbacks() {
+  // NULL pointer to struct to be sure it is not called
+  download_resolvable_receive.set_callbacks(NULL);
+  download_resolvable_receive.disconnect();
+  commit_preload_report.set_callbacks(NULL);
+  commit_preload_report.disconnect();
 }
 
 #ifdef __cplusplus
--- rust/zypp-agama/zypp-agama-sys/c-layer/include/callbacks.h
+++ rust/zypp-agama/zypp-agama-sys/c-layer/include/callbacks.h
@@ -53,6 +53,67 @@ struct DownloadProgressCallbacks {
   ZyppDownloadFinishCallback finish;
   void *finish_data;
 };
+
+enum DownloadResolvableError {
+  DRE_NO_ERROR,
+  DRE_NOT_FOUND, // the requested Url was not found
+  DRE_IO,        // IO error
+  DRE_INVALID    // the downloaded file is invalid
+};
+
+enum DownloadResolvableFileError {
+  DRFE_NO_ERROR,
+  DRFE_NOT_FOUND,     // the requested Url was not found
+  DRFE_IO,            // IO error
+  DRFE_ACCESS_DENIED, // user authent. failed while accessing restricted file
+  DRFE_ERROR          // other error
+};
+
+// keep in sync with https://github.com/openSUSE/libzypp/blob/master/zypp-logic/zypp/target/rpm/RpmDb.h#L376
+// maybe there is a better way to export it to C?
+enum GPGCheckPackageResult
+  {
+    CHK_OK            = 0, /*!< Signature is OK. */
+    CHK_NOTFOUND      = 1, /*!< Signature is unknown type. */
+    CHK_FAIL          = 2, /*!< Signature does not verify. */
+    CHK_NOTTRUSTED    = 3, /*!< Signature is OK, but key is not trusted. */
+    CHK_NOKEY         = 4, /*!< Public key is unavailable. */
+    CHK_ERROR         = 5, /*!< File does not exist or can't be opened. */
+    CHK_NOSIG         = 6, /*!< File has no gpg signature (only digests). */
+  };
+
+typedef void (*ZyppDownloadResolvableStartCallback)(void *user_data);
+// TODO: do we need more resolvable details? for now just use name and url
+typedef enum PROBLEM_RESPONSE (*ZyppDownloadResolvableProblemCallback)(
+    const char *resolvable_name, enum DownloadResolvableError error,
+    const char *description, void *user_data);
+typedef enum PROBLEM_RESPONSE (*ZyppDownloadResolvableGpgCheckCallback)(
+    const char *resolvable_name, const char *repo_url,
+    enum GPGCheckPackageResult check_result,
+    void *user_data);
+typedef void (*ZyppDownloadResolvableFileFinishCallback)(
+    const char *url, const char *local_path,
+    enum DownloadResolvableFileError error, const char *error_details,
+    void *user_data);
+
+// progress for downloading resolvables (rpms). There are 3 callbacks now ( can
+// be extended with progress and finish one):
+// 1. start for start of preload
+// 2. problem to react when something wrong happen and how to behave
+// 3. gpg_check when there is issue with gpg check on resolvable
+// 4. finish_file is when preload finish download of package including failed
+// NOTE: user_data is separated for each call.
+// NOTE: libzypp provides more data, but only those used by agama is used now.
+struct DownloadResolvableCallbacks {
+  ZyppDownloadResolvableStartCallback start_preload;
+  void *start_preload_data;
+  ZyppDownloadResolvableProblemCallback problem;
+  void *problem_data;
+  ZyppDownloadResolvableGpgCheckCallback gpg_check;
+  void *gpg_check_data;
+  ZyppDownloadResolvableFileFinishCallback file_finish;
+  void *file_finish_data;
+};
 #ifdef __cplusplus
 }
 #endif
