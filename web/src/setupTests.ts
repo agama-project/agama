3fb632a169b63adfdb786a8ce6da5d94a3f741b1
David Díaz González <dgonzalez@suse.com>
2025-11-10 15:09:20 +0000

    web: fix ReferenceError after React Router v7 migration
    After migrating to React Router v7, the test suite fails with the
    following error:
    
    > ReferenceError: TextEncoder is not defined
    
    This appears to be due to an unimplemented TextEncoder in jsdom. To
    resolve this, we follow the same workaround as React Router, as outlined
    in https://github.com/remix-run/react-router/issues/12363#issuecomment-2496226528

--- web/src/setupTests.ts
+++ web/src/setupTests.ts
@@ -3,3 +3,11 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
+import { TextDecoder, TextEncoder } from "util";
+
+globalThis.IS_REACT_ACT_ENVIRONMENT = true;
+
+if (!globalThis.TextEncoder || !globalThis.TextDecoder) {
+  globalThis.TextEncoder = TextEncoder;
+  globalThis.TextDecoder = TextDecoder;
+}
