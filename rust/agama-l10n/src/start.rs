961d15266ea25b6138e26d9585d3fb358f061261
Imobach González Sosa <igonzalezsosa@suse.com>
2025-11-13 16:54:45 +0000

    Use test_context in l10n start function tests

--- rust/agama-l10n/src/start.rs
+++ rust/agama-l10n/src/start.rs
@@ -75,12 +75,13 @@ mod tests {
         actor::{self, Handler},
         api::{
             self,
-            event::{self, Event},
+            event::Event,
             l10n::{Keymap, LocaleEntry, TimezoneEntry},
             scope::Scope,
         },
         issue, test,
     };
+    use test_context::{test_context, AsyncTestContext};
     use tokio::sync::broadcast;
 
     pub struct TestModel {
@@ -146,24 +147,34 @@ mod tests {
         }
     }
 
-    async fn start_testing_service() -> (event::Receiver, Handler<Service>, Handler<issue::Service>)
-    {
-        let (events_tx, events_rx) = broadcast::channel::<Event>(16);
-        let dbus = test::dbus::connection().await.unwrap();
-        let issues = issue::start(events_tx.clone(), dbus).await.unwrap();
-
-        let model = build_adapter();
-        let service = Service::new(model, issues.clone(), events_tx);
-
-        let handler = actor::spawn(service);
-        (events_rx, handler, issues)
+    struct Context {
+        events_rx: broadcast::Receiver<Event>,
+        handler: Handler<Service>,
+        issues: Handler<issue::Service>,
     }
 
-    #[tokio::test]
-    async fn test_get_and_set_config() -> Result<(), Box<dyn std::error::Error>> {
-        let (mut events_rx, handler, _issues) = start_testing_service().await;
+    impl AsyncTestContext for Context {
+        async fn setup() -> Context {
+            let (events_tx, events_rx) = broadcast::channel::<Event>(16);
+            let dbus = test::dbus::connection().await.unwrap();
+            let issues = issue::start(events_tx.clone(), dbus).await.unwrap();
 
-        let config = handler.call(message::GetConfig).await.unwrap();
+            let model = build_adapter();
+            let service = Service::new(model, issues.clone(), events_tx);
+            let handler = actor::spawn(service);
+
+            Self {
+                events_rx,
+                handler,
+                issues,
+            }
+        }
+    }
+
+    #[test_context(Context)]
+    #[tokio::test]
+    async fn test_get_and_set_config(ctx: &mut Context) -> Result<(), Box<dyn std::error::Error>> {
+        let config = ctx.handler.call(message::GetConfig).await.unwrap();
         assert_eq!(config.locale, Some("en_US.UTF-8".to_string()));
 
         let input_config = api::l10n::Config {
@@ -171,17 +182,21 @@ mod tests {
             keymap: Some("es".to_string()),
             timezone: Some("Atlantic/Canary".to_string()),
         };
-        handler
+        ctx.handler
             .call(message::SetConfig::with(input_config.clone()))
             .await?;
 
-        let updated = handler.call(message::GetConfig).await?;
+        let updated = ctx.handler.call(message::GetConfig).await?;
         assert_eq!(&updated, &input_config);
 
-        let proposal = handler.call(message::GetProposal).await?;
+        let proposal = ctx.handler.call(message::GetProposal).await?;
         assert!(proposal.is_some());
 
-        let event = events_rx.recv().await.expect("Did not receive the event");
+        let event = ctx
+            .events_rx
+            .recv()
+            .await
+            .expect("Did not receive the event");
         assert!(matches!(
             event,
             Event::ProposalChanged { scope: Scope::L10n }
@@ -194,11 +209,11 @@ mod tests {
         };
 
         // Use system info for missing values.
-        handler
+        ctx.handler
             .call(message::SetConfig::with(input_config.clone()))
             .await?;
 
-        let updated = handler.call(message::GetConfig).await?;
+        let updated = ctx.handler.call(message::GetConfig).await?;
         assert_eq!(
             updated,
             api::l10n::Config {
@@ -211,13 +226,12 @@ mod tests {
         Ok(())
     }
 
+    #[test_context(Context)]
     #[tokio::test]
-    async fn test_reset_config() -> Result<(), Box<dyn std::error::Error>> {
-        let (mut _events_rx, handler, _issues) = start_testing_service().await;
+    async fn test_reset_config(ctx: &mut Context) -> Result<(), Box<dyn std::error::Error>> {
+        ctx.handler.call(message::SetConfig::new(None)).await?;
 
-        handler.call(message::SetConfig::new(None)).await?;
-
-        let config = handler.call(message::GetConfig).await?;
+        let config = ctx.handler.call(message::GetConfig).await?;
         assert_eq!(
             config,
             api::l10n::Config {
@@ -230,81 +244,82 @@ mod tests {
         Ok(())
     }
 
+    #[test_context(Context)]
     #[tokio::test]
-    async fn test_set_invalid_config() -> Result<(), Box<dyn std::error::Error>> {
-        let (_events_rx, handler, _issues) = start_testing_service().await;
-
+    async fn test_set_invalid_config(ctx: &mut Context) -> Result<(), Box<dyn std::error::Error>> {
         let input_config = api::l10n::Config {
             locale: Some("es-ES.UTF-8".to_string()),
             ..Default::default()
         };
 
-        let result = handler
+        let result = ctx
+            .handler
             .call(message::SetConfig::with(input_config.clone()))
             .await;
         assert!(matches!(result, Err(service::Error::InvalidLocale(_))));
         Ok(())
     }
 
+    #[test_context(Context)]
     #[tokio::test]
-    async fn test_set_config_without_changes() -> Result<(), Box<dyn std::error::Error>> {
-        let (mut events_rx, handler, _issues) = start_testing_service().await;
-
-        let config = handler.call(message::GetConfig).await?;
+    async fn test_set_config_without_changes(
+        ctx: &mut Context,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        let config = ctx.handler.call(message::GetConfig).await?;
         assert_eq!(config.locale, Some("en_US.UTF-8".to_string()));
         let message = message::SetConfig::with(config.clone());
-        handler.call(message).await?;
+        ctx.handler.call(message).await?;
         // Wait until the action is dispatched.
-        let _ = handler.call(message::GetConfig).await?;
+        let _ = ctx.handler.call(message::GetConfig).await?;
 
-        let event = events_rx.try_recv();
+        let event = ctx.events_rx.try_recv();
         assert!(matches!(event, Err(broadcast::error::TryRecvError::Empty)));
         Ok(())
     }
 
+    #[test_context(Context)]
     #[tokio::test]
-    async fn test_set_config_unknown_values() -> Result<(), Box<dyn std::error::Error>> {
-        let (mut _events_rx, handler, issues) = start_testing_service().await;
-
+    async fn test_set_config_unknown_values(
+        ctx: &mut Context,
+    ) -> Result<(), Box<dyn std::error::Error>> {
         let config = api::l10n::Config {
             keymap: Some("jk".to_string()),
             locale: Some("xx_XX.UTF-8".to_string()),
             timezone: Some("Unknown/Unknown".to_string()),
         };
-        let _ = handler.call(message::SetConfig::with(config)).await?;
+        let _ = ctx.handler.call(message::SetConfig::with(config)).await?;
 
-        let found_issues = issues.call(issue::message::Get).await?;
+        let found_issues = ctx.issues.call(issue::message::Get).await?;
         let l10n_issues = found_issues.get(&Scope::L10n).unwrap();
         assert_eq!(l10n_issues.len(), 3);
 
-        let proposal = handler.call(message::GetProposal).await?;
+        let proposal = ctx.handler.call(message::GetProposal).await?;
         assert!(proposal.is_none());
         Ok(())
     }
 
+    #[test_context(Context)]
     #[tokio::test]
-    async fn test_get_system() -> Result<(), Box<dyn std::error::Error>> {
-        let (_events_rx, handler, _issues) = start_testing_service().await;
-
-        let system = handler.call(message::GetSystem).await?;
+    async fn test_get_system(ctx: &mut Context) -> Result<(), Box<dyn std::error::Error>> {
+        let system = ctx.handler.call(message::GetSystem).await?;
         assert_eq!(system.keymaps.len(), 2);
 
         Ok(())
     }
 
+    #[test_context(Context)]
     #[tokio::test]
-    async fn test_get_proposal() -> Result<(), Box<dyn std::error::Error>> {
-        let (_events_rx, handler, _issues) = start_testing_service().await;
-
+    async fn test_get_proposal(ctx: &mut Context) -> Result<(), Box<dyn std::error::Error>> {
         let input_config = api::l10n::Config {
             locale: Some("es_ES.UTF-8".to_string()),
             keymap: Some("es".to_string()),
             timezone: Some("Atlantic/Canary".to_string()),
         };
         let message = message::SetConfig::with(input_config.clone());
-        handler.call(message).await?;
+        ctx.handler.call(message).await?;
 
-        let proposal = handler
+        let proposal = ctx
+            .handler
             .call(message::GetProposal)
             .await?
             .expect("Could not get the proposal");
