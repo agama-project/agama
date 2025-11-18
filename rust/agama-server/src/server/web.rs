0222271b2bd8c076d44b956ea5c8c9744dc8d331
Imobach González Sosa <igonzalezsosa@suse.com>
2025-11-18 10:35:17 +0000

    Update agama-server tests

--- rust/agama-server/src/server/web.rs
+++ rust/agama-server/src/server/web.rs
@@ -73,6 +73,12 @@ pub struct ServerState {
     questions: Handler<question::Service>,
 }
 
+impl ServerState {
+    pub fn new(manager: Handler<manager::Service>, questions: Handler<question::Service>) -> Self {
+        Self { manager, questions }
+    }
+}
+
 type ServerResult<T> = Result<T, Error>;
 
 /// Sets up and returns the axum service for the manager module
@@ -91,9 +97,10 @@ pub async fn server_service(
         .spawn()
         .await
         .map_err(anyhow::Error::msg)?;
-
-    let state = ServerState { manager, questions };
-
+    let state = ServerState::new(manager, questions);
+    server_with_state(state)
+}
+pub fn server_with_state(state: ServerState) -> Result<Router, ServiceError> {
     Ok(Router::new()
         .route("/status", get(get_status))
         .route("/system", get(get_system))
