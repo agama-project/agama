6942f30cad57db120ad498a1e7b56305301a7449
Josef Reidinger <jreidinger@suse.cz>
2025-11-24 14:35:40 +0100

    cargo fmt

--- rust/agama-software/src/callbacks.rs
+++ rust/agama-software/src/callbacks.rs
@@ -1,15 +1,17 @@
 mod commit_download;
-use agama_utils::{actor::Handler, api::question::{Answer, QuestionSpec}, question::{self, AskError, ask_question}};
+use agama_utils::{
+    actor::Handler,
+    api::question::{Answer, QuestionSpec},
+    question::{self, ask_question, AskError},
+};
 pub use commit_download::CommitDownload;
 mod security;
 pub use security::Security;
 use tokio::runtime::Handle;
 
-
 fn ask_software_question(
     handler: &Handler<question::Service>,
     question: QuestionSpec,
 ) -> Result<Answer, AskError> {
-    Handle::current()
-      .block_on(async move { ask_question(handler, question).await })
+    Handle::current().block_on(async move { ask_question(handler, question).await })
 }
