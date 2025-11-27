fc26e3b4a5b0f2876fe86315ceb631fa5f51831b
Josef Reidinger <jreidinger@suse.cz>
2025-11-27 14:11:20 +0100

    initial version of suseconnect wrapper

---
+++ rust/suseconnect-agama/suseconnect-agama-sys/headers.h
@@ -0,0 +1,11 @@
+#ifndef HEADERS_H
+#define HEADERS_H
+
+
+char * announce_system(char *client_params, char *distro_target);
+char * create_credentials_file(char *login, char *password, char *_unused_empty, char *credentials_file);
+char * activate_product(char *product, char *client_params, char *email);
+char * write_config(char *client_params);
+char * show_product(char *product, char *client_params);
+
+#endif
