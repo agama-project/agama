038c42f15f8820d847a8db239f5dad0820eea8b9
Ladislav Slezák <lslezak@suse.com>
2025-12-01 21:53:03 +0100

    Added new boot option "live.net_config=1"
    to interactively configure network settings using the "nmtui" tool.

---
+++ live/live-root/usr/lib/dracut/modules.d/99initrd-nmtui/initrd-nmtui-cmdline.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# automatically configure network and start NetworkManager when live.net_config=1
+
+. /lib/dracut-lib.sh
+
+# generate Dracut command line options
+FILE="/etc/cmdline.d/initrd-nmtui.conf"
+
+# only if interactive network config requested
+if getargbool 0 live.net_config; then
+  # and network not disabled
+  if getargbool 1 rd.neednet; then
+    echo "rd.neednet=1" >"$FILE"
+    # use DHCP if there is no network configuration provided by user
+    if ! getarg "ip="; then
+      echo "ip=dhcp" >>"$FILE"
+    fi
+  fi
+fi
