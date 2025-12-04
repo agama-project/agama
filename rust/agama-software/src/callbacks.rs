5a0d639dcfea1509ba77aabafa5789d215368256
Martin Vidner <mvidner@suse.com>
2025-12-04 16:03:00 +0100

    ask_software_question: ask the question in a new thread, but still block on it

--- rust/agama-software/src/callbacks.rs
+++ rust/agama-software/src/callbacks.rs
@@ -9,11 +9,22 @@ mod security;
 pub use security::Security;
 mod install;
 pub use install::Install;
-use tokio::runtime::Handle;
+use std::sync::mpsc;
+use tokio::runtime::Builder;
 
 fn ask_software_question(
     handler: &Handler<question::Service>,
     question: QuestionSpec,
 ) -> Result<Answer, AskError> {
-    Handle::current().block_on(async move { ask_question(handler, question).await })
+    let (tx, rx) = mpsc::channel();
+    let question_handler = handler.clone();
+    std::thread::spawn(move || {
+        let runtime = Builder::new_current_thread().enable_all().build().unwrap();
+        let result =
+            runtime.block_on(async move { ask_question(&question_handler, question).await });
+        // unwrap OK: the receiver should not be dropped before the thread finishes
+        tx.send(result).unwrap();
+    });
+    // unwrap OK: the sender should not be dropped before sending a value
+    rx.recv().unwrap()
 }
