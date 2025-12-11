ea142becdc23ca42d09deef74ba41d45b626c668
Imobach González Sosa <igonzalezsosa@suse.com>
2025-12-11 15:18:18 +0000

    Use test_context in progress services tests

--- rust/agama-utils/src/progress.rs
+++ rust/agama-utils/src/progress.rs
@@ -38,24 +38,31 @@ mod tests {
             service::{self, Service},
         },
     };
+    use test_context::{test_context, AsyncTestContext};
     use tokio::sync::broadcast;
 
-    fn start_testing_service() -> (event::Receiver, Handler<Service>) {
-        let (events, receiver) = broadcast::channel::<Event>(16);
-        let handler = Service::starter(events).start();
-        (receiver, handler)
+    struct Context {
+        events_rx: event::Receiver,
+        handler: Handler<Service>,
     }
 
-    #[tokio::test]
-    async fn test_progress() -> Result<(), Box<dyn std::error::Error>> {
-        let (mut receiver, handler) = start_testing_service();
+    impl AsyncTestContext for Context {
+        async fn setup() -> Self {
+            let (events_tx, events_rx) = broadcast::channel::<Event>(16);
+            let handler = Service::starter(events_tx).start();
+            Self { events_rx, handler }
+        }
+    }
 
+    #[test_context(Context)]
+    #[tokio::test]
+    async fn test_progress(ctx: &mut Context) -> Result<(), Box<dyn std::error::Error>> {
         // Start a progress (first step)
-        handler
+        ctx.handler
             .call(message::Start::new(Scope::L10n, 3, "first step"))
             .await?;
 
-        let event = receiver.recv().await.unwrap();
+        let event = ctx.events_rx.recv().await.unwrap();
         let Event::ProgressChanged {
             progress: event_progress,
         } = event
@@ -69,21 +76,21 @@ mod tests {
         assert_eq!(event_progress.step, "first step");
         assert_eq!(event_progress.index, 1);
 
-        let progresses = handler.call(message::GetProgress).await?;
+        let progresses = ctx.handler.call(message::GetProgress).await?;
         assert_eq!(progresses.len(), 1);
 
         let progress = progresses.first().unwrap();
         assert_eq!(*progress, event_progress);
 
         // Second step
-        handler
+        ctx.handler
             .call(message::NextWithStep::new(Scope::L10n, "second step"))
             .await?;
 
-        let event = receiver.recv().await.unwrap();
+        let event = ctx.events_rx.recv().await.unwrap();
         assert!(matches!(event, Event::ProgressChanged { progress: _ }));
 
-        let progresses = handler.call(message::GetProgress).await.unwrap();
+        let progresses = ctx.handler.call(message::GetProgress).await.unwrap();
         let progress = progresses.first().unwrap();
         assert_eq!(progress.scope, Scope::L10n);
         assert_eq!(progress.size, 3);
@@ -92,12 +99,12 @@ mod tests {
         assert_eq!(progress.index, 2);
 
         // Last step (without step text)
-        handler.call(message::Next::new(Scope::L10n)).await?;
+        ctx.handler.call(message::Next::new(Scope::L10n)).await?;
 
-        let event = receiver.recv().await.unwrap();
+        let event = ctx.events_rx.recv().await.unwrap();
         assert!(matches!(event, Event::ProgressChanged { progress: _ }));
 
-        let progresses = handler.call(message::GetProgress).await.unwrap();
+        let progresses = ctx.handler.call(message::GetProgress).await.unwrap();
         let progress = progresses.first().unwrap();
         assert_eq!(progress.scope, Scope::L10n);
         assert_eq!(progress.size, 3);
@@ -106,29 +113,30 @@ mod tests {
         assert_eq!(progress.index, 3);
 
         // Finish the progress
-        handler.call(message::Finish::new(Scope::L10n)).await?;
+        ctx.handler.call(message::Finish::new(Scope::L10n)).await?;
 
-        let event = receiver.recv().await.unwrap();
+        let event = ctx.events_rx.recv().await.unwrap();
         assert!(matches!(
             event,
             Event::ProgressFinished { scope: Scope::L10n }
         ));
 
-        let progresses = handler.call(message::GetProgress).await.unwrap();
+        let progresses = ctx.handler.call(message::GetProgress).await.unwrap();
         assert!(progresses.is_empty());
 
         Ok(())
     }
 
+    #[test_context(Context)]
     #[tokio::test]
-    async fn test_set_progress() -> Result<(), Box<dyn std::error::Error>> {
-        let (mut receiver, handler) = start_testing_service();
-
+    async fn test_set_progress(ctx: &mut Context) -> Result<(), Box<dyn std::error::Error>> {
         // Set first progress.
         let progress = Progress::new(Scope::Storage, 3, "first step".to_string());
-        handler.call(message::SetProgress::new(progress)).await?;
+        ctx.handler
+            .call(message::SetProgress::new(progress))
+            .await?;
 
-        let event = receiver.recv().await.unwrap();
+        let event = ctx.events_rx.recv().await.unwrap();
         let Event::ProgressChanged {
             progress: event_progress,
         } = event
@@ -142,7 +150,7 @@ mod tests {
         assert_eq!(event_progress.step, "first step");
         assert_eq!(event_progress.index, 1);
 
-        let progresses = handler.call(message::GetProgress).await?;
+        let progresses = ctx.handler.call(message::GetProgress).await?;
         assert_eq!(progresses.len(), 1);
 
         let progress = progresses.first().unwrap();
@@ -150,9 +158,11 @@ mod tests {
 
         // Set second progress
         let progress = Progress::new(Scope::Storage, 3, "second step".to_string());
-        handler.call(message::SetProgress::new(progress)).await?;
+        ctx.handler
+            .call(message::SetProgress::new(progress))
+            .await?;
 
-        let event = receiver.recv().await.unwrap();
+        let event = ctx.events_rx.recv().await.unwrap();
         let Event::ProgressChanged {
             progress: event_progress,
         } = event
@@ -166,7 +176,7 @@ mod tests {
         assert_eq!(event_progress.step, "second step");
         assert_eq!(event_progress.index, 1);
 
-        let progresses = handler.call(message::GetProgress).await?;
+        let progresses = ctx.handler.call(message::GetProgress).await?;
         assert_eq!(progresses.len(), 1);
 
         let progress = progresses.first().unwrap();
@@ -175,19 +185,18 @@ mod tests {
         Ok(())
     }
 
+    #[test_context(Context)]
     #[tokio::test]
-    async fn test_progress_with_steps() -> Result<(), Box<dyn std::error::Error>> {
-        let (_receiver, handler) = start_testing_service();
-
+    async fn test_progress_with_steps(ctx: &mut Context) -> Result<(), Box<dyn std::error::Error>> {
         // Start a progress (first step)
-        handler
+        ctx.handler
             .call(message::StartWithSteps::new(
                 Scope::L10n,
                 &["first step", "second step", "third step"],
             ))
             .await?;
 
-        let progresses = handler.call(message::GetProgress).await?;
+        let progresses = ctx.handler.call(message::GetProgress).await?;
         let progress = progresses.first().unwrap();
         assert_eq!(progress.scope, Scope::L10n);
         assert_eq!(progress.size, 3);
@@ -199,42 +208,41 @@ mod tests {
         assert_eq!(progress.index, 1);
 
         // Second step
-        handler.call(message::Next::new(Scope::L10n)).await?;
+        ctx.handler.call(message::Next::new(Scope::L10n)).await?;
 
-        let progresses = handler.call(message::GetProgress).await.unwrap();
+        let progresses = ctx.handler.call(message::GetProgress).await.unwrap();
         let progress = progresses.first().unwrap();
         assert_eq!(progress.step, "second step");
         assert_eq!(progress.index, 2);
 
         // Third step
-        handler.call(message::Next::new(Scope::L10n)).await?;
+        ctx.handler.call(message::Next::new(Scope::L10n)).await?;
 
-        let progresses = handler.call(message::GetProgress).await.unwrap();
+        let progresses = ctx.handler.call(message::GetProgress).await.unwrap();
         let progress = progresses.first().unwrap();
         assert_eq!(progress.step, "third step");
         assert_eq!(progress.index, 3);
 
         // Finish the progress
-        handler.call(message::Finish::new(Scope::L10n)).await?;
+        ctx.handler.call(message::Finish::new(Scope::L10n)).await?;
 
-        let progresses = handler.call(message::GetProgress).await.unwrap();
+        let progresses = ctx.handler.call(message::GetProgress).await.unwrap();
         assert!(progresses.is_empty());
 
         Ok(())
     }
 
+    #[test_context(Context)]
     #[tokio::test]
-    async fn test_several_progresses() -> Result<(), Box<dyn std::error::Error>> {
-        let (_receiver, handler) = start_testing_service();
-
-        handler
+    async fn test_several_progresses(ctx: &mut Context) -> Result<(), Box<dyn std::error::Error>> {
+        ctx.handler
             .call(message::Start::new(Scope::Manager, 2, ""))
             .await?;
-        handler
+        ctx.handler
             .call(message::Start::new(Scope::L10n, 2, ""))
             .await?;
 
-        let progresses = handler.call(message::GetProgress).await.unwrap();
+        let progresses = ctx.handler.call(message::GetProgress).await.unwrap();
         assert_eq!(progresses.len(), 2);
         assert_eq!(progresses[0].scope, Scope::Manager);
         assert_eq!(progresses[1].scope, Scope::L10n);
@@ -242,14 +250,15 @@ mod tests {
         Ok(())
     }
 
+    #[test_context(Context)]
     #[tokio::test]
-    async fn test_progress_missing_step() -> Result<(), Box<dyn std::error::Error>> {
-        let (_receiver, handler) = start_testing_service();
-
-        handler
+    async fn test_progress_missing_step(
+        ctx: &mut Context,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        ctx.handler
             .call(message::Start::new(Scope::L10n, 1, ""))
             .await?;
-        let error = handler.call(message::Next::new(Scope::L10n)).await;
+        let error = ctx.handler.call(message::Next::new(Scope::L10n)).await;
         assert!(matches!(
             error,
             Err(service::Error::Progress(progress::Error::MissingStep(
@@ -260,14 +269,13 @@ mod tests {
         Ok(())
     }
 
+    #[test_context(Context)]
     #[tokio::test]
-    async fn test_missing_progress() -> Result<(), Box<dyn std::error::Error>> {
-        let (_receiver, handler) = start_testing_service();
-
-        handler
+    async fn test_missing_progress(ctx: &mut Context) -> Result<(), Box<dyn std::error::Error>> {
+        ctx.handler
             .call(message::Start::new(Scope::Manager, 2, ""))
             .await?;
-        let error = handler.call(message::Next::new(Scope::L10n)).await;
+        let error = ctx.handler.call(message::Next::new(Scope::L10n)).await;
         assert!(matches!(
             error,
             Err(service::Error::MissingProgress(Scope::L10n))
@@ -276,21 +284,24 @@ mod tests {
         Ok(())
     }
 
+    #[test_context(Context)]
     #[tokio::test]
-    async fn test_duplicated_progress() -> Result<(), Box<dyn std::error::Error>> {
-        let (_receiver, handler) = start_testing_service();
-
-        handler
+    async fn test_duplicated_progress(ctx: &mut Context) -> Result<(), Box<dyn std::error::Error>> {
+        ctx.handler
             .call(message::Start::new(Scope::L10n, 2, ""))
             .await?;
 
-        let error = handler.call(message::Start::new(Scope::L10n, 1, "")).await;
+        let error = ctx
+            .handler
+            .call(message::Start::new(Scope::L10n, 1, ""))
+            .await;
         assert!(matches!(
             error,
             Err(service::Error::DuplicatedProgress(Scope::L10n))
         ));
 
-        let error = handler
+        let error = ctx
+            .handler
             .call(message::StartWithSteps::new(Scope::L10n, &["step"]))
             .await;
         assert!(matches!(
@@ -301,18 +312,17 @@ mod tests {
         Ok(())
     }
 
+    #[test_context(Context)]
     #[tokio::test]
-    async fn test_set_and_get_stage() -> Result<(), Box<dyn std::error::Error>> {
-        let (_receiver, handler) = start_testing_service();
-
-        let status = handler.call(message::GetStatus).await?;
+    async fn test_set_and_get_stage(ctx: &mut Context) -> Result<(), Box<dyn std::error::Error>> {
+        let status = ctx.handler.call(message::GetStatus).await?;
         assert_eq!(status.stage, Stage::Configuring);
 
-        handler
+        ctx.handler
             .call(message::SetStage::new(Stage::Installing))
             .await?;
 
-        let status = handler.call(message::GetStatus).await?;
+        let status = ctx.handler.call(message::GetStatus).await?;
         assert_eq!(status.stage, Stage::Installing);
         Ok(())
     }
