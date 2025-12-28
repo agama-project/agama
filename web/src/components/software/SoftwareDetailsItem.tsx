9accd4a8972c4ca439db2b1f59bf331c0318f8e0
David Díaz González <dgonzalez@suse.com>
2025-12-28 12:26:45 +0000

    web: add initial software summary for overview revamp

---
+++ web/src/components/software/SoftwareDetailsItem.tsx
@@ -0,0 +1,124 @@
+/*
+ * Copyright (c) [2025] SUSE LLC
+ *
+ * All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, contact SUSE LLC.
+ *
+ * To contact SUSE LLC about this file by physical or electronic mail, you may
+ * find current contact information at www.suse.com.
+ */
+
+import React, { useEffect, useState } from "react";
+import xbytes from "xbytes";
+import { shake } from "radashi";
+import { Flex, Skeleton } from "@patternfly/react-core";
+import Details from "~/components/core/Details";
+import Link from "~/components/core/Link";
+import { useStatus } from "~/hooks/model/status";
+import { useSystem } from "~/hooks/model/system/software";
+import { useProposal } from "~/hooks/model/proposal/software";
+import { SelectedBy } from "~/model/proposal/software";
+import { SOFTWARE } from "~/routes/paths";
+import { sprintf } from "sprintf-js";
+import { _, n_ } from "~/i18n";
+import { COMMON_PROPOSAL_KEYS } from "~/hooks/model/proposal";
+import useTrackQueriesRefetch from "~/hooks/use-track-queries-refetch";
+
+// FIXME: copied from StorageDetailsItem,
+// FIXME: evaluate to extract this hook and use it instead of repeating such a
+// #find in multiple places
+const useProgress = (scope) => {
+  const { progresses } = useStatus();
+  return progresses.find((p) => p.scope === scope);
+};
+
+// TODO: put in a more generic, reusable place
+const useSelectedPatterns = () => {
+  const proposal = useProposal();
+  const { patterns } = useSystem();
+
+  const selectedPatternsKeys = Object.keys(
+    shake(proposal.patterns, (value) => value === SelectedBy.NONE),
+  );
+
+  return patterns.filter((p) => selectedPatternsKeys.includes(p.name));
+};
+
+const Summary = () => {
+  const patterns = useSelectedPatterns();
+  const patternsQty = patterns.length;
+
+  if (patternsQty === 0) {
+    return _("Base system");
+  }
+
+  return sprintf(
+    // TRANSLATORS: %s will be replaced with amount of selected patterns.
+    n_("Base system and %s pattern", "Base system and %s patterns", patternsQty),
+    patternsQty,
+  );
+};
+
+const Description = () => {
+  const proposal = useProposal();
+
+  if (!proposal.usedSpace) return;
+
+  return sprintf(
+    // TRANSLATORS: %s will be replaced with a human-readable installation size
+    // (e.g. 5.95 GiB).
+    _("Estimated installation size: %s"),
+    xbytes(proposal.usedSpace * 1024, { iec: true }),
+  );
+};
+
+export default function SoftwareDetailsItem() {
+  const progress = useProgress("software");
+  const [loading, setLoading] = useState(false);
+  const [progressFinishedAt, setProgressFinishedAt] = useState(null);
+  const { startTracking } = useTrackQueriesRefetch(COMMON_PROPOSAL_KEYS, (_, completedAt) => {
+    if (completedAt > progressFinishedAt) {
+      setLoading(false);
+      setProgressFinishedAt(null);
+    }
+  });
+
+  useEffect(() => {
+    if (!progress && loading && !progressFinishedAt) {
+      setProgressFinishedAt(Date.now());
+      startTracking();
+    }
+  }, [progress, startTracking, loading, progressFinishedAt]);
+
+  if (progress && !loading) {
+    setLoading(true);
+    setProgressFinishedAt(null);
+  }
+
+  return (
+    <Details.Item label={_("Software")}>
+      <Flex direction={{ default: "column" }} gap={{ default: "gapSm" }}>
+        {loading ? (
+          <Skeleton width="50%" />
+        ) : (
+          <Link to={SOFTWARE.root} variant="link" isInline>
+            <Summary />
+          </Link>
+        )}
+        <small>{loading ? <Skeleton /> : <Description />}</small>
+      </Flex>
+    </Details.Item>
+  );
+}
