fa3498f352edcafb1de8ea25701c71b13e7927fc
José Iván López González <jlopez@suse.com>
2026-01-29 12:12:43 +0000

    Add model and hooks for iSCSI

--- web/src/model/config.ts
+++ web/src/model/config.ts
@@ -28,6 +28,7 @@ import type * as Software from "~/openapi/config/software";
 import type * as User from "~/model/config/user";
 import type * as Root from "~/model/config/root";
 import type * as Storage from "~/openapi/config/storage";
+import type * as ISCSI from "~/model/config/iscsi";
 
 type Config = {
   hostname?: Hostname.Config;
@@ -35,9 +36,10 @@ type Config = {
   network?: Network.Config;
   product?: Product.Config;
   storage?: Storage.Config;
+  iscsi?: ISCSI.Config;
   software?: Software.Config;
   user?: User.Config;
   root?: Root.Config;
 };
 
-export type { Config, Hostname, Product, L10n, Network, Storage, User, Root };
+export type { Config, Hostname, Product, L10n, Network, Storage, User, Root, ISCSI };
