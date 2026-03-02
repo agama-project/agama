ba925c736f97923dd8b5a90f5a6811bc646c03a3
José Iván López González <jlopez@suse.com>
2026-03-02 12:39:10 +0000

    Add WithIssues mixin

---
+++ service/lib/agama/dbus/with_issues.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+# Copyright (c) [2026] SUSE LLC
+#
+# All Rights Reserved.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of version 2 of the GNU General Public License as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, contact SUSE LLC.
+#
+# To contact SUSE LLC about this file by physical or electronic mail, you may
+# find current contact information at www.suse.com.
+
+require "json"
+
+module Agama
+  module DBus
+    # Mixin to manage issues.
+    module WithIssues
+      # Generates the serialized JSON of the given issues.
+      #
+      # @param issues [Array<Agama::Issue>]
+      # @return [String]
+      def serialize_issues(issues)
+        json = issues.map { |i| issue_json(i) }
+        JSON.pretty_generate(json)
+      end
+
+      # JSON representation of the given issue.
+      #
+      # @param issue [Agama::Issue]
+      # @return [Hash]
+      def issue_json(issue)
+        {
+          description: issue.description,
+          class:       issue.kind.to_s,
+          details:     issue.details
+        }.compact
+      end
+    end
+  end
+end
