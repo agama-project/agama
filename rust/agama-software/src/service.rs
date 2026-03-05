4fc8c55a640a903357bce7c81374fd7a133fc30b
Ladislav Slezák <lslezak@suse.com>
2026-03-05 16:14:36 +0100

    Reformat

--- rust/agama-software/src/service.rs
+++ rust/agama-software/src/service.rs
@@ -133,7 +133,8 @@ impl Starter {
         let model = match self.model {
             Some(model) => model,
             None => {
-                let zypp_sender = ZyppServer::start(Self::TARGET_DIR, Self::INSTALL_DIR, &kernel_cmdline)?;
+                let zypp_sender =
+                    ZyppServer::start(Self::TARGET_DIR, Self::INSTALL_DIR, &kernel_cmdline)?;
                 Arc::new(Mutex::new(Model::new(
                     zypp_sender,
                     find_mandatory_repositories("/"),
