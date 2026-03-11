a353933136e2aed9deeacf919446b93053df8be9
Ancor Gonzalez Sosa <ancor@suse.de>
2026-03-11 12:12:42 +0100

    service: Infer boot device for reused LVM and RAID

--- service/test/agama/storage/config_solvers/boot_test.rb
+++ service/test/agama/storage/config_solvers/boot_test.rb
@@ -22,10 +22,12 @@
 require_relative "../storage_helpers"
 require "agama/config"
 require "agama/storage/config_conversions/from_json"
-require "agama/storage/config_solvers/boot"
+require "agama/storage/config_solvers"
 require "agama/storage/system"
 
 describe Agama::Storage::ConfigSolvers::Boot do
+  include Agama::RSpec::StorageHelpers
+
   subject { described_class.new(product_config, storage_system) }
 
   let(:product_config) { Agama::Config.new({}) }
@@ -195,15 +197,19 @@ describe Agama::Storage::ConfigSolvers::Boot do
             end
 
             context "and it corresponds to an existing (reused) RAID" do
-              let(:md_device) { instance_double(Y2Storage::Md) }
-
               before do
+                mock_storage(devicegraph: scenario)
                 allow(config.md_raids.first).to receive(:found_device).and_return md_device
-                allow(storage_system).to receive(:candidate?).with(md_device).and_return candidate
               end
 
+              let(:scenario) { "md_raids.yaml" }
+              let(:devicegraph) { Y2Storage::StorageManager.instance.probed }
+              let(:md_device) { devicegraph.find_by_name("/dev/md0") }
+
               context "if the RAID is considered bootable (candidate)" do
-                let(:candidate) { true }
+                before do
+                  allow(storage_system).to receive(:candidate?).with(md_device).and_return true
+                end
 
                 it "sets the alias of the mdRaid as boot device alias" do
                   subject.solve(config)
@@ -228,11 +234,92 @@ describe Agama::Storage::ConfigSolvers::Boot do
               end
 
               context "if the RAID is a regular one (not candidate)" do
-                let(:candidate) { false }
+                context "directly over full disks" do
+                  let(:scenario) { "md_disks.yaml" }
 
-                it "does not set a boot device alias" do
-                  subject.solve(config)
-                  expect(config.boot.device.device_alias).to be_nil
+                  it "does not set a boot device alias" do
+                    subject.solve(config)
+                    expect(config.boot.device.device_alias).to be_nil
+                  end
+                end
+
+                context "defined over partitions of some disks" do
+                  before do
+                    # These complex cases would need a lot of mocking if we do not resolve searches
+                    # first in order to connect the definitions and the devicegraph.
+                    Agama::Storage::ConfigSolvers::DrivesSearch.new(storage_system)
+                      .solve(config)
+                    Agama::Storage::ConfigSolvers::MdRaidsSearch.new(storage_system)
+                      .solve(config)
+                    Agama::Storage::ConfigSolvers::VolumeGroupsSearch.new(storage_system)
+                      .solve(config)
+                  end
+
+                  context "with no drive entries for the underlying disks" do
+                    let(:drives) { [] }
+
+                    it "creates a drive entry for the first disk of the RAID members" do
+                      subject.solve(config)
+                      drive = config.drives.first
+                      expect(drive.found_device.descendants).to include md_device
+                      # Check the first disk (by name) is consistently choosen
+                      expect(drive.found_device.name).to eq "/dev/vda"
+                      expect(drive.alias).to_not be_nil
+                    end
+
+                    it "sets the alias of the drive device as boot device alias" do
+                      subject.solve(config)
+                      drive = config.drives.first
+                      expect(config.boot.device.device_alias).to eq(drive.alias)
+                    end
+
+                    context "if none of the disks is a candidate for installation" do
+                      before do
+                        allow(storage_system).to receive(:candidate?).and_return false
+                      end
+
+                      it "does not set a boot device alias" do
+                        subject.solve(config)
+                        expect(config.boot.device.device_alias).to be_nil
+                      end
+                    end
+                  end
+
+                  context "with drive entries for the underlying disks" do
+                    let(:drives) do
+                      [
+                        {
+                          search: "/dev/vdb",
+                          alias:  "vdb-as-first"
+                        },
+                        {
+                          alias:      "data",
+                          partitions: [
+                            {
+                              filesystem: { path: "/data" },
+                              size:       "50 GiB"
+                            }
+                          ]
+                        }
+                      ]
+                    end
+
+                    it "solves the boot device using the first usable drive" do
+                      subject.solve(config)
+                      expect(config.boot.device.device_alias).to eq("vdb-as-first")
+                    end
+
+                    context "if none of the disks is a candidate for installation" do
+                      before do
+                        allow(storage_system).to receive(:candidate?).and_return false
+                      end
+
+                      it "does not set a boot device alias" do
+                        subject.solve(config)
+                        expect(config.boot.device.device_alias).to be_nil
+                      end
+                    end
+                  end
                 end
               end
             end
@@ -351,6 +438,235 @@ describe Agama::Storage::ConfigSolvers::Boot do
                   subject.solve(config)
                   expect(config.boot.device.device_alias).to be_nil
                 end
+              end
+            end
+          end
+
+          context "and an existing LVM volume group is reused for root" do
+            before do
+              mock_storage(devicegraph: scenario)
+
+              # These complex cases would need a lot of mocking if we do not resolve searches
+              # first in order to connect the definitions and the devicegraph.
+              Agama::Storage::ConfigSolvers::DrivesSearch.new(storage_system)
+                .solve(config)
+              Agama::Storage::ConfigSolvers::MdRaidsSearch.new(storage_system)
+                .solve(config)
+              Agama::Storage::ConfigSolvers::VolumeGroupsSearch.new(storage_system)
+                .solve(config)
+            end
+
+            let(:volume_groups) do
+              [
+                {
+                  search:         vg_name,
+                  logicalVolumes: [
+                    { filesystem: { path: "/" } }
+                  ]
+                }
+              ]
+            end
+
+            RSpec.shared_examples "no alias" do
+              it "does not set a boot device alias" do
+                subject.solve(config)
+                expect(config.boot.device.device_alias).to be_nil
+              end
+            end
+
+            RSpec.shared_examples "no alias if no candidate disk" do
+              context "if none of the disks is a candidate for installation" do
+                before do
+                  allow(storage_system).to receive(:candidate?).and_return false
+                end
+
+                include_examples "no alias"
+              end
+            end
+
+            RSpec.shared_examples "set alias" do
+              it "sets the alias of the drive device as boot device alias" do
+                subject.solve(config)
+                drive = config.drives.first
+                expect(config.boot.device.device_alias).to eq(drive.alias)
+              end
+            end
+
+            context "and the physical volumes are full disks" do
+              let(:scenario) { "lvm_with_nested_thin_lvs.xml" }
+              let(:vg_name) { "/dev/vg_b" }
+
+              context "with no drive entries for the underlying disks" do
+                let(:drives) { [] }
+
+                include_examples "no alias"
+              end
+
+              context "with drive entries for the underlying disks" do
+                let(:drives) do
+                  [
+                    {
+                      search: "/dev/sdd",
+                      alias:  "sdd"
+                    }
+                  ]
+                end
+
+                include_examples "no alias"
+              end
+            end
+
+            context "and the physical volumes are partitions on disks" do
+              let(:scenario) { "several_vgs.yaml" }
+              let(:vg_name) { "/dev/data" }
+
+              context "with no drive entries for the underlying disks" do
+                let(:drives) { [] }
+
+                it "creates a drive entry for the first disk of the RAID members" do
+                  subject.solve(config)
+                  drive = config.drives.first
+                  # Check the first disk (by name) is consistently choosen
+                  expect(drive.found_device.name).to eq "/dev/sda"
+                  expect(drive.alias).to_not be_nil
+                end
+
+                include_examples "set alias"
+                include_examples "no alias if no candidate disk"
+              end
+
+              context "with drive entries for some of the underlying disks" do
+                let(:drives) do
+                  [
+                    {
+                      search: "/dev/sdb",
+                      alias:  "sdb"
+                    }
+                  ]
+                end
+
+                it "solves the boot device using the first usable drive" do
+                  subject.solve(config)
+                  expect(config.boot.device.device_alias).to eq("sdb")
+                end
+
+                include_examples "no alias if no candidate disk"
+              end
+            end
+
+            context "and some physical volumes are software RAIDs on top of partitioned disks" do
+              let(:scenario) { "lvm-over-raids.yaml" }
+              let(:vg_name) { "/dev/vg0" }
+
+              context "with no drive entries for the underlying disks" do
+                let(:drives) { [] }
+
+                it "creates a drive entry for the first partitioned disk of the RAID members" do
+                  subject.solve(config)
+                  drive = config.drives.first
+                  expect(drive.found_device.name).to eq "/dev/vda"
+                  expect(drive.alias).to_not be_nil
+                end
+
+                include_examples "set alias"
+                include_examples "no alias if no candidate disk"
+              end
+
+              context "with drive entries for some of the underlying disks" do
+                let(:drives) do
+                  [
+                    {
+                      search: "/dev/vdc",
+                      alias:  "vdc"
+                    },
+                    {
+                      search: "/dev/vda",
+                      alias:  "vda"
+                    }
+                  ]
+                end
+
+                it "solves the boot device using the first usable drive" do
+                  subject.solve(config)
+                  # vdc cannot be chosen because it is fully used as RAID member, no partitions
+                  expect(config.boot.device.device_alias).to eq("vda")
+                end
+
+                include_examples "no alias if no candidate disk"
+              end
+            end
+
+            context "and all physical volumes are software RAIDs on top of full disks" do
+              let(:scenario) { "lvm-over-raids.yaml" }
+              let(:vg_name) { "/dev/vg1" }
+
+              context "with no drive entries for the underlying disks" do
+                let(:drives) { [] }
+
+                include_examples "no alias"
+              end
+
+              context "with drive entries for the underlying disks" do
+                let(:drives) do
+                  [
+                    {
+                      search: "/dev/vde",
+                      alias:  "vde"
+                    },
+                    {
+                      search: "/dev/vdf",
+                      alias:  "vdf"
+                    }
+                  ]
+                end
+
+                include_examples "no alias"
+              end
+            end
+
+            context "and the physical volumes are BIOS RAIDs and partitions on top of them" do
+              let(:scenario) { "lvm-over-hardware-raid.xml" }
+              let(:vg_name) { "/dev/system" }
+
+              context "with no mdRaid or drive entries for the underlying devices" do
+                let(:drives) { [] }
+                let(:mdRaids) { [] }
+
+                it "creates an mdRaid entry for the first partitioned RAID" do
+                  subject.solve(config)
+                  raid = config.md_raids.first
+                  expect(raid.found_device.name).to eq "/dev/md/a"
+                  expect(raid.alias).to_not be_nil
+                end
+
+                it "sets the alias of the RAID as boot device alias" do
+                  subject.solve(config)
+                  raid = config.md_raids.first
+                  expect(config.boot.device.device_alias).to eq(raid.alias)
+                end
+              end
+
+              context "with drive entries for some of the underlying RAIDs" do
+                let(:drives) do
+                  [
+                    {
+                      search: "/dev/md/b",
+                      alias:  "raid-b"
+                    },
+                    {
+                      search: "/dev/md/a",
+                      alias:  "raid-a"
+                    }
+                  ]
+                end
+
+                it "solves the boot device using the first usable drive" do
+                  subject.solve(config)
+                  # md/b cannot be chosen because it is fully used as physical volume, no partitions
+                  expect(config.boot.device.device_alias).to eq("raid-a")
+                end
+
+                include_examples "no alias if no candidate disk"
               end
             end
           end
