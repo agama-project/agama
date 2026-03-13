b9c289bc42640347d54c91f81f73bff1b9d1e9c1
David Díaz González <dgonzalez@suse.com>
2026-03-13 13:37:10 +0000

    refactor(web): remove network page tabs
    The devices table will also be removed since the improved connections
    table already covers its functionality.

--- web/src/components/network/NetworkPage.tsx
+++ web/src/components/network/NetworkPage.tsx
@@ -20,14 +20,13 @@
  * find current contact information at www.suse.com.
  */
 
-import React, { useState } from "react";
-import { Flex, Stack, Tab, Tabs, TabTitleText } from "@patternfly/react-core";
-import { Link, NestedContent, Page } from "~/components/core";
+import React from "react";
+import { Flex, Stack } from "@patternfly/react-core";
+import { Link, Page } from "~/components/core";
 import Text from "~/components/core/Text";
 import Icon from "~/components/layout/Icon";
 import NoPersistentConnectionsAlert from "~/components/network/NoPersistentConnectionsAlert";
 import ConnectionsTable from "~/components/network/ConnectionsTable";
-import DevicesTable from "~/components/network/DevicesTable";
 import { useNetworkChanges, useSystem } from "~/hooks/model/system/network";
 import { NETWORK } from "~/routes/paths";
 import { _ } from "~/i18n";
@@ -37,12 +36,7 @@ import { _ } from "~/i18n";
  */
 export default function NetworkPage() {
   useNetworkChanges();
-  const { devices, state } = useSystem();
-  const [tab, setTab] = useState("0");
-
-  const handleTabClick = (e, v) => {
-    setTab(v.toString());
-  };
+  const { state } = useSystem();
 
   return (
     <Page
@@ -51,61 +45,34 @@ export default function NetworkPage() {
     >
       <Page.Content>
         <NoPersistentConnectionsAlert />
-
-        <Tabs activeKey={tab} onSelect={handleTabClick} role="region">
-          <Tab eventKey={"0"} title={<TabTitleText>{_("Connections")}</TabTitleText>}>
-            <NestedContent margin="mxSm">
-              <Stack hasGutter>
-                <Text textStyle="textColorSubtle">
-                  {_(
-                    "Manage available connections, connect to Wi-Fi, or add a new connection. Switch to Devices to manage by device.",
-                  )}
-                </Text>
-                <Page.Section
-                  pfCardProps={{ isCompact: true, component: "div" }}
-                  actions={
-                    <>
-                      <Link to={NETWORK.newConnection} variant="plain">
-                        <Flex
-                          gap={{ default: "gapXs" }}
-                          alignItems={{ default: "alignItemsCenter" }}
-                        >
-                          <Icon name="add_circle" /> {_("Add connection")}
-                        </Flex>
-                      </Link>
-                      {state.wirelessEnabled && (
-                        <Link to={NETWORK.newWiFiConnection} variant="plain">
-                          <Flex
-                            gap={{ default: "gapSm" }}
-                            alignItems={{ default: "alignItemsCenter" }}
-                          >
-                            <Icon name="wifi" /> {_("Connect to Wi-Fi network")}
-                          </Flex>
-                        </Link>
-                      )}
-                    </>
-                  }
-                >
-                  <ConnectionsTable />
-                </Page.Section>
-              </Stack>
-            </NestedContent>
-          </Tab>
-          <Tab eventKey={"1"} title={<TabTitleText>{_("Devices")}</TabTitleText>}>
-            <NestedContent margin="mxSm">
-              <Stack hasGutter>
-                <Text textStyle="textColorSubtle">
-                  {_(
-                    "Browse available devices and configure their connections. Switch to Connections to manage by connection.",
-                  )}
-                </Text>
-                <Page.Section pfCardProps={{ isCompact: true, component: "div" }}>
-                  <DevicesTable devices={devices} />
-                </Page.Section>
-              </Stack>
-            </NestedContent>
-          </Tab>
-        </Tabs>
+        <Stack hasGutter>
+          <Text textStyle="textColorSubtle">
+            {_(
+              "Manage available connections, connect to Wi-Fi, or add a new connection. Switch to Devices to manage by device.",
+            )}
+          </Text>
+          <Page.Section
+            pfCardProps={{ isCompact: true, component: "div", isFullHeight: false }}
+            actions={
+              <>
+                <Link to={NETWORK.newConnection} variant="plain">
+                  <Flex gap={{ default: "gapXs" }} alignItems={{ default: "alignItemsCenter" }}>
+                    <Icon name="add_circle" /> {_("Add connection")}
+                  </Flex>
+                </Link>
+                {state.wirelessEnabled && (
+                  <Link to={NETWORK.newWiFiConnection} variant="plain">
+                    <Flex gap={{ default: "gapSm" }} alignItems={{ default: "alignItemsCenter" }}>
+                      <Icon name="wifi" /> {_("Connect to Wi-Fi network")}
+                    </Flex>
+                  </Link>
+                )}
+              </>
+            }
+          >
+            <ConnectionsTable />
+          </Page.Section>
+        </Stack>
       </Page.Content>
     </Page>
   );
