da6ab1f53e1097e60191225ade0ebb3c041e0dc5
Michal Filka <mfilka@suse.cz>
2026-03-13 15:58:48 +0100

    Improvement for OpenAPI fix

--- rust/agama-server/src/web/docs.rs
+++ rust/agama-server/src/web/docs.rs
@@ -18,7 +18,6 @@
 // To contact SUSE LLC about this file by physical or electronic mail, you may
 // find current contact information at www.suse.com.
 
-use agama_utils::api::users::config::{RootUserConfig, StringOrList};
 use utoipa::openapi::{Components, Info, InfoBuilder, OpenApi, OpenApiBuilder, Paths};
 
 mod config;
@@ -35,19 +34,7 @@ pub trait ApiDocBuilder {
 
     fn paths(&self) -> Paths;
 
-    fn components(&self) -> Components {
-        let mut builder = utoipa::openapi::schema::ComponentsBuilder::new();
-        let mut schemas = Vec::new();
-
-        <RootUserConfig as utoipa::ToSchema>::schemas(&mut schemas);
-        <StringOrList as utoipa::ToSchema>::schemas(&mut schemas);
-
-        for (name, schema) in schemas {
-            builder = builder.schema(name, schema);
-        }
-
-        builder.build()
-    }
+    fn components(&self) -> Components;
 
     fn info(&self) -> Info {
         InfoBuilder::new()
