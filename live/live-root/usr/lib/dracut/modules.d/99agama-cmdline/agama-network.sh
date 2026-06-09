30f6a643abec97071917f20cca87fbeddf399d0b
Knut Anderssen <kanderssen@suse.com>
2026-06-09 12:55:13 +0100

    Consider rd.hcn a custom network

--- live/live-root/usr/lib/dracut/modules.d/99agama-cmdline/agama-network.sh
+++ live/live-root/usr/lib/dracut/modules.d/99agama-cmdline/agama-network.sh
@@ -5,7 +5,7 @@
 . /lib/dracut-lib.sh
 . /lib/net-lib.sh
 
-if getargs ip= >/dev/null || getargs hcn.ip= >/dev/null || getargs hcn.route= >/dev/null; then
+if getargs ip= >/dev/null || getargs rd.hcn.ip= >/dev/null || getargs rd.hcn.route= >/dev/null; then
   mkdir -p /run/agama/
   : >/run/agama/custom_dracut_network
 fi
