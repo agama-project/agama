850c1a4f12aac60394fbb710becbe7421e0a90d2
Michal Filka <mfilka@suse.cz>
2026-06-11 09:42:47 +0200

    Fixed path

--- rust/agama-lib/src/profile.rs
+++ rust/agama-lib/src/profile.rs
@@ -128,7 +128,8 @@ pub struct ProfileValidator {
 
 impl ProfileValidator {
     pub fn default_schema() -> Result<Self, ProfileError> {
-        let relative_path = PathBuf::from("share/profile.schema.json");
+        // profile.schema.json moved to from /rust/agama-lib/share to /rust/share/
+        let relative_path = PathBuf::from("../share/profile.schema.json");
         let path = if relative_path.exists() {
             relative_path
         } else {
