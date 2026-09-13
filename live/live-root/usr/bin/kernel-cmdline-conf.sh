0b6b8616be30f8df7f6bb1d6baf91026e27cdcf1
Josef Reidinger <jreidinger@suse.cz>
2026-09-13 13:01:13 +0200

    initial POC for setting NTP either from dialog or from dracut module (jsc#PED-16702 )

--- live/live-root/usr/bin/kernel-cmdline-conf.sh
+++ live/live-root/usr/bin/kernel-cmdline-conf.sh
@@ -22,7 +22,7 @@ write_kernel_args() {
       _found=1
       ;;
     # remove the Kiwi PXE boot options or Live options
-    rd.kiwi.* | rd.live.* | ramdisk_size=* | initrd=* | BOOT_IMAGE=*)
+    rd.kiwi.* | rd.live.* | rd.ntp* | ramdisk_size=* | initrd=* | BOOT_IMAGE=*)
       _found=1
       ;;
     # remove the network configuration options
